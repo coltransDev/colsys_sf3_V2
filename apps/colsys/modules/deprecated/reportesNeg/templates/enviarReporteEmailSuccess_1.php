e2b4a8453af8f3d6308a296397055b1f146a4461
Andres Botero <abotero@coltrans.com.co>
2009-10-13 21:21:24 +0000

    Cotizacion en Status

--- apps/colsys/modules/deprecated/reportesNeg/templates/enviarReporteEmailSuccess_1.php
+++
@@ -1,1 +0,0 @@
-<h3> El correo electronico se ha enviado correctamente, se ha enviado una copia a su correo electronico</h3>
--- apps/colsys/modules/deprecated/reportesNeg/templates/guardarRecargoSuccess_1.php
+++
@@ -1,5 +0,0 @@
-<?
-use_helper("Javascript", "Object", "Validation");
-include_component("reportesNeg","relacionDeRecargos", array( "reporteNegocio"=>$reporteNegocio,  "tipo"=>$tipo, "editable"=>true ));
-?>
-
