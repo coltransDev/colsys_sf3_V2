62bd9e019686964eecde5634c813f84f86383cfc
Andres Botero <abotero@coltrans.com.co>
2009-10-13 21:37:49 +0000

    Cotizacion en Status

---
+++ apps/colsys/modules/gestDocumental/templates/verArchivoSuccess.php
@@ -0,0 +1,12 @@
+<?
+
+header('Content-Disposition: attachment; filename="'.basename($archivo).'"');
+header('content-type: "'.Utils::mimetype(basename($archivo)).'"');
+header('Content-Length: '.filesize($archivo));
+
+
+
+readfile($archivo);
+exit();
+
+?>
