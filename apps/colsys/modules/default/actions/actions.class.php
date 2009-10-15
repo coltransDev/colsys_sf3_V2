febedfd950f62d56b183bb9e2378c465124998cf
Andres Botero <abotero@coltrans.com.co>
2009-10-15 17:25:33 +0000

    & se imprimia &ampm; en cotización.

---
+++ apps/colsys/modules/default/actions/actions.class.php
@@ -0,0 +1,20 @@
+<?php
+/* 
+ *  This file is part of the Colsys Project.
+ * 
+ *  (c) Coltrans S.A. - Colmas Ltda.
+ */
+
+class defaultActions extends sfActions
+{
+    /**
+     * Error page for page not found (404) error
+     *
+     */
+    public function executeError404()
+    {
+    }
+}
+?>
+
+
---
+++ apps/colsys/modules/default/templates/error404Success.php
@@ -0,0 +1,10 @@
+<div class="content" align="center">
+
+    <div class="box1" style="width:550px" align="center">   
+      
+        <h1>Pagina no encontrada</h1>
+        El servidor retorno una respuesta 404  
+    
+
+    </div>
+</div>
