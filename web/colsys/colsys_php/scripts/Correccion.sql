7f3fa97d37ab12056ef11851bd32a1bca3cd764a
Andres Botero <abotero@coltrans.com.co>
2009-10-15 15:54:10 +0000

    Cotizacion en Status

---
+++ web/colsys/colsys_php/scripts/Correccion.sql
@@ -0,0 +1,13 @@
+update tb_trayectos  set ca_modalidad = 'LCL' where tb_trayectos.ca_idtrayecto = tb_fletes.ca_idtrayecto and 
+ca_transporte = 'Marítimo' and ca_modalidad = 'CONSOLIDADO' and tb_fletes.ca_idconcepto = 9;
+
+
+update tb_trayectos  set ca_modalidad = 'FCL' where tb_trayectos.ca_idtrayecto = tb_fletes.ca_idtrayecto and 
+ca_transporte = 'Marítimo' and ca_modalidad = 'CONSOLIDADO' and tb_fletes.ca_idconcepto != 9;
+
+
+update tb_trayectos set ca_modalidad = 'FCL' where tb_trayectos.ca_idtrayecto = tb_fletes.ca_idtrayecto and 
+ca_transporte = 'Marítimo' and ca_modalidad = 'LCL' and tb_fletes.ca_idconcepto != 9;
+
+
+update tb_fletes set ca_vlrminimo = ca_vlrneto where ca_vlrminimo = 0
