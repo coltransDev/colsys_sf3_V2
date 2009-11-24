48cea7fa70134f5dd91e9c9290adb7b241633aa1
Andres Botero <abotero@coltrans.com.co>
2009-11-24 14:23:33 +0000

    Creacion de recargos y usio de vistas para compatibilidad

---
+++ lib/form/doctrine/base/BaseInoTipoDocumentoForm.class.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * InoTipoDocumento form base class.
+ *
+ * @method InoTipoDocumento getObject() Returns the current form's model object
+ *
+ * @package    symfony
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 24171 2009-11-19 16:37:50Z Kris.Wallsmith $
+ */
+abstract class BaseInoTipoDocumentoForm extends BaseFormDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'ca_idtipo'         => new sfWidgetFormInputHidden(),
+      'ca_tipo'           => new sfWidgetFormTextarea(),
+      'ca_comprobante'    => new sfWidgetFormInputText(),
+      'ca_descripcion'    => new sfWidgetFormTextarea(),
+      'ca_titulo'         => new sfWidgetFormTextarea(),
+      'ca_mensaje'        => new sfWidgetFormTextarea(),
+      'ca_noautorizacion' => new sfWidgetFormTextarea(),
+      'ca_prefijo_aut'    => new sfWidgetFormTextarea(),
+      'ca_inicial_aut'    => new sfWidgetFormInputText(),
+      'ca_final_aut'      => new sfWidgetFormInputText(),
+      'ca_activo'         => new sfWidgetFormInputCheckbox(),
+    ));
+
+    $this->setValidators(array(
+      'ca_idtipo'         => new sfValidatorDoctrineChoice(array('model' => $this->getModelName(), 'column' => 'ca_idtipo', 'required' => false)),
+      'ca_tipo'           => new sfValidatorString(array('required' => false)),
+      'ca_comprobante'    => new sfValidatorInteger(array('required' => false)),
+      'ca_descripcion'    => new sfValidatorString(array('required' => false)),
+      'ca_titulo'         => new sfValidatorString(array('required' => false)),
+      'ca_mensaje'        => new sfValidatorString(array('required' => false)),
+      'ca_noautorizacion' => new sfValidatorString(array('required' => false)),
+      'ca_prefijo_aut'    => new sfValidatorString(array('required' => false)),
+      'ca_inicial_aut'    => new sfValidatorInteger(array('required' => false)),
+      'ca_final_aut'      => new sfValidatorInteger(array('required' => false)),
+      'ca_activo'         => new sfValidatorBoolean(array('required' => false)),
+    ));
+
+    $this->widgetSchema->setNameFormat('ino_tipo_documento[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'InoTipoDocumento';
+  }
+
+}
