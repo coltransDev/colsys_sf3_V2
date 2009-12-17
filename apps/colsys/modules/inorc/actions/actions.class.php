4e46d2b5cd5517fa80b4738dab3dfa82cf541ca3
Andres Botero <abotero@coltrans.com.co>
2009-12-17 17:18:22 +0000

    INO mejoras

---
+++ apps/colsys/modules/inorc/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * inorc actions.
+ *
+ * @package    symfony
+ * @subpackage inorc
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class inorcActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
