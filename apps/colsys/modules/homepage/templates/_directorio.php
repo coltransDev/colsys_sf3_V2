97bea0405922f95f961b6fd6f80ea0e11a1f09f0
Andres Botero <abotero@coltrans.com.co>
2010-02-01 15:06:34 +0000

    Se agrego BUN al directorio

--- apps/colsys/modules/homepage/templates/_directorio.php
+++ apps/colsys/modules/homepage/templates/_directorio.php
@@ -9,19 +9,20 @@
 <br>
 
 
-<table width="100%" border="1" class="tableList" >			
+<table width="100%" border="1" class="tableList" >
+    <tr>
 		<td><div align="left">Para llamar desde <b>Bogotá</b> a <br>
 				<ul>
 					<li><b>Medellín:</b> 804 44 + Ext.</li>
 					<li><b>Cali:</b> 804 22 + Ext.</li>
 					<li><b>B/quilla.:</b> 804 55 + Ext.</li>
+                    <li><b>Buenaventura:</b> 804 33 + Ext.</li>
 				</ul>
 		</div></td>
 		<td><div align="left">Para llamar desde <b>Medellín</b> a <br>
 				<ul>
 					<li><b>Bogotá:</b> 804 11 + Ext.</li>
 					<li><b>Cali:</b> 804 22 + Ext.</li>
-
 					<li><b>B/quilla.:</b> 804 55 + Ext.</li>
 				</ul>
 		</div></td>
@@ -29,7 +30,6 @@
 	<tr>
 		<td><div align="left">Para llamar desde <b>Cali</b> a <br>
 				<ul>
-
 					<li><b>Bogotá:</b> 84 11 + Ext.</li>
 					<li><b>Medellín:</b> 84 44 + Ext.</li>
 					<li><b>B/quilla.:</b> 84 55 + Ext.</li>
@@ -40,8 +40,17 @@
 				<ul>
 					<li><b>Bogotá:</b> 711 11 + Ext.</li>
 					<li><b>Medellín:</b> 711 44 + Ext.</li>
-
 					<li><b>Cali.:</b> 711 22 + Ext.</li>
 				</ul>
 		</div></td>
-		</table>
+    </tr>
+    <tr>
+		<td><div align="left">Para llamar desde <b>Buenaventura</b> a <br>
+				<ul>
+					<li><b>Bogotá:</b> 87 11 + Ext.</li>
+				</ul>
+
+		</div></td>
+		<td>&nbsp;</td>
+    </tr>
+</table>
