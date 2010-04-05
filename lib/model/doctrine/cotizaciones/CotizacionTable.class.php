2c5cafd4e6c417c7027cde62af494cb00f63fd29
Mauricio A Quinche <maquinche@coltrans.com.co>
2010-04-05 13:34:48 +0000

    [cotizaciones] Se modifico el metodo de siguiente consecutivo para recibir la empresa.

--- lib/model/doctrine/cotizaciones/CotizacionTable.class.php
+++ lib/model/doctrine/cotizaciones/CotizacionTable.class.php
@@ -7,9 +7,9 @@ class CotizacionTable extends Doctrine_Table
 	* Retorna el siguiente consecutivo para los reportes
 	* @author: Andres Botero
 	*/
-	public static function siguienteConsecutivo( $yy ){
+	public static function siguienteConsecutivo( $yy , $company){
 		if( $yy ){
-            $sql =  "SELECT fun_cotizacioncon('".$yy."') as next";
+            $sql =  "SELECT fun_cotizacioncon('".$yy."','".$company."') as next";
             $q = Doctrine_Manager::getInstance()->connection();
             $stmt = $q->execute($sql);
 			$row = $stmt->fetch();
