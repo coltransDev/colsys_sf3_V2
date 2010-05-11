0a54bcde95a2e67f4a755918801d61a46a86b024
Andres Botero <abotero@coltrans.com.co>
2010-05-11 19:51:28 +0000

    [myLogger] Cuando no existe el usuario se mostraba error

--- lib/myLogger.class.php
+++ lib/myLogger.class.php
@@ -11,8 +11,8 @@ class myLogger extends sfFileLogger
     protected function doLog($message, $priority)
     {
         
-        $id = " [".sfContext::getinstance()->getUser()->getUserId()." ".$_SERVER["REQUEST_METHOD"]." ".$_SERVER["REQUEST_URI"];
-        
+        $id = " [".(sfContext::getinstance()->getUser()?sfContext::getinstance()->getUser()->getUserId():"")." ".$_SERVER["REQUEST_METHOD"]." ".$_SERVER["REQUEST_URI"];
+
         if( $_SERVER["QUERY_STRING"] ){
             $id .=" query: ".$_SERVER["QUERY_STRING"];
         }
@@ -26,6 +26,7 @@ class myLogger extends sfFileLogger
         }
 
         $id .="]\n";
+        
 
         parent::doLog($message.$id, $priority);
     }
