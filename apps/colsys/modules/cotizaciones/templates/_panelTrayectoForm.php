116480b2e8507129ff4520517cfe9466ab536a94
Mauricio A Quinche <maquinche@coltrans.com.co>
2010-07-15 13:16:16 +0000

    [cotizacion] Se cambio la pestaña de continuacion de viaje, y se coloco la funcionalidad como en trayectos

--- apps/colsys/modules/cotizaciones/templates/_panelTrayectoForm.php
+++ apps/colsys/modules/cotizaciones/templates/_panelTrayectoForm.php
@@ -6,20 +6,17 @@
  */
 
 include_component("widgets", "widgetIncoterms");
-
+//include_component("widgets", "widgetPais");
+//include_component("widgets", "widgetCiudad");
 
 ?>
 <script type="text/javascript">
     PanelTrayectoForm = function( config ) {
         Ext.apply(this,config);
-        PanelTrayectoForm.superclass.constructor.call(this, {                
-                layout: 'form',
-                frame: true,
-                title: 'Ingrese los datos del trayecto',
-                autoHeight: true,
-                bodyStyle: 'padding: 5px 5px 0 5px;',
-                labelWidth: 100,
-                items: [{
+        this.items;        
+        if(this.tipo=="Trayecto")
+        {
+            this.items=[{
                             id: 'cotizacionId',
                             xtype:'hidden',
                             name: 'cotizacionId',
@@ -41,7 +38,7 @@ include_component("widgets", "widgetIncoterms");
                             width: 300
                         }
                         ,<?=include_component("widgets", "impoexpo" ,array("id"=>"impoexpo", "label"=>"Impo/Expo"))?>
-                        , new WidgetIncoterms({name:"incoterms"})                        
+                        , new WidgetIncoterms({name:"incoterms"})
                         ,<?=include_component("widgets", "transportes" ,array("id"=>"transporte", "allowBlank"=>"false"))?>
 
                         ,<?=include_component("widgets", "modalidades" ,array("id"=>"modalidad", "label"=>"Modalidad", "allowBlank"=>"false", "transporte"=>"transporte", "impoexpo"=>"impoexpo"))?>
@@ -56,8 +53,6 @@ include_component("widgets", "widgetIncoterms");
                         }
                         ,<?=include_component("widgets", "paises" ,array("id"=>"tra_origen", "label"=>"Pais Origen", "allowBlank"=>"false"))?>
                         ,<?=include_component("widgets", "ciudades" ,array("id"=>"ciu_origen", "label"=>"Ciudad Origen", "link"=>"tra_origen", "allowBlank"=>"false"))?>
-
-
                         ,<?=include_component("widgets", "paises" ,array("id"=>"tra_destino", "label"=>" Pais Destino", "value"=>"C0-057", "allowBlank"=>"false"))?>
                         ,<?=include_component("widgets", "ciudades" ,array("id"=>"ciu_destino", "label"=>"Ciudad Destino", "link"=>"tra_destino", "allowBlank"=>"false"))?>
                         ,<?=include_component("widgets", "paises" ,array("id"=>"tra_escala", "label"=>"Pais Escala"))?>
@@ -111,7 +106,126 @@ include_component("widgets", "widgetIncoterms");
                             value: '',
                             allowBlank:false
                         }
-                    ]            
+                    ];
+        }
+        else if(this.tipo=="OTM/DTA")
+        {
+            this.items=[{
+                            id: 'cotizacionId',
+                            xtype:'hidden',
+                            name: 'cotizacionId',
+                            value: '<?=$cotizacion->getCaIdcotizacion()?>',
+                            allowBlank:false
+                        },{
+                            id: 'idproducto',
+                            xtype:'hidden',
+                            name: 'idproducto',
+                            value: '',
+                            allowBlank:false
+                        },
+                        new Ext.form.ComboBox({
+                            fieldLabel: 'Tipo',
+                            typeAhead: true,
+                            forceSelection: true,
+                            triggerAction: 'all',
+                            emptyText:'',
+                            selectOnFocus: true,
+                            name: 'producto',
+                            id: 'producto',
+                            lazyRender:true,
+                            listClass: 'x-combo-list-small',
+                            store: [['OTM', 'OTM'],['DTA', 'DTA']]
+                        })
+                        ,
+                        new Ext.form.ComboBox({
+                            fieldLabel: 'Modalidad',
+                            typeAhead: true,
+                            forceSelection: true,
+                            triggerAction: 'all',
+                            emptyText:'',
+                            selectOnFocus: true,
+                            name: 'modalidad',
+                            id: 'modalidad',
+                            lazyRender:true,
+                            listClass: 'x-combo-list-small',
+                            store: [['FCL', 'FCL'],['LCL', 'LCL']]
+                        })
+                        ,{
+                            xtype:'hidden',
+                            id: 'impoexpo',
+                            name: 'impoexpo',
+                            value: '<?=Constantes::IMPO?>'
+                        },
+                        {
+                            xtype:'hidden',
+                            id: 'transporte',
+                            name: 'transporte',
+                            value: 'OTM-DTA'
+                        },
+                        {
+                            xtype:'hidden',
+                            id: 'incoterms',
+                            name: 'incoterms',
+                            value: ' '
+                        },
+                        {
+                            xtype:'hidden',
+                            id: 'imprimir',
+                            name: 'imprimir',
+                            value: ' '
+                        }
+                        ,<?=include_component("widgets", "paises" ,array("id"=>"tra_origen", "label"=>"Pais Origen", "allowBlank"=>"false"))?>
+                        ,<?=include_component("widgets", "ciudades" ,array("id"=>"ciu_origen", "label"=>"Ciudad Origen", "link"=>"tra_origen", "allowBlank"=>"false"))?>
+                        ,<?=include_component("widgets", "paises" ,array("id"=>"tra_destino", "label"=>" Pais Destino", "value"=>"C0-057", "allowBlank"=>"false"))?>
+                        ,<?=include_component("widgets", "ciudades" ,array("id"=>"ciu_destino", "label"=>"Ciudad Destino", "link"=>"tra_destino", "allowBlank"=>"false"))?>
+
+                        ,{
+                            xtype: 'textarea',
+                            width: 310,
+                            height: 40,
+                            fieldLabel: 'Observaciones',
+                            name: 'observaciones',
+                            value: '',
+                            allowBlank:true
+                        }
+                        ,{
+                            xtype: 'textfield',
+                            width: 100,
+                            fieldLabel: 'Frecuencia',
+                            name: 'frecuencia',
+                            value: '',
+                            allowBlank:true
+                        }
+                        ,{
+                            xtype: 'textfield',
+                            width: 100,
+                            fieldLabel: 'T/Transito',
+                            name: 'ttransito',
+                            value: '',
+                            allowBlank:true
+                        }
+                        ,
+                        {
+                            xtype: 'datefield',
+                            width: 100,
+                            fieldLabel: 'Vigencia',
+                            name: 'vigencia',
+                            format: 'Y-m-d',
+                            value: '',
+                            allowBlank:false
+                        }
+                    ];
+        }
+
+
+        PanelTrayectoForm.superclass.constructor.call(this, {                
+                layout: 'form',
+                frame: true,
+                title: 'Ingrese los datos del '+this.tipo,
+                autoHeight: true,
+                bodyStyle: 'padding: 5px 5px 0 5px;',
+                labelWidth: 100,
+                items: this.items
 
         });
 
