3d4a7fc65568bb9222ffb845d08de9021e9aaa22
Andres Botero <abotero@coltrans.com.co>
2010-07-27 19:18:19 +0000

    [gestDocumental] Opcion de subir archivos desde YUI Editor

--- apps/colsys/modules/gestDocumental/actions/actions.class.php
+++ apps/colsys/modules/gestDocumental/actions/actions.class.php
@@ -133,21 +133,58 @@ class gestDocumentalActions extends sfActions
 	}
 
 
-
     /*
-	*Conector ckfinder
-	* @author: Andres Botero
-	*/
-	public function executeCkFinderConnector(){
-        
-        define("CKFINDER_LIB", sfConfig::get("sf_lib_dir").DIRECTORY_SEPARATOR."vendor".DIRECTORY_SEPARATOR
-                              ."ckfinder".DIRECTORY_SEPARATOR );
-        
-		require(CKFINDER_LIB."connector".DIRECTORY_SEPARATOR
-                              ."php".DIRECTORY_SEPARATOR."connector.php");
+     * Permite subir una imagen desde el editor YUI
+     */
+    public function executeUploadImage( $request ){
+        $folder = $request->getParameter("folder");
+        $idissue = $request->getParameter("idissue");
+        //header("content-type: text/html"); // the return type must be text/html
+        //if file has been sent successfully:
+        $this->responseArray = array();
+        if (isset($_FILES['image']['tmp_name'])) {
+            // open the file
+            $img = $_FILES['image']['tmp_name'];
+            $himage = fopen ( $img, "r"); // read the temporary file into a buffer
+            $image = fread ( $himage, filesize($img) );
+            fclose($himage);
+            //if image can't be opened, either its not a valid format or even an image:
+            if ($image === FALSE) {
+                $this->responseArray["status"] = utf8_encode('Error Reading Uploaded File.');
+            }else{
+                // create a new random numeric name to avoid rewriting other images already on the server...
+                $ran = rand ();
+                $ran2 = $ran.".";
 
-        return sfView::NONE;
-	}
+                $directory = sfConfig::get('app_digitalFile_root').DIRECTORY_SEPARATOR.$folder.DIRECTORY_SEPARATOR;
+
+                if(!is_dir($directory)){
+                    @mkdir($directory, 0777, true);
+                }
+
+                $filename = $ran2.'jpg';
+                // join path and name
+                $path = $directory . $filename;
+                // copy the image to the server, alert on fail
+                $hout=fopen($path,"w");
+                fwrite($hout,$image);
+                fclose($hout);
+                //you'll need to modify the path here to reflect your own server.
+                
+                $urlpath = "/gestDocumental/verArchivo/folder/".base64_encode($folder)."/idarchivo/".base64_encode($filename);
+                $this->responseArray["status"] = 'UPLOADED';
+                $this->responseArray["image_url"] = $urlpath;
+            }
+        } else {
+             $this->responseArray["status"] = utf8_encode('No file was submitted');
+        }
+
+
+
+
+
+        $this->setTemplate("responseTemplate");
+    }
 	
 	
 	
