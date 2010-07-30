1389c18d659a8bea89ddb4e9dd71fd598694b40b
Mauricio A Quinche <maquinche@coltrans.com.co>
2010-07-30 20:16:02 +0000

    [cotizaciones] Cotizaciones con versiones

--- apps/colsys/modules/cotizaciones/templates/busquedaCotizacionSuccess.php
+++ apps/colsys/modules/cotizaciones/templates/busquedaCotizacionSuccess.php
@@ -1,7 +1,7 @@
 <?
 /*
 * Muestra los resultados de la busqueda de la cotización 
-* @author Andres Botero
+* @author Andres Botero y Mauricio Quinche
 */	
 ?>
 <div align="center">
@@ -10,15 +10,12 @@ $url = "cotizaciones/busquedaCotizacion?criterio=".$criterio;
 if( $cadena ){
 	$url.="&cadena=".$cadena;
 }
-
 if( $login ){
 	$url.="&login=".$login;
 }
-
 if( $seguimiento ){
 	$url.="&seguimiento=".$seguimiento;
 }
-
 $pagerLayout = new Doctrine_Pager_Layout(
       $pager,
       new Doctrine_Pager_Range_Sliding(array(
@@ -26,50 +23,48 @@ $pagerLayout = new Doctrine_Pager_Layout(
       )),
       url_for($url)."?page={%page_number}"
 );
-
 $pagerLayout->setTemplate('<a href="{%url}">{%page}</a> ');
 $pagerLayout->setSelectedTemplate('{%page}');
-
 
 $idsList = $pager->execute();
 
 $pagerLayout->display();
-
-
 ?>
 <br />
 <br />
-<form id="formProducto" name="formProducto" >
-<table class="tableList" width="800px" border="1" id="mainTable">
-	<tr>
-		<th width="57" scope="col">Consecutivo</th>
-		<th width="668" colspan="4" scope="col">Cotizaci&oacute;n</th>
-	</tr>
-	<?
-	
+<form id="formProducto" name="formProducto" method="post" action="#" >
+<table class="tableList" width="900px" border="1" id="mainTable">
+    <tr>
+        <th width="70" scope="col">Consecutivo</th>
+        <th width="660" colspan="4" scope="col">Cotizaci&oacute;n</th>
+        <th width="170" colspan="1" scope="col">Seguimiento</th>
+        
+    </tr>
+<?	
 	foreach( $cotizaciones as $cotizacion ){
 		$contacto = $cotizacion->getContacto();
 		$cliente = $contacto->getCliente(); 
-	?>
-	<tr>
-	  <td rowspan="2"  >
-        <?=link_to("C".$cotizacion->getCaConsecutivo(), "cotizaciones/consultaCotizacion?id=".$cotizacion->getCaIdcotizacion())?>
-          <?=$cotizacion->getCaFchanulado()?"<br />Anulada":""?>
-      </td>
-	  <td ><b>Fch.Cotizacion:</b><br />
-      <?=$cotizacion->getCaFchcreado()?></td>
-      <td ><b>Cliente:</b><br />
-      <?=$cliente?></td>
-      <td ><b>Contacto:</b><br />
-      <?=$contacto->getNombre()?></td>
-	  <td ><b>Vendedor:</b><br />
-        <?=$cotizacion->getCaUsuario()?></td>
-	</tr>
-    
-	<tr>
-	  <td colspan="4" >
-        <b>Trayectos:</b>
-	  	<table class="tableList" width="100%" border="1">
+?>
+    <tr>
+        <td rowspan="2"  >
+            <?=link_to("C".$cotizacion->getCaConsecutivo(), "cotizaciones/consultaCotizacion?id=".$cotizacion->getCaIdcotizacion())?>
+            <br>V-<?=$cotizacion->getCaVersion()?>
+            <?=$cotizacion->getCaFchanulado()?"<br />Anulada":""?>
+        </td>
+        <td ><b>Fch.Cotizacion:</b><br />
+            <?=$cotizacion->getCaFchcreado()?></td>
+        <td ><b>Cliente:</b><br />
+            <?=$cliente?></td>
+        <td ><b>Contacto:</b><br />
+            <?=$contacto->getNombre()?></td>
+        <td ><b>Vendedor:</b><br />
+            <?=$cotizacion->getCaUsuario()?></td>
+        <td colspan="2"></td>
+    </tr>
+    <tr>
+        <td colspan="6" >
+            <b>Trayectos:</b>
+            <table class="tableList" width="100%" border="1">
         <?
         $productos = $cotizacion->getCotProductos();
 
@@ -83,24 +78,24 @@ $pagerLayout->display();
                 $origen = $producto->getOrigen();
                 $destino = $producto->getDestino();
             ?>
-        <tr>
-          <td width="33%" class="listar" >
-            <?=$producto->getCaImpoexpo()?>
-            &raquo;			
-            <?=$producto->getCaTransporte()?>
-            &raquo;
-            <?=$producto->getCaModalidad()?> [<?=$producto->getCaProducto()?>]</td>
-          <td width="32%" class="listar"><?=$origen->getTrafico()." ".$origen->getCaCiudad()?>
-            &raquo;
-            <?=$destino->getTrafico()." ".$destino->getCaCiudad()?></td>
-            <td width="35%" class="listar">
-                <?
-                echo isset($estados[$producto->getCaEtapa()])?$estados[$producto->getCaEtapa()]:"";
+                <tr>
+                    <td width="300"  class="listar" >
+                        <?=$producto->getCaImpoexpo()?>
+                        &raquo;
+                        <?=$producto->getCaTransporte()?>
+                        &raquo;
+                        <?=$producto->getCaModalidad()?> [<?=$producto->getCaProducto()?>]</td>
+                    <td width="230" class="listar"><?=$origen->getTrafico()." ".$origen->getCaCiudad()?>
+                        &raquo;
+                        <?=$destino->getTrafico()." ".$destino->getCaCiudad()?></td>
+                    <td width="190" class="listar">
+                    <?
+                    echo isset($estados[$producto->getCaEtapa()])?$estados[$producto->getCaEtapa()]:"";
 
-                $seg = $producto->getUltSeguimiento();
-                if( $producto->getCaEtapa()==Cotizacion::EN_SEGUIMIENTO ){
-                ?>
-                <a style="cursor:pointer" onclick="window.open('<?=url_for("cotseguimientos/formSeguimiento?idcotizacion=".$cotizacion->getCaIdcotizacion()."&idproducto=".$producto->getCaIdproducto())?>')" title="Haga click aca para crear un seguimiento. Se abrira una nueva ventana.">
+                    $seg = $producto->getUltSeguimiento();
+                    if( $producto->getCaEtapa()==Cotizacion::EN_SEGUIMIENTO ){
+                    ?>
+                        <a style="cursor:pointer" onclick="window.open('<?=url_for("cotseguimientos/formSeguimiento?idcotizacion=".$cotizacion->getCaIdcotizacion()."&idproducto=".$producto->getCaIdproducto())?>')" title="Haga click aca para crear un seguimiento. Se abrira una nueva ventana.">
                         <?
                         if( $seg ){
                             echo image_tag("16x16/button_ok.gif")."  <br />Ult. seguimiento ".Utils::fechaMes($seg->getCaFchseguimiento());
@@ -108,15 +103,55 @@ $pagerLayout->display();
                             echo image_tag("16x16/button_cancel.gif")." <br /> Sin seguimientos";
                         }
                         ?>
-                </a>
-            </td><td width="50" style="width: 50px; padding: 15px">
-                <input type="checkbox" name="idproducto[]" value="<?=$producto->getCaIdproducto()?>">
+                        </a>
+                    </td>
+                    <td  width="182" style="width: 182px; background-color: #FFFFCC">
+                        <input type="hidden" name="idproducto[]" class="idproducto" value="<?=$producto->getCaIdproducto()?>" >
+                        <select name="etapa[]" id="etapa_<?=$producto->getCaIdproducto()?>" onchange="chseguimiento('<?=$producto->getCaIdproducto()?>')" >
+                                <option value="">...</option>
+                                <option value="APR">Aprobar</option>
+                                <option value="NAP">No Aprobar</option>
+                                <option value="SEG">Seguimiento</option>
+                        </select>
+                        <br/>
+                        <div id="NAP_<?=$producto->getCaIdproducto()?>" style="display:none;" >
+                            <select name="seguimiento[]" id="seg_<?=$producto->getCaIdproducto()?>" disabled style="width: 250px"  >
+                                <option value="Tarifa NO competitiva" title="Tarifa NO competitiva">Tarifa NO competitiva</option>
+                                <option value="No se realizo el embarque" title="No se realizo el embarque">No se realizo el embarque</option>
+                                <option value="Asignado a la competencia por preferencia del cliente" title="Asignado a la competencia por preferencia del cliente">Asignado a la competencia por preferencia del cliente</option>
+                                <option value="Cot. solicitada para efectos de presupuesto del cliente" title="Cot. solicitada para efectos de presupuesto del cliente">Cot. solicitada para efectos de presupuesto del cliente</option>
+                                <option value="Embarque asignado y anulado por el cliente" title="Embarque asignado y anulado por el cliente">Embarque asignado y anulado por el cliente</option>
+                                <option value="Cambio de Modalidad" title="Cambio de Modalidad">Cambio de Modalidad</option>
+                            </select>
+                        </div>
+                        <div id="SEG_<?=$producto->getCaIdproducto()?>" style="display:none">
+                            <textarea name="seguimiento[]" id="seg_<?=$producto->getCaIdproducto()?>" style="width: 100%"  rows="3" disabled></textarea>
+                            <br>
+                            <b>recordar seguimiento:</b>
+                                <input type="checkbox" id="prog_seguimiento_<?=$producto->getCaIdproducto()?>" name="prog_seguimiento[]" onclick="fchseg('<?=$producto->getCaIdproducto()?>')" disabled>
+                            
+                            <div id="fchaseg_<?=$producto->getCaIdproducto()?>" style="display: none">
+                                <input type="text" size="10" id="fchseguimiento_<?=$producto->getCaIdproducto()?>" name="fchseguimiento[]" disabled  />
+                                <script type="text/javascript">
+                                     new Ext.form.DateField({
+                                                     applyTo: 'fchseguimiento_<?=$producto->getCaIdproducto()?>',
+                                                     value: '',
+                                                     width: 100,
+                                                     format: 'Y-m-d',
+                                                     enable:false
+                                            });
+                                </script>
+                            </div>
+                        </div>
                 <?
                 }
+                else
+                {
+                    echo "</td><td>";
+                }
                 ?>
-            </td>
-          </tr>
-       
+                    </td>
+                </tr>
         <?
             }            
         }else{
@@ -124,8 +159,7 @@ $pagerLayout->display();
 
             <tr>
                 <td width="65%" class="listar" colspan="2" >
-                Sin trayectos</td>
-             
+                Sin trayectos</td>             
                 <td width="35%" class="listar">
                     <?
                     echo isset($estados[$cotizacion->getCaEtapa()])?$estados[$cotizacion->getCaEtapa()]:"";
@@ -146,38 +180,78 @@ $pagerLayout->display();
                     ?>
                 </td>
               </tr>
-
             <?
         }
         ?>
          </table>
      </td>
-    </tr>
-
-	
+    </tr>	
 	<?
 	}
 	if( count($cotizaciones)==0 ){
 	?>
-	<tr>
-		
-		<td  colspan="5" scope="col"><div align="center">No hay resultados</div></td>
+	<tr>		
+            <td  colspan="5" scope="col"><div align="center">No hay resultados</div></td>
 	</tr>
 	<?
 	}else
         {
         ?>
-        <tr><td colspan="4"></td><td align="right"><input value="Aprobar" type="button" onclick="validar()"  ></td></tr>
+        <tr>
+            <td colspan="5"></td>
+            <td align="right"><input value="Guardar Seguimientos" type="button" onclick="validar()" ></td>
+        </tr>
         <?
         }
-	?>
-	
-	
+	?>	
 </table>
 </form>
 <script type="text/javascript">
     function validar()
     {
+        objs=$(".idproducto");
+        $.each(objs, function(i,item){
+            if($("#etapa_"+item.value).val()!="")
+            {
+                 //$("#NAP_"+item.value+">input,#NAP_"+item.value+">textaera,#NAP_"+item.value+">select").attr("disabled",false);
+                 //$("#SEG_"+item.value+">input,#SEG_"+item.value+">textaera,#SEG_"+item.value+">select").attr("disabled",false);
+
+        $("#NAP_"+id+">*,#NAP_"+id+">*>*,#NAP_"+id+">*>*>*").attr("disabled",false);
+        $("#SEG_"+id+">*,#SEG_"+id+">*>*,#SEG_"+id+">*>*>*").attr("disabled",false);
+
+                 //$("#fchseguimiento_"+item.value).attr("disabled",false);
+
+                if($("#etapa_"+item.value).val()=="NAP")
+                {                    
+                    $("#SEG_"+item.value+">#seg_"+item.value).attr("disabled",true);
+                }
+                else if($("#etapa_"+item.value).val()=="SEG")
+                {                    
+                    $("#NAP_"+item.value+">#seg_"+item.value).attr("disabled",true);
+                }
+                else
+                {
+                    $("#NAP_"+item.value+">#seg_"+item.value).attr("disabled",true);
+                    $("#SEG_"+item.value+">#seg_"+item.value).attr("disabled",false);
+                }
+            }
+            else
+            {
+                item.disabled=true;
+                $("#etapa_"+item.value).attr("disabled",true)
+
+                if($("#etapa_"+item.value).val()=="NAP")
+                {
+                    $("#NAP_"+item.value+">input,#NAP_"+item.value+">textaera,#NAP_"+item.value+">select").attr("disabled",true);
+                }
+                else if($("#etapa_"+item.value).val()=="SEG")
+                {
+                    $("#SEG_"+item.value+">input,#SEG_"+item.value+">textaera,#SEG_"+item.value+">select").attr("disabled",true);
+                    $("#fchseguimiento_"+item.value).attr("disabled",true);
+                }
+            }
+        });
+        
         Ext.Ajax.request(
         {
             waitMsg: 'Guardando cambios...',
@@ -195,12 +269,57 @@ $pagerLayout->display();
            },
            failure: function(a,b){
                alert("Error:"+a.responseText.toString());
-
            }
-
         });
     }
+    function checked1(id)
+    {
+        val=$("#idp_"+id).attr("checked");
+        $("#seg_"+id).attr("disabled",!val);
+        if(val==true)
+            $("#seg_"+id).show();
+        else
+            $("#seg_"+id).hide();
+    }
+    function chseguimiento(id)
+    {
+        tipo=$("#etapa_"+id).attr("value");
+        $("#NAP_"+id).hide();
+        //$("#NAP_"+id+">input,#NAP_"+id+">textaera,#NAP_"+id+">select").attr("disabled",true);
+        $("#NAP_"+id+">*,#NAP_"+id+">*>*,#NAP_"+id+">*>*>*").attr("disabled",true);
+
+        $("#SEG_"+id).hide();
+        //$("#SEG_"+id+">input,#SEG_"+id+">textaera,#SEG_"+id+">select").attr("disabled",true);
+        $("#SEG_"+id+">*,#SEG_"+id+">*>*,#SEG_"+id+">*>*>*").attr("disabled",true);
+        //$("#fchseguimiento_"+id).attr("disabled",true);
+        
+        if(tipo=="NAP")
+        {            
+            $("#NAP_"+id).show();
+            //$("#NAP_"+id+">input,#NAP_"+id+">textaera,#NAP_"+id+">select").attr("disabled",false);
+            $("#NAP_"+id+">*,#NAP_"+id+">*>*,#NAP_"+id+">*>*>*").attr("disabled",false);
+        }
+        else if(tipo=="SEG")
+        {         
+            //seg=$("#SEG_"+id);
+            $("#SEG_"+id).show();
+            //seg.show();
+            //seg.children().attr("disabled",false);
+            //$("#fchseguimiento_"+id).attr("disabled",false);
+            $("#SEG_"+id+">*,#SEG_"+id+">*>*,#SEG_"+id+">*>*>*").attr("disabled",false);
+        }
+    }
+    function fchseg(id)
+    {
+        val=$("#prog_seguimiento_"+id).attr("checked");
+        if(val==true)
+        {
+            $("#fchaseg_"+id).show();      
+        }
+        else
+        {
+            $("#fchaseg_"+id).hide();      
+        }
+    }
 </script>
-<br />
-<br />
 </div>
