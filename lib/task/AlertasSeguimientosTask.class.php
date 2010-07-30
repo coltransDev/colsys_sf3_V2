e4cd2d746944c503389c860f63d250f7c08b5070
Mauricio A Quinche <maquinche@coltrans.com.co>
2010-07-30 16:10:44 +0000

    [cotseguimientos] Version 1, alertas de seguimientos a los comerciales

---
+++ lib/task/AlertasSeguimientosTask.class.php
@@ -0,0 +1,34 @@
+<?php
+
+class AlertaSeguimientosTask extends sfDoctrineBaseTask
+{
+  protected function configure()
+  {
+    $this->namespace        = 'colsys';
+    $this->name             = 'alertasSeguimientos';
+    $this->briefDescription = 'Genera Reporte Mensual de las Cotizaciones que estan en seguimiento';
+    $this->detailedDescription = <<<EOF
+The [alertaSeguimientos|INFO] task does things.
+Call it with:
+
+  [php symfony alertaSeguimientos|INFO]
+EOF;
+    // add arguments here, like the following:
+    //$this->addArgument('application', sfCommandArgument::REQUIRED, 'The application name');
+    // add options here, like the following:
+    //$this->addOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev');
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    $this->configuration = ProjectConfiguration::getApplicationConfiguration('colsys', 'cli', true );
+	
+	// Borra las dos líneas siguientes si no utilizas una base de datos
+	$databaseManager = new sfDatabaseManager($this->configuration);
+	$databaseManager->loadConfiguration();
+
+	sfContext::createInstance($this->configuration)->dispatch();	
+	
+	echo sfContext::getInstance()->getController()->getPresentationFor( 'cotseguimientos', 'reporteSeguimientosEmail');
+  }
+}
