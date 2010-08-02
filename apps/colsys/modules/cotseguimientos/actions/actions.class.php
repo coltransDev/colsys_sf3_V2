e66d0b8a0a0393fd775eca4647bdca3479692ebb
Mauricio A Quinche <maquinche@coltrans.com.co>
2010-08-02 19:09:44 +0000

    [cotseguimientos] Niveles de acceso en Estadisticas

--- apps/colsys/modules/cotseguimientos/actions/actions.class.php
+++ apps/colsys/modules/cotseguimientos/actions/actions.class.php
@@ -20,7 +20,7 @@ class cotseguimientosActions extends sfActions
 	public function executeIndex($request)
 	{
 		$this->nivel = $this->getUser()->getNivelAcceso( cotseguimientosActions::RUTINA );
-
+        $this->vendedor=$this->getUser()->getUserId();
 		if( $this->nivel==-1 ){
 		//	$this->forward404();
 		}
@@ -53,10 +53,21 @@ class cotseguimientosActions extends sfActions
 		$fechaInicial = Utils::parseDate($request->getParameter("fechaInicial"));
 		$fechaFinal = Utils::parseDate($request->getParameter("fechaFinal"));
 
-        $checkboxVendedor = $request->getParameter( "checkboxVendedor" );
-		$checkboxSucursal = $request->getParameter( "checkboxSucursal" );
-		$this->login = $request->getParameter( "login" );
+        $this->nivel = $this->getUser()->getNivelAcceso( cotseguimientosActions::RUTINA );
 
+        if($this->nivel=="1")
+        {
+            $checkboxVendedor = $request->getParameter( "checkboxVendedor" );
+            $checkboxSucursal = $request->getParameter( "checkboxSucursal" );
+        }
+        else
+        {
+            $checkboxVendedor=true;
+            $checkboxSucursal=false;
+        }
+        
+		$this->login = $request->getParameter( "login" );
+        //$this->login="bdelatorre";
 		$this->usuario = Doctrine::getTable("Usuario")->find( $this->login );
 
 		$q = Doctrine_Query::create()
@@ -249,151 +260,147 @@ class cotseguimientosActions extends sfActions
             $this->redirect( "cotseguimientos/verSeguimiento?idcotizacion=".$cotizacion->getCaIdcotizacion() );
 	}
 
-        public function executeAprobarSeguimiento( $param ){
-            $idproducto=$param["idproducto"];
-            $etapa=$param["etapa"];
-            $seguimientos=(isset($param["seguimiento"])?$param["seguimiento"]:"");
-            //$prog_seguimiento=(isset($param["prog_seguimiento"])?$param["prog_seguimiento"]:"");
-            $fchseguimiento=(isset($param["fchseguimiento"])?$param["fchseguimiento"]:"");
+    public function executeAprobarSeguimiento( $param ){
+        $idproducto=$param["idproducto"];
+        $etapa=$param["etapa"];
+        $seguimientos=(isset($param["seguimiento"])?$param["seguimiento"]:"");
+        //$prog_seguimiento=(isset($param["prog_seguimiento"])?$param["prog_seguimiento"]:"");
+        $fchseguimiento=(isset($param["fchseguimiento"])?$param["fchseguimiento"]:"");
 
-            if( $idproducto ){
-                $producto = Doctrine::gettable("CotProducto")->find( $idproducto );
-                $this->forward404Unless( $producto );
+        if( $idproducto ){
+            $producto = Doctrine::gettable("CotProducto")->find( $idproducto );
+            $this->forward404Unless( $producto );
 
-                if( $producto->getCaIdtarea() ){
-                    $tarea  =  Doctrine::gettable("NotTarea")->find( $producto->getCaIdtarea() );
-                    $tarea->setCaFchterminada( date("Y-m-d H:i:s") );
-                    $tarea->save();
-                }
+            if( $producto->getCaIdtarea() ){
+                $tarea  =  Doctrine::gettable("NotTarea")->find( $producto->getCaIdtarea() );
+                $tarea->setCaFchterminada( date("Y-m-d H:i:s") );
+                $tarea->save();
             }
+        }
 
-            $cotizacion = Doctrine::gettable("Cotizacion")->find( $producto->getCaIdcotizacion() );
-            $this->forward404Unless( $cotizacion );
+        $cotizacion = Doctrine::gettable("Cotizacion")->find( $producto->getCaIdcotizacion() );
+        $this->forward404Unless( $cotizacion );
 
-            $seguimiento = new CotSeguimiento();
+        $seguimiento = new CotSeguimiento();
+        if( $idproducto ){
+            $seguimiento->setCaIdproducto( $idproducto );
+            $producto->setCaEtapa( $etapa );
+            $producto->save();
+        }
+
+        $seguimiento->setCaLogin( $this->getUser()->getUserId() );
+        $seguimiento->setCaFchseguimiento( date("Y-m-d H:i:s") );
+        $seguimiento->setCaSeguimiento( $seguimientos );
+        $seguimiento->setCaEtapa( $etapa );
+        $seguimiento->save();
+
+        if( $fchseguimiento )
+        {
+            $titulo = "Seguimiento Cotización ".$cotizacion->getCaConsecutivo()." ".$cotizacion->getCliente()->getCaCompania()."";
+            $texto = "Ha programado un seguimiento para una cotización, por favor haga click en el link para realizar esta tarea";
+            $tarea = new NotTarea();
+            $tarea->setCaUrl( "/cotseguimientos/verSeguimiento/idcotizacion/".$cotizacion->getCaIdcotizacion() );
+            $tarea->setCaIdlistatarea( 7 );
+
+            $tarea->setCaFchvencimiento( $fchseguimiento." 23:59:59" );
+            $tarea->setCaFchvisible( $fchseguimiento." 00:00:00" );
+            $tarea->setCaUsucreado( $this->getUser()->getUserId() );
+            $tarea->setCaTitulo( $titulo );
+            $tarea->setCaTexto( $texto );
+            $tarea->save();
+            $loginsAsignaciones = array( $this->getUser()->getUserId(), $cotizacion->getCaUsuario() );
+            $loginsAsignaciones = array_unique( $loginsAsignaciones );
+            $tarea->setAsignaciones( $loginsAsignaciones );
+
             if( $idproducto ){
-                $seguimiento->setCaIdproducto( $idproducto );
-                $producto->setCaEtapa( $etapa );
+                $producto->setCaIdtarea( $tarea->getCaIdtarea() );
                 $producto->save();
             }
-
-            $seguimiento->setCaLogin( $this->getUser()->getUserId() );
-            $seguimiento->setCaFchseguimiento( date("Y-m-d H:i:s") );
-            $seguimiento->setCaSeguimiento( $seguimientos );
-            $seguimiento->setCaEtapa( $etapa );
-            $seguimiento->save();
-
-            if( $fchseguimiento )
-            {
-                $titulo = "Seguimiento Cotización ".$cotizacion->getCaConsecutivo()." ".$cotizacion->getCliente()->getCaCompania()."";
-                $texto = "Ha programado un seguimiento para una cotización, por favor haga click en el link para realizar esta tarea";
-                $tarea = new NotTarea();
-                $tarea->setCaUrl( "/cotseguimientos/verSeguimiento/idcotizacion/".$cotizacion->getCaIdcotizacion() );
-                $tarea->setCaIdlistatarea( 7 );
-
-                $tarea->setCaFchvencimiento( $fchseguimiento." 23:59:59" );
-                $tarea->setCaFchvisible( $fchseguimiento." 00:00:00" );
-                $tarea->setCaUsucreado( $this->getUser()->getUserId() );
-                $tarea->setCaTitulo( $titulo );
-                $tarea->setCaTexto( $texto );
-                $tarea->save();
-                $loginsAsignaciones = array( $this->getUser()->getUserId(), $cotizacion->getCaUsuario() );
-                $loginsAsignaciones = array_unique( $loginsAsignaciones );
-                $tarea->setAsignaciones( $loginsAsignaciones );
-
-                if( $idproducto ){
-                    $producto->setCaIdtarea( $tarea->getCaIdtarea() );
-                    $producto->save();
-                }
-            }
+        }
 	}
 
-        public function executeAprobarSeguimientos( $request){
-            $idproductos=$request->getParameter("idproducto");
-            $etapa=$request->getParameter("etapa");
-            $seguimientos=$request->getParameter("seguimiento");
-            //$prog_seguimientos=$request->getParameter("prog_seguimiento");
-            $fchseguimientos=$request->getParameter("fchseguimiento");
-            for($i=0;$i<count($idproductos);$i++)
-            {
-                $param["idproducto"]=$idproductos[$i];
-                $param["etapa"]=$etapa[$i];
-                if($seguimientos[$i]!="")
-                    $param["seguimiento"]=$seguimientos[$i];
-                else
-                    $param["seguimiento"]="";
-                //if($prog_seguimientos[$i]!="")
-                    //$param["prog_seguimiento"]=$prog_seguimientos[$i];
-                if($fchseguimientos[$i]!="")
-                    $param["fchseguimiento"]=$fchseguimientos[$i];
-                else
-                    $param["fchseguimiento"]="";
-                //echo "<pre>";print_r($param);echo "</pre>";
-                $this->executeAprobarSeguimiento( $param );
-            }
-            $this->responseArray = array("success"=>true);
-            $this->setTemplate("responseTemplate");
-        }
-
-        public function executeReporteSeguimientosEmail( $request)
+    public function executeAprobarSeguimientos( $request){
+        $idproductos=$request->getParameter("idproducto");
+        $etapa=$request->getParameter("etapa");
+        $seguimientos=$request->getParameter("seguimiento");
+        //$prog_seguimientos=$request->getParameter("prog_seguimiento");
+        $fchseguimientos=$request->getParameter("fchseguimiento");
+        for($i=0;$i<count($idproductos);$i++)
         {
-
-            $ultimo_dia = mktime(0, 0, 0, date("m"), 0, 2010);
-            $last_day=date("Y-m-d",$ultimo_dia);
-            $first_day=date("Y-m-01",$ultimo_dia);
-            //echo $first_day."-".$last_day;
-
-
-            $seguimientos = Doctrine_Query::create()
-                ->select("cli.ca_compania,p.ca_idproducto,c.ca_consecutivo,p.ca_transporte,o.ca_ciudad,d.ca_ciudad,c.ca_usuario,u.ca_email")
-                ->from("CotProducto p")
-                ->innerJoin("p.Origen o")
-                ->innerJoin("p.Destino d")
-                ->innerJoin("p.Cotizacion c")
-                ->innerJoin("c.Contacto con")
-                ->innerJoin("con.Cliente cli ON cli.ca_idcliente=con.ca_idcliente")
-                ->innerJoin("c.Usuario u")
-                ->leftJoin("p.CotSeguimiento s" )
-                ->addWhere("c.ca_fchcreado BETWEEN ? AND ? AND p.ca_etapa IS NOT NULL", array($first_day, $last_day))
-                ->addWhere("(s.ca_etapa<>'NAP' and s.ca_etapa<>'APR' or s.ca_etapa is null)" )
-                ->orderBy("c.ca_usuario")
-                ->addOrderBy("c.ca_consecutivo DESC")
-                ->addOrderBy("p.ca_idproducto ")
-                ->setHydrationMode(Doctrine::HYDRATE_SCALAR)
-                ->execute();
-            $this->emails=array();
-            foreach($seguimientos as $seguimiento)
-            {
-                if(!isset($this->emails[$seguimiento["c_ca_usuario"]]))
-                {
-                        $this->emails[$seguimiento["c_ca_usuario"]]["html"]="<tr bgcolor='#EAEAEA'><td colspan='4'>Usuario : ".$seguimiento["c_ca_usuario"]."</td></tr><tr bgcolor='#EAEAEA' ><td>Cotizaci&oacute;n</td><td>Cliente</td><td>Transporte</td><td>Trayecto</td></tr>";
-                        $this->emails[$seguimiento["c_ca_usuario"]]["email"]=$seguimiento["u_ca_email"];
-                }
-                $this->emails[$seguimiento["c_ca_usuario"]]["html"].="<tr><td>".$seguimiento["c_ca_consecutivo"]."</td><td>".$seguimiento["cli_ca_compania"]."</td><td>".$seguimiento["p_ca_transporte"]."</td><td>".$seguimiento["o_ca_ciudad"]."-".$seguimiento["d_ca_ciudad"]."::".$seguimiento["p_ca_idproducto"]."</td></tr>";
-            }
-            $keys= array_keys($this->emails);
-
-            foreach($keys as $key)
-            {
-                $this->emails[$key]["html"]="<table width='90%' cellspacing='1' border='1'>".$this->emails[$key]["html"]."</table>";
-                $email = new Email();
-                $email->setCaUsuenvio( "Administrador" );
-                $email->setCaTipo( "SDNList Compair" );
-                $email->setCaIdcaso( "1" );
-                $email->setCaFrom( "admin@coltrans.com.co" );
-                $email->setCaFromname( "Administrador Sistema Colsys" );
-                $email->setCaReplyto( "admin@coltrans.com.co" );
-                $email->setCaCc( "maquinche@coltrans.com.co" );
-                $email->setCaSubject( "Seguimientos de Cotizaciones de $first_day a $last_day" );
-                $email->setCaBodyhtml( $this->emails[$key]["html"] );
-                $email->addTo( $this->emails[$key]["email"] );
-                //$email->addTo( "maquinche@coltrans.com.co" );
-                $email->save(); //guarda el cuerpo del mensaje
-                $email->send(); //envia el mensaje de correo
-                //break;
-            }
-//            print_r($this->emails);
-    //$email->save();  
+            $param["idproducto"]=$idproductos[$i];
+            $param["etapa"]=$etapa[$i];
+            if($seguimientos[$i]!="")
+                $param["seguimiento"]=$seguimientos[$i];
+            else
+                $param["seguimiento"]="";
+            //if($prog_seguimientos[$i]!="")
+                //$param["prog_seguimiento"]=$prog_seguimientos[$i];
+            if($fchseguimientos[$i]!="")
+                $param["fchseguimiento"]=$fchseguimientos[$i];
+            else
+                $param["fchseguimiento"]="";
+            //echo "<pre>";print_r($param);echo "</pre>";
+            $this->executeAprobarSeguimiento( $param );
         }
+        $this->responseArray = array("success"=>true);
+        $this->setTemplate("responseTemplate");
+    }
+
+    public function executeReporteSeguimientosEmail( $request)
+    {
+        $ultimo_dia = mktime(0, 0, 0, date("m"), 0, 2010);
+        $last_day=date("Y-m-d",$ultimo_dia);
+        $first_day=date("Y-m-01",$ultimo_dia);
+
+        $seguimientos = Doctrine_Query::create()
+            ->select("cli.ca_compania,p.ca_idproducto,c.ca_consecutivo,p.ca_transporte,o.ca_ciudad,d.ca_ciudad,c.ca_usuario,u.ca_email")
+            ->from("CotProducto p")
+            ->innerJoin("p.Origen o")
+            ->innerJoin("p.Destino d")
+            ->innerJoin("p.Cotizacion c")
+            ->innerJoin("c.Contacto con")
+            ->innerJoin("con.Cliente cli ON cli.ca_idcliente=con.ca_idcliente")
+            ->innerJoin("c.Usuario u")
+            ->leftJoin("p.CotSeguimiento s" )
+            ->addWhere("c.ca_fchcreado BETWEEN ? AND ? AND p.ca_etapa IS NOT NULL", array($first_day, $last_day))
+            ->addWhere("(s.ca_etapa<>'NAP' and s.ca_etapa<>'APR' or s.ca_etapa is null)" )
+            ->orderBy("c.ca_usuario")
+            ->addOrderBy("c.ca_consecutivo DESC")
+            ->addOrderBy("p.ca_idproducto ")
+            ->setHydrationMode(Doctrine::HYDRATE_SCALAR)
+            ->execute();
+        $this->emails=array();
+        foreach($seguimientos as $seguimiento)
+        {
+            if(!isset($this->emails[$seguimiento["c_ca_usuario"]]))
+            {
+                    $this->emails[$seguimiento["c_ca_usuario"]]["html"]="<tr bgcolor='#EAEAEA'><td colspan='4'>Usuario : ".$seguimiento["c_ca_usuario"]."</td></tr><tr bgcolor='#EAEAEA' ><td>Cotizaci&oacute;n</td><td>Cliente</td><td>Transporte</td><td>Trayecto</td></tr>";
+                    $this->emails[$seguimiento["c_ca_usuario"]]["email"]=$seguimiento["u_ca_email"];
+            }
+            $this->emails[$seguimiento["c_ca_usuario"]]["html"].="<tr><td>".$seguimiento["c_ca_consecutivo"]."</td><td>".$seguimiento["cli_ca_compania"]."</td><td>".$seguimiento["p_ca_transporte"]."</td><td>".$seguimiento["o_ca_ciudad"]."-".$seguimiento["d_ca_ciudad"]."::".$seguimiento["p_ca_idproducto"]."</td></tr>";
+        }
+        $keys= array_keys($this->emails);
+
+        foreach($keys as $key)
+        {
+            $this->emails[$key]["html"]="<table width='90%' cellspacing='1' border='1'>".$this->emails[$key]["html"]."</table>";
+            $email = new Email();
+            $email->setCaUsuenvio( "Administrador" );
+            $email->setCaTipo( "SDNList Compair" );
+            $email->setCaIdcaso( "1" );
+            $email->setCaFrom( "admin@coltrans.com.co" );
+            $email->setCaFromname( "Administrador Sistema Colsys" );
+            $email->setCaReplyto( "admin@coltrans.com.co" );
+            $email->setCaCc( "maquinche@coltrans.com.co" );
+            $email->setCaSubject( "Seguimientos de Cotizaciones de $first_day a $last_day" );
+            $email->setCaBodyhtml( $this->emails[$key]["html"] );
+            $email->addTo( $this->emails[$key]["email"] );
+            //$email->addTo( "maquinche@coltrans.com.co" );
+            $email->save(); //guarda el cuerpo del mensaje
+            $email->send(); //envia el mensaje de correo
+            //break;
+        }
+
+    }
 }
 ?>
