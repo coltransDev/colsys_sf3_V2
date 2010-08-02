5aff779a3e28094ad14e2b8edbe9af19456d99d2
Andres Botero <abotero@coltrans.com.co>
2010-08-02 17:23:16 +0000

    [ids] Verificacion de proveedores lista clinton

---
+++ apps/colsys/modules/ids/templates/verificarListaClintonSuccess.php
@@ -0,0 +1,55 @@
+<?php
+/* 
+ *  This file is part of the Colsys Project.
+ * 
+ *  (c) Coltrans S.A. - Colmas Ltda.
+ */
+$stmt = $sf_data->getRaw("stmt");
+
+
+?>
+<div class="content" align="center">
+   La siguiente es una relación de <b>SIMILITUDES</b>, halladas entre el cliente consultado y la lista Clinton publicada el <?=$fechaActualizacion?>.
+   <br />Tener en cuenta que se compara Número de Nit y Razón Social.<br /><br />
+
+    <table align="center" class="tableList">
+
+    <?
+    $i= 0;
+    while( $row = $stmt->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT) ){
+        //print_r( $row );
+        $keys = array_keys( $row );
+        if( $i++==0 ){
+            ?>
+            <tr>
+            <?
+            foreach( $keys as $key ){
+            ?>
+                <th><?=ucfirst(str_replace("_", " ",str_replace("ca_", "",$key)))?></th>
+            <?
+            }
+            ?>
+            </tr>
+            <?
+        }
+        ?>
+        <tr>
+            <?
+            foreach( $keys as $key ){
+            ?>
+                <td><?=$row[$key]?></td>
+            <?
+            }
+        ?>
+        </tr>
+        <?
+
+    }
+
+    ?>
+    </table>
+
+   <br>
+   <input type="button" value="Regresar" class="button" onClick="document.location='<?=url_for("ids/verIds?modo=".$modo."&id=".$ids->getCaId())?>'" />
+
+</div>
