2725151aafe1ae4be4e6aef114419d94e77c3986
Andres Botero <abotero@coltrans.com.co>
2010-08-02 17:22:10 +0000

    [helpdesk] Al visualizar el ticket se veian las entidades html

--- apps/colsys/modules/helpdesk/templates/_listaRespuestasTicket.php
+++ apps/colsys/modules/helpdesk/templates/_listaRespuestasTicket.php
@@ -1,10 +1,11 @@
 <?
+$responses = $sf_data->getRaw("responses");
 foreach( $responses as $response ){
 	echo "<b>".($response->getUsuario()?$response->getUsuario()->getCaNombre():$response->getCaLogin())."</b> ".Utils::fechaMes($response->getCaCreatedat());
 	?>
 	<hr />	
 	<?
-	echo Utils::replace($response->getCaText());
+	echo str_replace("\n", "<br />",$response->getCaText());
 	?>
 	<br /><br /><br />
 <?	
