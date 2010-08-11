8f39b43bb20a9c7cbbda917e430c555aac4b7e23
Mauricio A Quinche <maquinche@coltrans.com.co>
2010-08-11 19:51:32 +0000

    [cotseguimientos] Se genero un reporte de auditoria de seguimientos de cotizaciones.

--- apps/colsys/modules/cotseguimientos/actions/actions.class.php
+++ apps/colsys/modules/cotseguimientos/actions/actions.class.php
@@ -11,6 +11,7 @@
 class cotseguimientosActions extends sfActions
 {
 	const RUTINA = 19;
+	const RUTINA_AUDITORIA = 95;
 	/**
 	* Executes index action
 	*
@@ -28,16 +29,42 @@ class cotseguimientosActions extends sfActions
 			$origenes = Doctrine::getTable("TraficoUsers")
                                     ->createQuery("tu")
                                     ->select("tu.*")
-									->where("tu.ca_login=?",array($this->getUser()->getUserId()) )
+									->where("tu.ca_login=? and tu.ca_impo=true",array($this->getUser()->getUserId()) )
                                     ->execute();
 
 			$this->pais_origen="";
-			foreach($origenes as $origen)
+			if($origenes)
 			{
-				$this->pais_origen.=($this->pais_origen!="")?",".$origen->getCaIdtrafico():$origen->getCaIdtrafico();
+				foreach($origenes as $origen)
+				{
+					$this->pais_origen.=($this->pais_origen!="")?",".$origen->getCaIdtrafico():$origen->getCaIdtrafico();
 
+				}
 			}
-			$this->pais_destino="CO-057";
+			if($this->pais_origen=="")
+			{
+				$this->pais_origen="CO-057";
+			}
+
+			$destinos = Doctrine::getTable("TraficoUsers")
+                                    ->createQuery("tu")
+                                    ->select("tu.*")
+									->where("tu.ca_login=? and tu.ca_expo=true",array($this->getUser()->getUserId()) )
+                                    ->execute();
+
+			$this->pais_destino="";
+			if($destinos)
+			{
+				foreach($destinos as $destino)
+				{
+					$this->pais_destino.=($this->pais_destino!="")?",".$destino->getCaIdtrafico():$destino->getCaIdtrafico();
+
+				}
+			}
+			if($this->pais_destino=="")
+			{
+				$this->pais_destino="CO-057";
+			}
 		}
 
 		$this->usuarios = Doctrine::getTable("Usuario")
@@ -432,5 +459,112 @@ class cotseguimientosActions extends sfActions
             //break;
         }
     }
+
+
+	public function executeBusquedaAuditoria($request)
+	{
+		$this->nivel = $this->getUser()->getNivelAcceso( cotseguimientosActions::RUTINA_AUDITORIA );
+        
+		if( $this->nivel<=-1 ){
+			$this->forward404();
+		}		
+		$this->vendedor=$this->getUser()->getUserId();
+		$this->usuarios = Doctrine::getTable("Usuario")
+                                    ->createQuery("u")
+                                    ->select("u.*")
+                                    ->innerJoin("u.Cotizacion")
+                                    ->distinct()
+                                    ->orderBy("u.ca_nombre")
+                                    ->execute();
+
+		$this->sucursales = Doctrine::getTable("Sucursal")
+                                    ->createQuery("s")
+                                    ->orderBy("s.ca_nombre")
+                                    ->execute();
+		$this->estados = ParametroTable::retrieveByCaso( "CU074" );
+	}
+
+	public function executeAuditoria($request)
+	{
+
+		$this->fechaInicial = Utils::parseDate($request->getParameter("fechaInicial"));
+		$this->fechaFinal = Utils::parseDate($request->getParameter("fechaFinal"));
+		$checkboxVendedor = $request->getParameter( "checkboxVendedor" );
+        $checkboxSucursal = $request->getParameter( "checkboxSucursal" );
+		$this->login = $request->getParameter( "login" );
+		$this->usuario = Doctrine::getTable("Usuario")->find( $this->login );
+
+ 
+	//recuperamos el singleton de la conexión
+		$con = Doctrine_Manager::getInstance()->connection();
+		//ejecutamos la consulta
+		$sql="select c.ca_usuario,c.ca_consecutivo,u.ca_idsucursal,to_char(c.ca_fchcreado,'yyyy-mm-dd') as ca_fchcreado from tb_cotizaciones c
+			INNER JOIN control.tb_usuarios u ON c.ca_usuario = u.ca_login
+			WHERE
+			( c.ca_idcotizacion IN ( SELECT ca_idcotizacion FROM tb_cotProductos  WHERE ca_etapa='SEG' )
+			and  c.ca_idcotizacion  NOT IN ( SELECT ca_idcotizacion FROM tb_cotProductos  WHERE ca_etapa<>'SEG' )
+			or  c.ca_idcotizacion  IN ( SELECT ca_idcotizacion FROM tb_cotProductos  )  )
+			and 
+			( ca_usuanulado is  null or ca_usuanulado='' ) and c.ca_fchcreado between '".$this->fechaInicial."' and '".$this->fechaFinal."'
+		";
+		if( $checkboxVendedor ){
+            $sql.=" and c.ca_usuario = '". $this->login."'" ;
+		}
+
+		if( $checkboxSucursal ){
+			$this->sucursal = $request->getParameter( "sucursal_est" );
+			$sql.=" and u.ca_idsucursal = '". $this->sucursal."'" ;
+            //$q->addWhere("u.ca_idsucursal = ?", $this->sucursal );
+		}else{
+			$this->sucursal = "";
+		}
+		$sql.=" and c.ca_idcotizacion NOT IN ( SELECT s.ca_idcotizacion FROM tb_cotseguimientos s WHERE s.ca_idcotizacion IS NOT null OR s.ca_idcotizacion>0 )";
+
+		$sql.=" order by c.ca_fchcreado ";
+		$st = $con->execute($sql);
+	//recuperamos las tuplas de resultados
+		$this->seguimientos = $st->fetchAll();
+		$this->numcotizaciones=count($this->seguimientos);
+//	print_r($rs);
+//exit;
+/*				->where(" ( c.ca_idcotizacion IN ( SELECT p.ca_idcotizacion FROM CotProducto p WHERE p.ca_etapa='SEG' ) )
+					and ( c.ca_idcotizacion  NOT IN ( SELECT p1.ca_idcotizacion FROM CotProducto p1 WHERE p1.ca_etapa<>'SEG' ) )
+					or ( c.ca_idcotizacion  IN ( SELECT p2.ca_idcotizacion FROM CotProducto p2)  ) ")
+ *
+ */
+/*		$q = Doctrine_Query::create()
+				->select("c.ca_usuario,c.ca_consecutivo,u.ca_idsucursal")
+				->from("Cotizacion c")
+				->innerJoin("c.Usuario u")
+				->where("  ( c.ca_idcotizacion IN ( SELECT p.ca_idcotizacion FROM tb_cotProductos p WHERE p.ca_etapa='SEG' )
+					and  t.ca_idcotizacion  NOT IN ( SELECT p1.ca_idcotizacion FROM tb_cotProductos p1 WHERE p1.ca_etapa<>'SEG' )
+					or  t.ca_idcotizacion  IN ( SELECT p2.ca_idcotizacion FROM CotProducto p2 )  )  ")
+//				->where("(ca_usuanulado is  null or ca_usuanulado='') and c.ca_fchcreado between ? and ?", array($this->fechaInicial,$this->fechaFinal));
+//				->where(" c.ca_idcotizacion IN ( SELECT p.ca_idcotizacion FROM CotProducto p WHERE p.ca_etapa='SEG' )")
+//				->addWhere("c.ca_idcotizacion NOT IN ( SELECT p1.ca_idcotizacion FROM CotProducto p1 WHERE p1.ca_etapa<>'SEG' )")
+//				->orWhere("c.ca_idcotizacion NOT IN ( SELECT p2.ca_idcotizacion FROM CotProducto p2  ) ");
+				->addWhere(" ( ca_usuanulado is  null or ca_usuanulado='' ) and c.ca_fchcreado between ? and ?", array($this->fechaInicial,$this->fechaFinal));
+
+		if( $checkboxVendedor ){
+            $q->addWhere("c.ca_usuario = ?", $this->login );
+		}
+
+		if( $checkboxSucursal ){
+			$this->sucursal = $request->getParameter( "sucursal_est" );
+            $q->addWhere("u.ca_idsucursal = ?", $this->sucursal );
+		}else{
+			$this->sucursal = "";
+		}
+
+		$this->seguimientos = $q->addWhere("c.ca_idcotizacion NOT IN ( SELECT s.ca_idcotizacion FROM CotSeguimiento s WHERE s.ca_idcotizacion IS NOT null OR s.ca_idcotizacion>0 )")
+                        ->setHydrationMode(Doctrine::HYDRATE_SCALAR)
+                        ->execute();
+		echo(count($this->seguimientos));
+		$this->numcotizaciones=count($this->seguimientos);
+ *
+ */
+	}
+
+
 }
 ?>
