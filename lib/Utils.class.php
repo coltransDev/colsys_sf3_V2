f24505c1426138d5c16f5522e025e9e7e53dd61c
Andrea L. Ramirez <alramirez@coltrans.com.co>
2010-08-24 16:55:40 +0000

    Algunas aplicaciones se convirtieron en plugins

--- lib/Utils.class.php
+++ lib/Utils.class.php
@@ -319,5 +319,16 @@ class Utils{
         }
         return $ip;
     }
+
+    static public function slugify($text){
+        // replace all non letters or digits by -
+        $text = preg_replace('/\W+/', '-', $text);
+
+        // trim and lowercase
+        $text = strtolower(trim($text, '-'));
+
+        return $text;
+    }
+
 }
 ?>
