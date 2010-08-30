e6793d7c537eaae83de98c9625010ceeb91f6492
Mauricio A Quinche <maquinche@coltrans.com.co>
2010-08-30 19:06:29 +0000

    [reportesNeg] nuevo executeFormEventosNew

--- apps/colsys/modules/ids/actions/actions.class.php
+++ apps/colsys/modules/ids/actions/actions.class.php
@@ -976,6 +976,107 @@ class idsActions extends sfActions
     }
 
 
+    public function executeFormEventosNew(sfWebRequest $request){
+        //Se debe verificar que la referencia exista y determinar el proveedor.
+
+        $this->modo=$request->getParameter("modo");
+        $this->form = new NuevoEventoForm();
+
+        if( $request->getParameter("idevento") ){
+            $evento = Doctrine::getTable("IdsEvento")->find( $request->getParameter("idevento") );
+            $this->forward404Unless($evento);
+
+        }else{
+            $evento = new IdsEvento();
+        }
+
+
+        $this->idreporte=$request->getParameter("idreporte");
+        if( $this->modo ){ //Esta ingresando desde la maestra de proveedores
+
+
+            if( $request->getParameter("idevento") ){
+                $this->ids = $evento->getIds();
+            }else{
+                $this->ids = Doctrine::getTable("Ids")->find($request->getParameter("id"));
+            }
+            $this->url = "/ids/verIds?modo=".$this->modo."&id=".$this->ids->getCaId();
+            $numreferencia = "";
+            
+
+        }else{
+
+            if( $this->idreporte ){ // Esta ingresando desde el reporte
+                $this->reporte = Doctrine::getTable("Reporte")->find( $this->idreporte );
+                $this->forward404Unless( $this->reporte );
+                $this->agente = $this->reporte->getIdsAgente();
+                $this->url = "/reportesNeg/consultaReporte/id/".$this->idreporte."/impoexpo/".$this->reporte->getCaImpoexpo()."/modo/".$this->reporte->getCaTransporte();
+
+                $numreferencia = $this->reporte->getCaConsecutivo();
+            }else{// Esta ingresando desde la referencia
+                $numreferencia = str_replace("_",".",$request->getParameter("referencia"));
+                $this->forward404Unless(  $numreferencia );
+
+                $idproveedores = array();
+
+                if( substr($numreferencia,0,1)=="4" || substr($numreferencia,0,1)=="5" ){
+                    $referencia = Doctrine::getTable("InoMaestraSea")->find($numreferencia);
+                    $linea = $referencia->getCaIdlinea();
+
+                    $idproveedores[] = $linea;
+
+                    $this->url = "/colsys_php/inosea.php?boton=Consultar&id=".$numreferencia;
+                }
+
+                if( substr($numreferencia,0,1)=="1"  ){
+                    $referencia = Doctrine::getTable("InoMaestraAir")->find($numreferencia);
+
+                    $linea = $referencia->getCaIdlinea();
+
+                    $idproveedores[] = $linea;
+
+                    $this->url = "/Coltrans/InoAir/ConsultaReferenciaAction.do?referencia=".$numreferencia;
+                }
+                $this->form->setIdproveedores($idproveedores);
+
+                $this->numreferencia = $numreferencia;
+            }
+
+            $this->eventos = Doctrine::getTable("IdsEvento")
+                              ->createQuery("e")
+                              ->select("e.*")
+                              ->where("e.ca_referencia=?",$numreferencia)
+                              ->addOrderBy("e.ca_fchcreado ASC")
+                              ->execute();
+        }
+
+        $this->form->configure();
+
+        if ($request->isMethod('post')){
+
+			$bindValues = array();
+            $bindValues["id"] = $request->getParameter("id");
+            $bindValues["tipo_evento"] = $request->getParameter("tipo_evento");
+            $bindValues["evento"] = $request->getParameter("evento");
+
+            $this->form->bind( $bindValues );
+			if( $this->form->isValid() ){
+
+                $evento->setCaId( $bindValues["id"] );
+                $evento->setCaEvento( $bindValues["evento"] );
+                if( $numreferencia ){
+                    $evento->setCaReferencia( $numreferencia );
+                }
+                $evento->setCaIdcriterio( $bindValues["tipo_evento"] );
+                $evento->save();
+
+                $this->redirect($this->url);
+            }
+        }
+        $this->evento = $evento;
+    }
+
+
      /*
     * Permite agregar lineas de transporte
     *
