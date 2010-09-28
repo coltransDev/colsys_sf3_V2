f0cb258f4d8b7906cbd50af6d70edc174c96760a
Andres Botero <abotero@coltrans.com.co>
2010-09-28 21:50:42 +0000

    nueva plantilla para los emails

--- apps/colsys/templates/email.php
+++ apps/colsys/templates/email.php
@@ -1,48 +1,123 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<title>Coltrans S.A.</title>
+    <head>        
+        <style type="text/css" >
 
-    <?php include_stylesheets() ?>
-    <?php include_javascripts() ?>
-</head>
-<body>
-<style type="text/css" >
-	.vigencia{
-		font-size:9px;
-		font-family:Arial, Helvetica, sans-serif;
-	}
-	
-	.htmlContent{
-		font-size:12px;
-		font-family:Arial, Helvetica, sans-serif;
-	}
-	
-	table.tableList th{
-		
-		border:solid 0.5px #EBEBEB;
-		padding:2px;
-		font-size:11px;
-		font-family:Arial, Helvetica, sans-serif;
-		font-weight:bold;
-		background-color:#F8F8F8; 
-	}
-	
-	table.tableList td{
-		
-		border:solid 1px #EBEBEB;		
-		padding:2px;
-		font-size:11px;
-		font-family:Arial, Helvetica, sans-serif;
-		border-collapse:collapse;
-		
-		
-	}		
-</style>
+            img.img{
+                border: 0px;
+            }
 
-<?php 
-echo $sf_content; 
-?>
-</body>
+
+
+            a.link:link {
+               text-decoration:none;
+               color:#0000FF;
+            }
+            a.link:active {
+               text-decoration:none;
+               color:#0000FF;
+            }
+            a.link:visited {
+               text-decoration: none;
+               color: #062A7D;
+            }
+
+            .entry {
+                border-bottom: 1px solid #DDDDDD;
+                clear:both;
+                padding: 0 0 10px;
+            }
+
+
+            .entry-even {
+                background-color:#F6F6F6;
+                border-color:#CCCCCC;
+                border-style:dotted;
+                border-width:1px ;
+                margin:12px 0 0;
+                padding:12px 12px 24px;
+                font-size: 12px;
+                font-family: arial, helvetica, sans-serif;
+
+            }
+
+            .entry-odd {
+                background-color:#FFFFFF;
+                border-color:#CCCCCC;
+                border-style:dotted;
+                border-width:1px ;
+                margin:12px 0 0;
+                padding:12px 12px 24px;
+                font-size: 12px;
+                font-family: arial, helvetica, sans-serif;
+
+            }
+
+            .entry-yellow {
+                background-color:#FFFFCC;
+                border-color:#CCCCCC;
+                border-style:dotted;
+                border-width:1px ;
+                margin:12px 0 0;
+                padding:12px 12px 24px;
+                font-size: 12px;
+                font-family: arial, helvetica, sans-serif;
+
+            }
+            .entry-date{
+                float: right;
+                color: #0464BB;
+            }
+
+            .vigencia{
+            font-size:9px;
+            font-family:Arial, Helvetica, sans-serif;
+        }
+
+        .htmlContent{
+            font-size:12px;
+            font-family:Arial, Helvetica, sans-serif;
+        }
+
+        table.tableList th{
+
+            border:solid 0.5px #EBEBEB;
+            padding:2px;
+            font-size:11px;
+            font-family:Arial, Helvetica, sans-serif;
+            font-weight:bold;
+            background-color:#F8F8F8;
+        }
+
+        table.tableList td{
+
+            border:solid 1px #EBEBEB;
+            padding:2px;
+            font-size:11px;
+            font-family:Arial, Helvetica, sans-serif;
+            border-collapse:collapse;
+
+
+        }
+        </style>
+    </head>
+    <body>
+    
+        <!-- GREY BORDER -->
+        <table width="100%" border="0" cellspacing="15" cellpadding="0" bgcolor="#E1E1E1"><tr><td>
+                    <!-- WHITE BACKGROUND -->
+                    <table width="100%" border="0" cellspacing="15" cellpadding="0" bgcolor="#FFFFFF"><tr><td>
+                                <!-- MAIN CONTENT TABLE -->
+
+                                <?php
+                                echo $sf_content;
+                                ?>
+                            </td></tr>
+                    </table>
+                </td></tr>
+            <!-- COPYRIGHT -->
+            <tr><td><font size="1" face="arial, helvetica, sans-serif" color="#666666">&copy; Coltrans S.A. </font></td></tr>
+        </table>
+
+    </body>
 </html>
+
