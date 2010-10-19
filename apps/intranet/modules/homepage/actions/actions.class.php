1ebb3e8f44d8ffa88c9e55f7ce129d559c54f12f
Andrea L. Ramirez <alramirez@coltrans.com.co>
2010-10-19 19:22:51 +0000

    [Intranet] Busqueda Personalizada

--- apps/intranet/modules/homepage/actions/actions.class.php
+++ apps/intranet/modules/homepage/actions/actions.class.php
@@ -15,52 +15,81 @@ class homepageActions extends sfActions
   *
   * @param sfRequest $request A request object
   */
-  public function executeIndex(sfWebRequest $request)
-  {
-    // $this->forward('default', 'module');
-    $this->user = sfContext::getInstance()->getUser();
 
-  }
-  public function executeEditarNoticia(sfWebRequest $request){
+    const RUTINA_COLSYS = 99;
+    const RUTINA_INTRANET = 99;
 
-            $this->setLayout("layout2col");
-	    $idnoticia = $request->getParameter("idnoticia");
-            $this->noticia = Doctrine::getTable("Noticia")->find( $idnoticia );
+    public function getNivel() {
 
-            if( !$this->noticia ){
-                    $this->noticia = new Noticia();
-            }
+        $app =  sfContext::getInstance()->getConfiguration()->getApplication();
+        //   return 5;
+        switch( $app ){
+            case "colsys":
+                $rutina = homepageActions::RUTINA_COLSYS;
+            break;
+            case "intranet":
+                $rutina = homepageActions::RUTINA_INTRANET;
+            break;
+        }
 
-            $this->forward404Unless( $this->noticia );
-            $this->idnoticia = $idnoticia;
-            $response = sfContext::getInstance()->getResponse();
+        $this->nivel = $this->getUser()->getNivelAcceso($rutina);
+        if (!$this->nivel) {
+        $this->nivel = 0;
+        }
 
+        $this->forward404Unless($this->nivel != -1);
 
-
-
-
-        //Utility Dependencies
-            $response->addJavaScript("yui/yahoo-dom-event/yahoo-dom-event.js",'last');
-            $response->addJavaScript("yui/element/element-min.js",'last');
-        //Needed for Menus, Buttons and Overlays used in the Toolbar
-            $response->addJavaScript("yui/container/container_core-min.js",'last');
-            $response->addJavaScript("yui/menu/menu-min.js",'last');
-            $response->addJavaScript("yui/button/button-min.js",'last');
-        //Source file for Rich Text Editor
-            $response->addJavaScript("yui/editor/editor-min.js",'last');
-            $response->addJavaScript("yui/connection/connection-min.js",'last');
-            $response->addJavaScript("yui/logger/logger-min.js",'last');
-            $response->addJavaScript("yui-image-uploader26.js",'last');
-            $response->addStyleSheet("yui/assets/skins/sam/skin.css",'last');
+        return $this->nivel;
     }
 
+    public function executeIndex(sfWebRequest $request)
+    {
+        // $this->forward('default', 'module');
+        $this->user = sfContext::getInstance()->getUser();
+
+    }
+    public function executeEditarNoticia(sfWebRequest $request){
+
+        $this->setLayout("layout2col");
+
+        $this->nivel = $this->getNivel();
+
+        if( $this->nivel==0){
+            	$this->forward("adminUsers", "noAccess");
+        }
+
+        $idnoticia = $request->getParameter("idnoticia");
+        $this->noticia = Doctrine::getTable("Noticia")->find( $idnoticia );
+
+        if( !$this->noticia ){
+            $this->noticia = new Noticia();
+        }
+
+        $this->forward404Unless( $this->noticia );
+        $this->idnoticia = $idnoticia;
+        $response = sfContext::getInstance()->getResponse();
+
+        //Utility Dependencies
+        $response->addJavaScript("yui/yahoo-dom-event/yahoo-dom-event.js",'last');
+        $response->addJavaScript("yui/element/element-min.js",'last');
+        //Needed for Menus, Buttons and Overlays used in the Toolbar
+        $response->addJavaScript("yui/container/container_core-min.js",'last');
+        $response->addJavaScript("yui/menu/menu-min.js",'last');
+        $response->addJavaScript("yui/button/button-min.js",'last');
+        //Source file for Rich Text Editor
+        $response->addJavaScript("yui/editor/editor-min.js",'last');
+        $response->addJavaScript("yui/connection/connection-min.js",'last');
+        $response->addJavaScript("yui/logger/logger-min.js",'last');
+        $response->addJavaScript("yui-image-uploader26.js",'last');
+        $response->addStyleSheet("yui/assets/skins/sam/skin.css",'last');
+        }
 
     /**
-	* Guarda los datos del formulario
-	*
-	* @param sfRequest $request A request object
-	*/
-	public function executeGuardarNoticia(sfWebRequest $request){
+    * Guarda los datos del formulario
+    *
+    * @param sfRequest $request A request object
+    */
+    public function executeGuardarNoticia(sfWebRequest $request){
 
         $this->setLayout("layout2col");
 
