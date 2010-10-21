29f46a242679dc47b9367de2eb3588c714d14b1d
Andrea L. Ramirez <alramirez@coltrans.com.co>
2010-10-21 17:16:15 +0000

    [Intranet] Arreglo Logos

--- apps/intranet/modules/homepage/templates/_logos.php
+++ apps/intranet/modules/homepage/templates/_logos.php
@@ -1,5 +1,12 @@
 <?
-	if($user){
-		echo image_tag('coltrans_logo.jpg',array('border'=>'none'));
-	}
+    if($user->getSucursal()=='ABO' || $user->getSucursal()=='ACL' || $user->getSucursal()=='ABU' || $user->getSucursal()=='ACT' || $user->getSucursal()=='ABA' || $user->getSucursal()=='AMD'){
+		echo image_tag('colmas_logo.jpg',array('border'=>'none'));
+    }elseif($user->getSucursal()=='CCT' || $user->getSucursal()=='CBU' || $user->getSucursal()=='CBO' || $user->getSucursal()=='CMD'){
+        echo image_tag('consolcargo2.png',array('border'=>'none'));
+    }elseif($user->getSucursal()=='MIA'){
+        echo image_tag('coltrans_usa1.png',array('border'=>'none'));
+    }else{
+        echo image_tag('coltrans_logo.jpg',array('border'=>'none'));
+    }
+
 ?>
