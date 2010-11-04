1453a5d0c4681ee0e0c44a6885d4333327ea7652
Andres Botero <abotero@coltrans.com.co>
2010-11-04 19:42:53 +0000

    [intranet] Indices al guardar usuario

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,13 +1,13 @@
 <?php
 
-$sf_symfony_lib_dir  = 'C:/Desarrollo/htdocs/symfony1.4/lib';
+$sf_symfony_lib_dir  = '/home/abotero/Desarrollo/symfony1.4/lib';
 
 require_once $sf_symfony_lib_dir .'/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
 {
-    //static protected $zendLoaded = false;
+    static protected $zendLoaded = false;
 
     public function setup()
     {
@@ -22,7 +22,7 @@ class ProjectConfiguration extends sfProjectConfiguration
     **/
     public function configureDoctrine(Doctrine_Manager $manager)
     {
-        //$manager->setAttribute(Doctrine::ATTR_QUERY_CACHE, new Doctrine_Cache_Apc());
+        $manager->setAttribute(Doctrine::ATTR_QUERY_CACHE, new Doctrine_Cache_Apc());
 
         $options = array('baseClassName' => 'myDoctrineRecord'); 
         sfConfig::set('doctrine_model_builder_options', $options);
@@ -33,6 +33,18 @@ class ProjectConfiguration extends sfProjectConfiguration
 
 
     }
+
+    public static function  registerZend(){
+
+	    if (self::$zendLoaded){
+	      return;
+	    }
+
+	    set_include_path(sfConfig::get('sf_lib_dir').'/vendor'.PATH_SEPARATOR.get_include_path());
+	    require_once sfConfig::get('sf_lib_dir').'/vendor/Zend/Loader/Autoloader.php';
+	    Zend_Loader_Autoloader::getInstance();
+	    self::$zendLoaded = true;
+	}
 
 
 }
