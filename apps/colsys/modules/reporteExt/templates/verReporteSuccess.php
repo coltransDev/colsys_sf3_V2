d654c49ba89d38b2631ad465bd7e0354ca24be8e
Mauricio A Quinche <maquinche@coltrans.com.co>
2010-12-03 18:48:35 +0000

    [reportesneg] actualizacion

--- apps/colsys/modules/reporteExt/templates/verReporteSuccess.php
+++ apps/colsys/modules/reporteExt/templates/verReporteSuccess.php
@@ -4,12 +4,27 @@ echo nl2br($introduccion);
 <br />
 
 <?
-if( $reporte->getCaTransporte()==Constantes::MARITIMO ){
-    include_component("reporteExt","reporteMaritimoExt", array("reporte"=>$reporte));
-}
+if($reporte->setCaTiporep()>0)
+{
+    if( $reporte->getCaTransporte()==Constantes::MARITIMO ){
+        include_component("reporteExt","reporteMaritimoExtNew", array("reporte"=>$reporte));
+    }
 
-if( $reporte->getCaTransporte()==Constantes::AEREO ){
-    include_component("reporteExt","reporteAereoExt", array("reporte"=>$reporte));
+    if( $reporte->getCaTransporte()==Constantes::AEREO ){
+        include_component("reporteExt","reporteAereoExtNew", array("reporte"=>$reporte));
+    }
+
+
+}
+else
+{
+    if( $reporte->getCaTransporte()==Constantes::MARITIMO ){
+        include_component("reporteExt","reporteMaritimoExt", array("reporte"=>$reporte));
+    }
+
+    if( $reporte->getCaTransporte()==Constantes::AEREO ){
+        include_component("reporteExt","reporteAereoExt", array("reporte"=>$reporte));
+    }
 }
 ?>
 
