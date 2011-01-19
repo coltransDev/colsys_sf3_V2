8f1ae2524b37dd6192187ee0f4c0a8e3aeae74e1
Andres Botero <abotero@coltrans.com.co>
2011-01-19 14:41:35 +0000

    [lib]

--- lib/model/doctrine/sea/InoMaestraSeaTable.class.php
+++ lib/model/doctrine/sea/InoMaestraSeaTable.class.php
@@ -3,5 +3,12 @@
  */
 class InoMaestraSeaTable extends Doctrine_Table
 {
-
+    public static function getNumReferencia($impoexpo, $transporte, $modalidad, $idorigen, $iddestino, $mm, $yy ){        
+        $yy = substr($yy,-1,1);
+        $sql =  "SELECT fun_referencia('".$transporte."', '".$modalidad."','".$idorigen."','".$iddestino."','".$mm."-".$yy."') as next";
+        $q = Doctrine_Manager::getInstance()->connection();
+        $stmt = $q->execute($sql);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);        
+        return $row['next'];
+    }
 }
