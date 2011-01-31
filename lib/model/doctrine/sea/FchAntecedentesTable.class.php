8a39b2bb7ab9de458adc8faade8408308a239903
Andres Botero <abotero@coltrans.com.co>
2011-01-31 16:27:15 +0000

    [lib/model] Actualizacion modelo antecedentes

---
+++ lib/model/doctrine/sea/FchAntecedentesTable.class.php
@@ -0,0 +1,5 @@
+<?php
+
+class FchAntecedentesTable extends Doctrine_Table
+{
+}
--- lib/model/doctrine/sea/InoMaestraSeaTable.class.php
+++ lib/model/doctrine/sea/InoMaestraSeaTable.class.php
@@ -5,7 +5,11 @@ class InoMaestraSeaTable extends Doctrine_Table
 {
     public static function getNumReferencia($impoexpo, $transporte, $modalidad, $idorigen, $iddestino, $mm, $yy ){        
         $yy = substr($yy,-1,1);
-        $sql =  "SELECT fun_referencia('".$transporte."', '".$modalidad."','".$idorigen."','".$iddestino."','".$mm."-".$yy."') as next";
+        $mm = str_pad($mm, 2, "0", STR_PAD_LEFT);
+
+        $ciudad = Doctrine::getTable("Ciudad")->find($idorigen);
+
+        $sql =  "SELECT fun_referencia('".$transporte."', '".$modalidad."','".$ciudad->getCaIdtrafico()."','".$iddestino."','".$mm."-".$yy."') as next";
         $q = Doctrine_Manager::getInstance()->connection();
         $stmt = $q->execute($sql);
         $row = $stmt->fetch(PDO::FETCH_ASSOC);        
