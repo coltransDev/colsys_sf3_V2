ed8f5f0a513c32755ccb88e3dd24928a48c062f3
Andres Botero <abotero@coltrans.com.co>
2011-04-12 20:50:58 +0000

    Branding

--- web/colsys/colsys_php/footer.php
+++ web/colsys/colsys_php/footer.php
@@ -1,6 +1,6 @@
 <div class="footer">	
 	<div class="copyright">
-		Coltrans S.A. - Colmas LTDA. Todos los derechos reservados
+		<?=$templateName?>. Todos los derechos reservados
         <br />
         colsys@<?=$_SERVER["SERVER_ADDR"]?>
 	</div>
