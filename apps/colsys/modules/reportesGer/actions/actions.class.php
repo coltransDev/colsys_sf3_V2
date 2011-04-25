1e2640df730db843fa48bf4fae5b3e6673448eee
Mauricio A Quinche <maquinche@coltrans.com.co>
2011-04-25 16:12:59 +0000

    [reportesger] nuevos filtros , agente , sucursalagente y incoterms

--- apps/colsys/modules/reportesGer/actions/actions.class.php
+++ apps/colsys/modules/reportesGer/actions/actions.class.php
@@ -25,6 +25,11 @@ class reportesGerActions extends sfActions
 	{
         $this->fechainicial=$request->getParameter("fechaInicial");
         $this->fechafinal=$request->getParameter("fechaFinal");
+        $this->fechaembinicial=$request->getParameter("fechaEmbInicial");
+        $this->fechaembfinal=$request->getParameter("fechaEmbFinal");
+        $this->fechaarrinicial=$request->getParameter("fechaArrInicial");
+        $this->fechaarrfinal=$request->getParameter("fechaArrFinal");
+
         $this->idpais_origen=$request->getParameter("idpais_origen");
         $this->origen=$request->getParameter("origen");
         $this->idorigen=$request->getParameter("idorigen");
@@ -33,14 +38,29 @@ class reportesGerActions extends sfActions
         $this->iddestino=$request->getParameter("iddestino");
         $this->idmodalidad=$request->getParameter("idmodalidad");
         $this->opcion=$request->getParameter("opcion");
+        $this->idlinea=$request->getParameter("idlinea");
+        $this->linea=$request->getParameter("linea");
+        $this->incoterms=$request->getParameter("incoterms");
+
+        $this->idagente=$request->getParameter("idagente");
+        $this->agente=$request->getParameter("agente");
+        $this->idsucursalagente=$request->getParameter("idsucursalagente");
+        $this->sucursalagente=$request->getParameter("sucursalagente");
 
         if($this->opcion)
         {
             $q = Doctrine::getTable("RepCargaTraficos")
                             ->createQuery("ct")
                             ->select("*")
-                            ->where("(ca_fchreferencia between ? and ?) and ca_modalidad=?", array($this->fechainicial,$this->fechafinal,$this->idmodalidad))
+                            ->where("ca_modalidad=?", array($this->idmodalidad))
                             ->setHydrationMode(Doctrine::HYDRATE_ARRAY);
+
+            if($this->fechainicial && $this->fechafinal)
+                    $q->addWhere ("(ca_fchreferencia between ? and ?)",array($this->fechainicial,$this->fechafinal));
+            if($this->fechaembinicial && $this->fechaembfinal)
+                    $q->addWhere ("(ca_fchembarque between ? and ?)",array($this->fechaembinicial,$this->fechaembfinal));
+            if($this->fechaarrinicial && $this->fechaarrfinal)
+                    $q->addWhere ("(ca_fcharribo between ? and ?)",array($this->fechaarrinicial,$this->fechaarrfinal));
             if($this->idpais_origen)
                 $q->addWhere("ori_ca_idtrafico=?",$this->idpais_origen);
             if($this->idorigen)
@@ -49,6 +69,17 @@ class reportesGerActions extends sfActions
                 $q->addWhere("des_ca_idtrafico=?",$this->idpais_destino);
             if($this->iddestino)
                 $q->addWhere("ca_destino=?",$this->iddestino);
+            if($this->idlinea)
+                $q->addWhere("ca_idlinea=?",$this->idlinea);
+
+            if($this->incoterms)
+                $q->addWhere("ca_incoterms like ?","%".$this->incoterms."%");
+
+            if($this->idagente)
+                    $q->addWhere("ca_idagente = ?",$this->idagente);
+
+            if($this->idsucursalagente)
+                    $q->addWhere("ca_idsucursalagente = ?",$this->idsucursalagente);
             
             $this->resul=$q->execute();
             
--- apps/colsys/modules/reportesGer/templates/reporteCargaTraficosSuccess.php
+++ apps/colsys/modules/reportesGer/templates/reporteCargaTraficosSuccess.php
@@ -2,6 +2,13 @@
 include_component("widgets", "widgetModalidad");
 include_component("widgets", "widgetPais");
 include_component("widgets", "widgetCiudad");
+include_component("widgets", "widgetLinea");
+include_component("widgets", "widgetIncoterms");
+include_component("widgets", "widgetAgente");
+include_component("widgets", "widgetSucursalAgente");
+$agente = $sf_data->getRaw("agente");
+$linea = $sf_data->getRaw("linea");
+$sucursalagente = $sf_data->getRaw("sucursalagente");
 ?>
 <div align="center" >
 <br />
@@ -16,7 +23,7 @@ var tabs = new Ext.FormPanel({
 	labelWidth: 75,
 	border:true,
 	fame:true,
-	width: 650,
+	width: 900,
 	standardSubmit: true,
         id: 'formPanel',
 	items: {
@@ -32,81 +39,174 @@ var tabs = new Ext.FormPanel({
             labelWidth: 75,
 
 			items: [
-                    {
-                        xtype:'hidden',
-                        name:"opcion",
-                        value:"buscar"
-                    },
-                    {
-                            xtype:'datefield',
-                            fieldLabel: 'Fecha Inicial',
-                            name : 'fechaInicial',
-                            format: 'Y-m-d',
-                            value: '<?=($fechainicial)?$fechainicial:date("Y-m-")."01"?>'
-                    },
-                    {
-                            xtype:'datefield',
-                            fieldLabel: 'Fecha final',
-                            name : 'fechaFinal',
-                            format: 'Y-m-d',
-                            value: '<?=($fechainicial)?$fechafinal:date("Y-m-")."01"?>'
-                    },
-                     {
-                        xtype:"hidden",
-                        id: 'impoexpo',
-                        name: 'impoexpo',
-                        value:'<?=Constantes::IMPO?>'
-                    }
-                    ,
-                    {
-                        xtype:"hidden",
-                        id: 'transporte',
-                        name: 'transporte',
-                        value:'<?=Constantes::MARITIMO?>'
-                    }
-                    ,
-                    new WidgetModalidad({fieldLabel: 'Tipo Envio',
-                                        id: 'modalidad',
-                                        hiddenName: "idmodalidad",
-                                        linkTransporte: "transporte",
-                                        linkImpoexpo: "impoexpo",
-                                        value:"<?=$idmodalidad?>"
-                                        })
-                                        ,
-                    new WidgetPais({fieldLabel: 'Pais origen',
-                                        id: 'pais_origen',
-                                        name: 'pais_origen',
-                                        hiddenName: "idpais_origen",
-                                        pais:"todos",
-                                        value:"<?=$idpais_origen?>"
-                                        }),
-                    new WidgetCiudad({fieldLabel: 'origen',
-                                      id: 'origen',
-                                      idciudad:"origen",
-                                      hiddenName:"idorigen",
-                                      tipo:"3",
-                                      traficoParent:"pais_origen",
-                                      impoexpo: "impoexpo",
-                                      value:"<?=$origen?>",
-                                      hiddenValue:"<?=$idorigen?>"
-                                    }),
-                new WidgetPais({fieldLabel: 'Pais destino',
-                                        id: 'pais_destino',
-                                        name: 'pais_destino',
-                                        hiddenName: "idpais_destino",
-                                        pais:"todos",
-                                        value:"<?=$idpais_destino?>"
-                                        }),
-                    new WidgetCiudad({fieldLabel: 'destino',
-                                      id: 'destino',
-                                      idciudad:"destino",
-                                      hiddenName:"iddestino",
-                                      tipo:"3",
-                                      traficoParent:"pais_destino",
-                                      impoexpo: "impoexpo",
-                                      value:"<?=$destino?>",
-                                      hiddenValue:"<?=$iddestino?>"
-                                    })
+                {
+                        xtype:'fieldset',
+                        title: 'filtros',
+                        autoHeight : true,
+                        layout : 'column',
+                        columns : 2,
+                        defaults:{
+                            columnWidth:0.5,
+                            layout:'form',
+                            border:false,
+                            bodyStyle:'padding:4px;'
+                        },
+                        items :
+                        [
+                            {
+                                xtype:'hidden',
+                                name:"opcion",
+                                value:"buscar"
+                            },
+                            {
+                                xtype:"hidden",
+                                id: 'impoexpo',
+                                name: 'impoexpo',
+                                value:'<?=Constantes::IMPO?>'
+                            },
+                            {
+                                xtype:"hidden",
+                                id: 'transporte',
+                                name: 'transporte',
+                                value:'<?=Constantes::MARITIMO?>'
+                            },
+                            {
+                                columnWidth:.5,
+                                border:false,
+                                items:
+                                [
+                                    {
+                                            xtype:'datefield',
+                                            fieldLabel: 'Fecha Emb Ini',
+                                            name : 'fechaEmbInicial',
+                                            format: 'Y-m-d',
+                                            value: '<?=$fechaembinicial?>'
+                                    },
+                                    {
+                                            xtype:'datefield',
+                                            fieldLabel: 'Fecha Arribo Ini',
+                                            name : 'fechaArrInicial',
+                                            format: 'Y-m-d',
+                                            value: '<?=$fechaarrinicial?>'
+                                    },
+                                    {
+                                            xtype:'datefield',
+                                            fieldLabel: 'Fecha Ini',
+                                            name : 'fechaInicial',
+                                            format: 'Y-m-d',
+                                            value: '<?=$fechainicial?>'
+                                    }
+                                    ,
+                                    new WidgetModalidad({fieldLabel: 'Tipo Envio',
+                                                        id: 'modalidad',
+                                                        hiddenName: "idmodalidad",
+                                                        linkTransporte: "transporte",
+                                                        linkImpoexpo: "impoexpo",
+                                                        value:"<?=$idmodalidad?>"
+                                                        })
+                                                        ,
+                                    new WidgetPais({title: 'Pais origen',
+                                                    fieldLabel: 'Pais origen',
+                                                        id: 'pais_origen',
+                                                        name: 'pais_origen',
+                                                        hiddenName: "idpais_origen",
+                                                        pais:"todos",
+                                                        value:"<?=$idpais_origen?>"
+                                                        }),                                    
+                                    new WidgetPais({title: 'Pais destino',
+                                                        fieldLabel: 'Pais destino',
+                                                        id: 'pais_destino',
+                                                        name: 'pais_destino',
+                                                        hiddenName: "idpais_destino",
+                                                        pais:"todos",
+                                                        value:"<?=$idpais_destino?>"
+                                                        }),
+                                    new WidgetAgente({fieldLabel: 'Agente',
+                                                      linkImpoExpo: "<?=constantes::IMPO?>",
+                                                      linkListarTodos: "all",
+                                                      id:"agente",
+                                                      hiddenName:"idagente",
+                                                      width:350,
+                                                      value:"<?=$agente?>",
+                                                      hiddenValue:"<?=$idagente?>"
+                                                      
+                                                    }),
+                                    new WidgetIncoterms({title: 'Terminos',
+                                                      fieldLabel:"Terminos",
+                                                      id: 'terminos',
+                                                      hiddenName:"incoterms",
+                                                      width:250,
+                                                      value:"<?=$incoterms?>"
+                                                    })
+                              ]
+                         },
+                         {
+                                columnWidth:.5,
+                                border:false,
+                                items:
+                                [
+                                    {
+                                            xtype:'datefield',
+                                            fieldLabel: 'Fecha Emb fin',
+                                            name : 'fechaEmbFinal',
+                                            format: 'Y-m-d',
+                                            value: '<?=$fechaembfinal?>'
+                                    },
+                                    {
+                                            xtype:'datefield',
+                                            fieldLabel: 'Fecha Arribo fin',
+                                            name : 'fechaArrFinal',
+                                            format: 'Y-m-d',
+                                            value: '<?=$fechaarrfinal?>'
+                                    },
+                                    {
+                                            xtype:'datefield',
+                                            fieldLabel: 'Fecha final',
+                                            name : 'fechaFinal',
+                                            format: 'Y-m-d',
+                                            value: '<?=$fechafinal?>'
+                                    },
+                                    new WidgetLinea({fieldLabel: 'Naviera',
+                                                     linkTransporte: "transporte",
+                                                     name:"linea",
+                                                     id:"linea",
+                                                     hiddenName: "idlinea",
+                                                     value:"<?=$idlinea?>",
+                                                     width:300
+                                                    }),
+                                     new WidgetCiudad({fieldLabel: 'origen',
+                                                      id: 'origen',
+                                                      idciudad:"origen",
+                                                      hiddenName:"idorigen",
+                                                      tipo:"3",
+                                                      traficoParent:"pais_origen",
+                                                      impoexpo: "impoexpo",
+                                                      value:"<?=$origen?>",
+                                                      hiddenValue:"<?=$idorigen?>"
+                                                    }),
+                                     new WidgetCiudad({fieldLabel: 'destino',
+                                                      id: 'destino',
+                                                      idciudad:"destino",
+                                                      hiddenName:"iddestino",
+                                                      tipo:"3",
+                                                      traficoParent:"pais_destino",
+                                                      impoexpo: "impoexpo",
+                                                      value:"<?=$destino?>",
+                                                      hiddenValue:"<?=$iddestino?>"
+                                                    }),
+                                     new WidgetSucursalAgente({fieldLabel: 'Sucursal',
+                                                      linkAgente: "agente",
+                                                      id:"sucursalagente",
+                                                      name:"sucursalagente",
+                                                      hiddenName:"idsucursalagente",
+                                                      width:250,                                                      
+                                                      hiddenValue:"<?=$sucursalagente?>",
+                                                      value:"<?=$idsucursalagente?>"
+                                                    })
+                             ]
+                         }
+                     ]
+                }
 			]
 		}]
 	},
@@ -122,7 +222,36 @@ var tabs = new Ext.FormPanel({
                     }
                     owner.getForm().submit();
             }
-	}]
+	}],
+    listeners:{afterrender:function(){
+
+        linea_sel='<?=$linea?>';
+        idlinea_sel='<?=$idlinea?>';
+        if(linea_sel!="")
+        {
+            Ext.getCmp("linea").setValue(idlinea_sel);
+            $("#linea").val(linea_sel);
+
+        }
+
+        agente_sel='<?=$agente?>';
+        idagente_sel='<?=$idagente?>';
+        if(agente_sel!="")
+        {
+            Ext.getCmp("agente").setValue(idagente_sel);
+            $("#agente").val(agente_sel);
+        }
+
+        suc_agente_sel='<?=$sucursalagente?>';
+        idsucagente_sel='<?=$idsucursalagente?>';
+        if(suc_agente_sel!="")
+        {
+            Ext.getCmp("sucursalagente").setValue(idsucagente_sel);
+            $("#sucursalagente").val(suc_agente_sel);
+        }
+    }
+
+    }
 });
 tabs.render("container");
 </script>
@@ -134,16 +263,25 @@ if($opcion)
 
 <div align="center">
 <br>
-<h3>Estadisticas de cargas <?=$fechainicial?> <?=$fechafinal?> <br>
+<h3>Estadisticas de cargas  <br>
 <?
+if( $fechainicial && $fechafinal ){
+    echo " fechas de Creacion referencia: ".$fechainicial." - ".$fechafinal;
+}
+if( $fechaembinicial && $fechaembfinal ){
+    echo " fechas de Embarque: ".$fechaembinicial." - ".$fechaembfinal;
+}
+if( $fechaarrinicial && $fechaarrfinal ){
+    echo " fechas de Arribo: ".$fechaarrinicial." - ".$fechaarrfinal;
+}
 if( $idmodalidad ){
-    echo "modalidad: ".$idmodalidad." - ";
+    echo " modalidad: ".$idmodalidad." - ";
 }
 if( $origen ){
-    echo "origen: ".$origen." - ";
+    echo " origen: ".$origen." - ";
 }
 if( $destino ){
-    echo "destino: ".$destino." - ";
+    echo " destino: ".$destino." - ";
 }
 
 ?>
@@ -152,7 +290,7 @@ if( $destino ){
 <br />
 </div>
 <table class="tableList" width="900px" border="1" id="mainTable" align="center">
-    <tr><td>Fecha<br>Emabrque</td><td>Fecha<br>Referencia</td><td>Referencia</td><td>Origen</td><td>Destino</td><td>Linea</td><td>Contenedores</td><td>Teus</td><td>Piezas</td><td>Peso</td><td>Volumen</td></tr>
+    <tr><td>Fecha<br>Embarque</td><td>Fecha<br>Arribo</td><td>Fecha<br>Referencia</td><td>Referencia</td><td>Origen</td><td>Destino</td><td>Linea</td><td>Contenedores</td><td>Teus</td><td>Piezas</td><td>Peso</td><td>Volumen</td></tr>
 <?
     $ref="";
     $tvolumen=0;
@@ -184,6 +322,7 @@ if( $destino ){
 ?>
     <tr>
         <td><?=$r["ca_fchembarque"]?></td>
+        <td><?=$r["ca_fcharribo"]?></td>
         <td><?=$r["ca_fchreferencia"]?></td>
         <td><?=$r["ca_referencia"]?></td>
         <td><?=$r["ori_ca_ciudad"]?></td>
@@ -199,7 +338,7 @@ if( $destino ){
     $ref=$r["ca_referencia"];
     }
 ?>
-    <tr><td colspan="7">Totales</td>
+    <tr><td colspan="8">Totales</td>
         <td align="right"><?=$teus?></td>
         <td align="right"><?=number_format($tpiezas,0)?></td><td align="right"><?=number_format($tpeso,2)?></td><td align="right"><?=number_format($tvolumen,2)?></td></tr>
 
