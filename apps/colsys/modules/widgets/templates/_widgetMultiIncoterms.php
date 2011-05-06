188b8ddffb22abb3ac439c6aa919b68b07ff61c8
Mauricio A Quinche <maquinche@coltrans.com.co>
2011-05-06 19:13:38 +0000

    [widgets] WidgetMultiIncoterms

--- apps/colsys/modules/widgets/templates/_widgetMultiIncoterms.php
+++ apps/colsys/modules/widgets/templates/_widgetMultiIncoterms.php
@@ -10,67 +10,15 @@ $incoterms = $sf_data->getRaw("incoterms");
 ?>
 <script type="text/javascript">
 
-/*var sbs1 = new Ext.ux.form.SuperBoxSelect({
-	        allowBlank:false,
-	        id:'selector1',
-	        xtype:'superboxselect',
-	        fieldLabel: 'States',
-	        emptyText: 'Select some US States',
-	        resizable: true,
-	        name: 'states',
-	        anchor:'100%',
-	        store: states,
-	        mode: 'local',
-	        displayField: 'state',
-	        displayFieldTpl: '{state} ({abbr})',
-	        valueField: 'abbr',
-	        value: 'CA,NY',
-	        forceSelection : true,
-			allowQueryAll : false,
-			listeners : {
-		        render : function(sbs){
-			        sbs.wrapEl.on('contextmenu', function(ev,h,o){
-						ev.stopEvent();
-						var rec = sbs.findSelectedRecord(h),
-						    i = sbs.findSelectedItem(h),
-							n = rec.get('abbr');
-						var ctxMenu = new Ext.menu.Menu({
-							items:[{
-								text : 'Action 1 on ' + n 
-							},
-							{
-                                text : 'Action 2 on ' + n
-                            }]
-						});
-						ctxMenu.showAt([ev.getPageX(), ev.getPageY()]);
-					},sbs,{
-						delegate : 'li.x-superboxselect-item'
-					});		
-				}  	
-		    }
-	    });
-*/
-
 
 WidgetMultiIncoterms = function( config ){
     Ext.apply(this, config);
-    
-    this.store = new Ext.data.Store({
-        autoLoad : true,
-        reader: new Ext.data.JsonReader(
-            {
-                root: 'root',
-                totalProperty: 'total',
-                successProperty: 'success'
-            },
-            Ext.data.Record.create([
-                {name: 'valor'}
-            ])
-        ),
-        proxy: new Ext.data.MemoryProxy( <?=json_encode(array("root"=>$incoterms, "total"=>count($incoterms), "success"=>true) )?> )
-    });
 
-
+    this.store=new Ext.data.SimpleStore({
+            fields: ['valor'],
+            data: <? print_r($incoterms); ?>,
+            sortInfo: {field: 'valor', direction: 'ASC'}
+        });    
 
     WidgetMultiIncoterms.superclass.constructor.call(this, {            	        
 	        xtype:'superboxselect',
@@ -79,16 +27,14 @@ WidgetMultiIncoterms = function( config ){
             displayField: 'valor', 
 	        resizable: true,	        
 	        anchor:'100%',
-	        mode: 'local',	       
-            addNewDataOnBlur : true, 
-	        forceSelection : true//,
-			//allowQueryAll : false//,            
-            //allowAddNewData: true
+	        mode: 'local'
     });
 };
 
 
 Ext.extend(WidgetMultiIncoterms, Ext.ux.form.SuperBoxSelect, {
+    
+    
 	
 });
 
