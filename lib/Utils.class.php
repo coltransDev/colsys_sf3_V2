11911e77da50bbceff9d05533e2ccf50c94f7659
Mauricio A Quinche <maquinche@coltrans.com.co>
2011-06-02 20:29:23 +0000

    [reportesGer] actualizacion

--- lib/Utils.class.php
+++ lib/Utils.class.php
@@ -237,6 +237,52 @@ class Utils{
 				break;
 		}
 	}
+    
+    
+    public static function nmes( $m ){
+		//settype($m, "integer");
+		switch($m) {
+			case "Enero":
+				return 1;
+				break;
+			case "Febrero": 
+				return 2;
+				break;
+			case "Marzo":
+				return 3;
+				break;
+			case "Abril":
+				return 4;
+				break;
+			case "Mayo":
+				return 5;
+				break;
+			case "Junio":
+				return 6;
+				break;
+			case "Julio":
+				return 7;
+				break;
+			case "Agosto":
+				return 8;
+				break;	
+			case "Septiembre":
+				return 9;
+				break;
+			case "Octubre":
+				return 10;
+				break;
+			case "Noviembre":
+				return 11;
+				break;
+			case "Diciembre":
+				return 12;
+				break;			
+			default:
+				return 0;
+				break;
+		}
+	}
 	
 	
 	public static function extension($file){
