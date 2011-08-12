72127abd94333597730ea6740d5901291b99675e
Mauricio A Quinche <maquinche@coltrans.com.co>
2011-08-12 20:04:06 +0000

    [reportes] trackingetapa

--- lib/model/doctrine/reportes/TrackingEtapa.class.php
+++ lib/model/doctrine/reportes/TrackingEtapa.class.php
@@ -13,34 +13,20 @@
 class TrackingEtapa extends BaseTrackingEtapa
 {
     public function getIntroAsunto(){
-		switch( $this->getCaIdetapa() ){
-			case "IMCPD":
-				$asunto = "Confirmación de Llegada";
-				break;
-			case "IMCOL":
-				$asunto = "Confirmación de Llegada OTM";
-				break;
-			case "IACAD":
-				$asunto = "Confirmación de Llegada";
-				break;
-			case "IMETA":
-				$asunto = "Aviso";
-				break;
-            case "IMETT":
-				$asunto = "Aviso";
-				break;
-			case "99999":
-				$asunto = "Cierre";
-				break;
-			default:
-				if( $this->getCaDepartamento()=="OTM/DTA" ){
-					$asunto = "Status OTM";
-				}else{
-					$asunto = "Status";
-				}
-				break;
-		}
-
+        if($this->getCaPreasunto()!="")
+        {
+            $asunto=$this->getCaPreasunto()." ";
+        }
+        else
+        {
+            if( $this->getCaDepartamento()=="OTM/DTA" ){
+                $asunto = "Status OTM";
+            }else{
+                $asunto = "Status";
+            }
+        }
+		
 		return $asunto;
 	}
 }
+
