ec4f10d5a3ffa616dd60a80c3535c55a849224b1
Andres Botero <abotero@coltrans.com.co>
2011-08-26 22:01:41 +0000

    [inv] Modelo Inventarios

--- lib/model/doctrine/inv/InvCategory.class.php
+++ lib/model/doctrine/inv/InvCategory.class.php
@@ -12,4 +12,28 @@
  */
 class InvCategory extends BaseInvCategory
 {
+    public function getPrefijo( $idsucursal ){
+        
+        $parent = $this->getParent();
+        $pref = null;
+        if( $parent ){
+            $pref = Doctrine::getTable("InvPrefijo")
+                              ->createQuery("p")                                      
+                              ->addWhere("p.ca_idsucursal = ?", $idsucursal)
+                              ->addWhere("p.ca_idcategory = ?", $parent->getCaIdcategory() )
+                              ->fetchOne();
+           
+        }
+        
+        if( !$pref || $pref&&!$pref->getCaPrefix() ){
+            $pref = Doctrine::getTable("InvPrefijo")
+                              ->createQuery("p")                                      
+                              ->addWhere("p.ca_idsucursal = ?", $idsucursal)
+                              ->addWhere("p.ca_idcategory = ?", $this->getCaIdcategory() )
+                              ->fetchOne();
+        }
+        return $pref;        
+        
+    }
+    
 }
---
+++ lib/model/doctrine/inv/InvPrefijoTable.class.php
@@ -0,0 +1,5 @@
+<?php
+
+class InvPrefijoTable extends Doctrine_Table
+{
+}
