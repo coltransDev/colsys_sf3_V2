2d99fdf233ebaf7de884367941a7cb2f58f1198c
Andres Botero <abotero@coltrans.com.co>
2011-10-19 21:33:55 +0000

    [ids] Se eliminaron comentarios del código

--- apps/colsys/modules/ids/templates/_documentos.php
+++ apps/colsys/modules/ids/templates/_documentos.php
@@ -166,65 +166,7 @@ $documentos = $sf_data->getRaw( "documentos" );
     
     $tipo = null;
     $i = 0;
-    /*foreach( $documentos as $documento ){
-        if( $tipo!=$documento->getCaIdtipo() ){
-            $tipo = $documento->getCaIdtipo();
-            $class="";
-        }else{
-            $class="doc_".$documento->getCaIdtipo();
-
-        }
     
-    ?>
-    <tr id="<?=$class?$class."_".$i++:""?>" <?=$class?'style="display:none;"':""?>  >
-        <td>
-           <?
-           if( !$class ){
-           ?>
-           <div class="group_collapsed" id='group_<?=$documento->getCaIdtipo()?>' onclick="expandCollapseGroup( this, 'doc_<?=$documento->getCaIdtipo()?>')">
-           <?=$documento->getIdsTipoDocumento()?>
-           <?
-           }else{
-               ?>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?=$documento->getIdsTipoDocumento()?>
-               <?
-           }
-           ?>
-           </div>
-        </td>
-        <td>
-           <?=Utils::fechaMes($documento->getCaFchinicio() )?>
-        </td>
-        <td>
-            <?
-            if( $documento->getCaFchvencimiento() ){
-                if($documento->getCaFchvencimiento()<date("Y-m-d")){
-                    ?>
-                <span class="rojo">
-                     <?=Utils::fechaMes($documento->getCaFchvencimiento() )?>
-                </span>
-                <?
-                }else{
-                    echo Utils::fechaMes($documento->getCaFchvencimiento() );
-                }
-            }
-            ?>
-        <td>
-        <td>
-           <?=($documento->getCaUbicacion()&&file_exists($documento->getArchivo())?mime_type_icon($documento->getCaUbicacion())." ".link_to($documento->getCaUbicacion(),"ids/verDocumento?iddocumento=".$documento->getCaIddocumento()):"&nbsp;")?>
-        </td>
-        <td>
-            <?
-            if( !$class ){
-                if( $nivel>=3 ){
-                    echo link_to(image_tag("16x16/edit.gif"), "ids/formDocumentos?modo=".$modo."&iddocumento=".$documento->getCaIddocumento(),array("title"=>"Editar documento"));
-                }
-            }
-            ?>
-        </td>
-    </tr> 
-    <?
-    }*/
     ?>
 </table>
 
