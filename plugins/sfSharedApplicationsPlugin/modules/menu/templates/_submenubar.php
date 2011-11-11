210f2e10e4e8a244eb866a49acf2456d2558b62a
Andres Botero <abotero@coltrans.com.co>
2011-11-11 21:25:31 +0000

    [menu] se colocó el reloj

---
+++ plugins/sfSharedApplicationsPlugin/modules/menu/templates/_submenubar.php
@@ -0,0 +1,50 @@
+<?
+
+$buttons = $sf_data->getRaw('buttons');
+
+if( count($buttons)>0 ){
+?>
+<br />
+<div align="center">
+<div class="toolbar">
+	<div class="submenuleft" ><?=image_tag("layout/submenu/leftbg.gif")?></div>
+	<div class="submenuright" ><?=image_tag("layout/submenu/rightbg.gif")?></div>
+<?php 		
+foreach($buttons as $button){			
+	isset($button['onClick'])?$onClick="onClick='".$button['onClick']."'":$onClick="";
+	isset($button['options'])?$options=$button['options']."'":$options="";
+?>	
+  <div class="toolbarbtnWraper">  	
+  	
+	<?			
+	echo image_tag("");
+	if( isset($button['onClick']) ){
+            $button['id']=isset($button['id'])?$button['id']:"";
+		?>
+		<a onclick="<?=$button['onClick']?>" class="toolbarBtn" <?=$button['id']?'id="'.$button['id'].'"':""?> title="<?=$button['tooltip']?>"><?=image_tag( $button["image"] ,'size=18x18' ) ."<br> ".$button["name"] ?></a>
+		<?
+	}else{
+		echo link_to( image_tag( $button["image"] ,'size=18x18' ) ."<br> ".$button["name"]  , isset($button["link"])?$button["link"]:"#" , "$options class=toolbarBtn  $onClick ".($button["tooltip"]?" title='".$button["tooltip"]."'":"")." ".(isset($button["confirm"])?" confirm='".$button["confirm"]."'":"") );
+	}//onmouseover=return overlib('".$button["tooltip"]."') onmouseout=return nd();
+		
+	?>
+	</div>
+<?php 
+}
+if(isset($buttonHelp) && count($buttonHelp)>0)
+{
+
+
+?>
+    <div class="toolbarbtnhelp">
+        <a onclick="<?=$buttonHelp['onClick']?>" class="toolbarBtn" title="<?=$buttonHelp['tooltip']?>"><?=image_tag( $buttonHelp["image"] ,'size=18x18' ) ?></a>
+    </div>
+<?
+}
+?>
+</div>
+</div>
+
+<?
+}
+?>
