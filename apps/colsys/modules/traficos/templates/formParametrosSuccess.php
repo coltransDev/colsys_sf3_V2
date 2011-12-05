f8742f1cd2a8aa660af68877ec3ff02fda467e6b
Andrea L. Ramirez <alramirez@coltrans.com.co>
2011-12-05 15:34:25 +0000

    traficos [actions,components,Nuevoparametro,_infoReporte] Se adiciona casilla para digitar información adicional para IDGs

---
+++ apps/colsys/modules/traficos/templates/formParametrosSuccess.php
@@ -0,0 +1,48 @@
+<?php
+$url = "traficos/formParametros?&idreporte=".$reporte->getCaIdreporte();
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<div class="content" align="center">
+
+<form action="<?=url_for( $url )?>" method="post" name="form1" >
+
+<table width="60%" border="0" class="tableList">
+<?
+$widgets = $form->getWidgetsClientes();
+if( count($widgets)>0 ){
+    foreach( $widgets as $name=>$val ){									
+?>
+        <tr>
+            <td colspan="3">
+
+                <div align="left">
+                    <?
+                 echo "<b>".$val["label"].":</b><br />"; 
+                 echo $form[$name]->renderError(); 
+                 if( $reporte ){	
+                   $form->setDefault($name, $reporte->getProperty($name) ); 
+                 }
+                 echo $form[$name]->render();
+                 ?>
+                    </div></td>
+        </tr>						
+<?
+    }
+}
+
+?>
+        <tr>
+		<td ><div align="center">
+            <input type="submit" value="Guardar" class="button" />&nbsp;
+            
+            <input type="button" value="Cancelar" class="button" onClick="document.location='<?=url_for("traficos/listaStatus?&reporte=".$reporte->getCaConsecutivo())?>'" />&nbsp;
+			
+		</div></td>
+		</tr>
+
+</table>
+</form>
+</div>
