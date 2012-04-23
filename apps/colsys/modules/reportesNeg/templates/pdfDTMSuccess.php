ca8e8416f5ec1059183fa72e9c807e1b3e8f32bc
Mauricio A Quinche <maquinche@coltrans.com.co>
2012-04-23 13:59:19 +0000

    [reportesNeg]  nuevos pdf otm

---
+++ apps/colsys/modules/reportesNeg/templates/pdfDTMSuccess.php
@@ -0,0 +1,175 @@
+<?
+$reporte = $sf_data->getRaw("reporte");
+?>
+
+<table cellpadding="1" cellspacing="1" border="1">
+    <tr>
+        <td>
+            <table style="font-size: 22px;">
+                <tr >
+                    <td><div style="font-size: 12px;">2.CONSIGNOR </div> Expedidor <br> ITALIAN CABLE COMPANY SPA VIA FRANCESA</td>
+                </tr>
+                <tr>
+                    <td><div style="font-size: 12px;">3.CONSIGNED </div> Consignado a la orden de <br> LOGIMAT ZONA FRANCA BOGOTA/ <BR> FABRIELECTRICOS EU</td>
+                </tr>
+                <tr>
+                    <td><div style="font-size: 12px;">4.ADDRESS FOR NOTIFICATIONS </div> Direccion para notificacion <br> FABRIELECTRICOS EU <br> cr76 No 8C-24</td>
+                </tr>
+                <tr>
+                    <td><div style="font-size: 12px;">5.PLACE AND DATE OF RECEIPT - CODE </div> Lugar y fecha de recibo - codigo <br> MILANO 7/14/2011</td>
+                </tr>
+            </table>
+       </td>
+       <td>
+            <table border="1">
+                <tr>
+                    <td>04-009-697</td>
+                </tr>
+                <tr>
+                    <td>NEGOTIABLE MULTIMODAL TRANSPORTATION ANDEAN<BR>DOCUMENT INTERNATIONATIONAL M.T.A.D.I<BR>Documento Andino de Transporte<br> Multimodal Internacional D.A.T.5.M.I.<br>
+                        <img src="/images/logos/LOGOCOLTM200.jpg" width="200" height="69"><br>
+                    </td>
+                </tr>
+            </table>
+       </td>
+    </tr>
+    <tr>
+        <td colspan="2">
+            <table border="1">
+                <tr style="font-size: 12px;" >
+                    <td>MODES<BR>modo</td>
+                    <td>ORIGIN<BR>Origen</td>
+                    <td>DESTINATION<BR>DESTINO</td>                    
+                    <td>MODES<BR>modo</td>
+                    <td>ORIGIN<BR>Origen</td>
+                    <td>DESTINATION<BR>DESTINO</td>
+                    <td>MODES<BR>modo</td>
+                    <td>ORIGIN<BR>Origen</td>
+                    <td>DESTINATION<BR>DESTINO</td>
+                    <td>MODES<BR>modo</td>
+                    <td>ORIGIN<BR>Origen</td>
+                    <td>DESTINATION<BR>DESTINO</td>
+                </tr>
+                <tr style="font-size: 22px;">
+                    <td>3</td>
+                    <td>VADOLIGURE</td>
+                    <td>CARTAGENA</td>
+                    <td>1</td>
+                    <td>CARTAGENA</td>
+                    <td>BOGOTA</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="2" style="font-size: 22px;"><div style="font-size: 12px;">7.HANDLING INSTRUCTIONS</div>
+        Instruccion de manejo
+        </td>
+    </tr>
+    
+    <tr>
+        <td colspan="2">
+            <table>
+                <tr style="font-size: 12px;">
+                    <td>8.INTENTED DATE OF DELIVERY<BR>fecha de entrega programada</td>
+                    <td>9.NUMBER DE ORIGINAL DTMs ISSUED<BR>No. de D.T.M. orig.</td>
+                    <td>10.TYPE OF CARGO Tipo de carga<BR>General</td>
+                    <td>PLACE OF FINAL DELIBERY<BR>Lugar de entrega final<br><b>LOGIMAT ZONA FRANCA BOGOTA COLOMBIA</b></td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    
+    <tr>
+        <td colspan="2">
+            <table>
+                <tr style="font-size: 12px;">
+                    <td>12.MARKS AND NUMBERS<BR>Marcas y numeros</td>
+                    <td>13.NUMBERS AND TYPE OF PACKAGES<BR>Numero y clase de empaque</td>
+                    <td>14.DESCRIPTION OF GOODS <BR>descripcion de los bienes</td>
+                    <td>15.GROSS WEIGHT<BR>peso Bruto</td>
+                    <td>16.VOLUMEN<BR>volumen</td>
+                </tr>
+                
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="2">
+            <table style="font-size: 15px;"  border="1">
+                <tr>
+                    <td width="60%" style="font-size: 22px;"><div style="font-size: 12px;">18.GOODS AND INSTRUCTIONS ARE ACEPTED</div>
+                    <BR>
+                    Los bienes e instrucciones son aceptadas y manejados con sujecion a las coindiciones del contrato de transporte multimodal a menos que se indique lo contrario en el presente documento, son tomados a cargo en unbuen orden y condicion aparente en el lugar de recibo para su transporte y entrega segun lo arriba mencionado. Uno de estos documentos de transporte multimodal debe ser entregado debidamentre endosado a cambio de los bienes en fe de lo cual el D.T.M. Original y todas estas copias ecaxtas y fachadas han sido firmadas en el numero de estipulados abajo.
+                    </td>
+                    <td width="40%">
+                    <div style="font-size: 12px;">19 COST AND FREIGHT</div>
+                        <table  border="1"> 
+                            <tr >
+                                <td rowspan="2">FREIGHT<BR>Flete</td>
+                                <td>PREPAID<br>Prepagado</td>
+                                <td>CURRENCY<br>Moneda</td>
+                                <td>COLLECT<br>Al cobro</td>
+                                <td>CURRENCY<br>Moneda</td>
+                            </tr>
+                            <tr>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td>OTHERS<br>Otros</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td>TOTAL</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
---
+++ apps/colsys/modules/reportesNeg/templates/pdfOTMSuccess.php
@@ -0,0 +1,189 @@
+<?
+$reporte = $sf_data->getRaw("reporte");
+
+$cliente=$reporte->getCliente();
+        
+        
+$stretching=100;
+$spacing=1.8;
+//style="font-stretch:<?=$stretching %;letter-spacing:<?=$spacing mm;"
+$spacing=2;
+
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, "LETTER", true, 'UTF-8', false);
+
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor('Coltrans');
+$pdf->SetMargins(1, 1, 1,true);
+
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+$pdf->SetFont('helvetica', '', 10);
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Print a table
+
+$pdf->AddPage('', '',true);
+
+// MultiCell($w, $h, $txt, $border=0, $align='J', $fill=0, $ln=1, $x='', $y='', $reseth=true, $stretch=0, $ishtml=false, $autopadding=true, $maxh=0)
+
+// set some text for example
+$txt = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+
+// Multicell test
+//$pdf->Ln(4);
+//$pdf->MultiCell(55, 5, '2012', 0, 'L', 0, 1, 10, 20);
+
+//$txt=sprintf ("%14s",$reporte->getContacto()->getCaIdcontacto());
+//$txt=sprintf ("%14s",$reporte->getContacto());
+$y=20;
+$x=13;
+$txt="2012";
+$pdf->setFontSpacing(1.9);
+$pdf->MultiCell(55, 10, $txt, 0, 'L', 0, 1, $x+1, $y-8);
+
+$x=8;
+$y=$y+32;
+
+$txt=$cliente->getCaIdalterno();//sprintf ("%s %13s ",$cliente->getCaIdalterno(),$cliente->getCaDigito());
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x, $y);
+
+$txt=$cliente->getCaDigito();
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+56, $y);
+
+
+$pdf->setFontSpacing(0);
+//$txt=sprintf ("%30s %30s %30s %30s","Yepes","","Sandra", "Pepita" );
+$txt="";
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+42, $y);
+
+$y=$y+8;
+$txt=$cliente->getCaCompania();
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x, $y);
+
+$pdf->setFontSpacing($spacing);
+
+$y=$y+10;
+
+$txt="900451936";//sprintf ("%s %13s ",$cliente->getCaIdalterno(),$cliente->getCaDigito());
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x, $y);
+
+$txt="8";
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+56, $y);
+
+//$pdf->MultiCell(55, 5, "2012", 1, 'L', 1, 0, '', '', true);
+
+$pdf->setFontSpacing(0);
+$txt=sprintf ("%s %85s %12s","COLOTM S.A.S.", "035-12", "" );
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+65, $y);
+
+$y=$y+9;
+$pdf->setFontSpacing($spacing);
+
+$txt="52823309";//sprintf ("%s %13s ",$cliente->getCaIdalterno(),$cliente->getCaDigito());
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x, $y);
+
+$txt="8";
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+56, $y);
+
+$pdf->setFontSpacing(0);
+$txt=sprintf ("%30s %30s %30s %30s","Yepes","Leon","Sandra", "Lucia" );
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+42, $y);
+
+
+//Transportador
+
+$y=$y+8;
+$pdf->setFontSpacing($spacing);
+$txt="900451936";//sprintf ("%s %13s ",$cliente->getCaIdalterno(),$cliente->getCaDigito());
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x, $y);
+
+$txt="8";
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+56, $y);
+
+
+
+//garantia
+$pdf->setFontSpacing(0);
+$y=$y+4;
+
+$txt="1";//sprintf ("%s %13s ",$cliente->getCaIdalterno(),$cliente->getCaDigito());
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+23, $y);
+
+$txt="2";
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+50, $y);
+
+$txt="";
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+80, $y);
+
+
+$txt=  number_format("1133400000");
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+130, $y);
+
+
+$txt=  "2013";
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+180, $y);
+
+$txt=  "05";
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+190, $y);
+
+$txt=  "22";
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+200, $y);
+
+$y=$y+9;
+//datos de la operacion
+$txt="X";
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+54, $y);
+
+//$reporte = new Reporte();
+$repotm=$reporte->getRepOtm();
+$txt=$repotm->getOrigenimp()->getCaCiudad();
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+83, $y);
+
+
+$y=$y+9;
+
+$txt=$repotm->getCaManifiesto();
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+54, $y);
+
+
+$fechaArribo = explode("-",$repotm->getCaFcharribo());
+$txt=$fechaArribo[0]." ".$fechaArribo[1]." ".$fechaArribo[2];
+$txt=sprintf ("%4s %5s %6s",$fechaArribo[0],$fechaArribo[1],$fechaArribo[2]);
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+102, $y);
+
+
+$txt=$repotm->getCaDoctransporte();
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+150, $y);
+
+$fechaArribo = explode("-",$repotm->getCaFchdoctransporte());
+$txt=sprintf ("%4s %5s %6s",$fechaArribo[0],$fechaArribo[1],$fechaArribo[2]);
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+180, $y);
+
+$y=$y+9;
+
+$txt=$reporte->getBodega()->getCaNombre();
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x, $y);
+
+$txt=$repotm->getCaValorfob();
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+150, $y);
+
+//$ciu_ori = ParametroTable::retrieveByCaso("CU111", null, $repotm->getOrigenimp() );
+$c = ParametroTable::retrieveQueryByCaso( "CU111", null, $repotm->getOrigenimp() );
+$ciu_ori=$c->fetchOne();
+if($ciu_ori)
+    $txt=$ciu_ori->getCaIdentificacion();
+else
+    $txt="";
+$pdf->MultiCell(500, 10, $txt, 0, 'L', 0, 1, $x+170, $y);
+
+//retrieveByCaso( $casoUso, $valor1=null, $valor2=null, $id=null ){
+//$reporte->getBodega()->getCaNombre()
+
+// reset pointer to the last page
+$pdf->lastPage();
+
+//Close and output PDF document
+$pdf->Output('example.pdf', 'D');
+
+       exit;
+?>
