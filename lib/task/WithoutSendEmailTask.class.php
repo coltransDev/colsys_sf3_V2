4b5900007c0d90b2ede5b13521f61ecf0e189b3a
Andrea L. Ramirez <alramirez@coltrans.com.co>
2012-10-11 21:20:24 +0000

    [Task] Correo con listado de emails sin despachar

---
+++ lib/task/WithoutSendEmailTask.class.php
@@ -0,0 +1,71 @@
+<?php
+ 
+class WithoutSendEmailTask extends sfDoctrineBaseTask
+{
+  protected function configure()
+  {
+    $this->namespace        = 'colsys';
+    $this->name             = 'without-send-email';
+    $this->briefDescription = 'Envia listado con emails que llevan más de un día sin despacharse';
+    $this->detailedDescription = <<<EOF
+The [circularClientes|INFO] task does things.
+Call it with:
+
+  [php symfony circularClientes|INFO]
+EOF;
+    // add arguments here, like the following:
+    //$this->addArgument('application', sfCommandArgument::REQUIRED, 'The application name');
+    // add options here, like the following:
+    //$this->addOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev');
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    $this->configuration = ProjectConfiguration::getApplicationConfiguration('colsys', 'cli', true );
+    
+    
+	
+	// Borra las dos líneas siguientes si no utilizas una base de datos
+	$databaseManager = new sfDatabaseManager($this->configuration);
+	$databaseManager->loadConfiguration();
+    sfContext::createInstance($this->configuration)->dispatch();
+    sfContext::getInstance()->getRequest()->setParameter("layout", "email");
+		
+	$listado = Doctrine::getTable("Email")
+                        ->createQuery("e")
+                        ->addWhere("e.ca_fchenvio IS NULL")
+                        ->addWhere("e.ca_fchcreado <= ? ", date("Y-m-d H:i:s", time()-86400))
+                        ->execute();
+	
+    if(count($listado)>0){
+            
+            $email = new Email();		
+            $email->setCaUsuenvio("Administrador");
+            $email->setCaTipo( "Error Email" );
+            $email->setCaFrom( "no-reply@coltrans.com.co" );
+            $email->setCaFromname( "Colsys Notificaciones" );
+            $email->setCaSubject(date("Y-m-d").": Correos sin despachar");
+            $email->setCaAddress("alramirez@coltrans.com.co");
+            $email->setCaAddress("maquinche@coltrans.com.co");
+            $email->setCaAddress("clopez@coltrans.com.co");
+            $email->setCaAddress("grmartinez@coltrans.com.co");
+            $email->setCaAddress("falopez@coltrans.com.co");
+
+            $texto = "Los siguientes correos no han sido despachados \n\n<br /><br />" ;
+            
+            
+            $texto .= "<table border='0'><tr><th>Enviado Por:</th> <th>Tipo</th> <th>Asunto</th> <th>Fch. Creado</th></tr>";
+            foreach( $listado as $lista ){
+                $fchCreado = date("Y-m-d", $lista->getCaFchcreado());
+                $texto .= "<tr><td>".$lista->getCaUsuenvio()."</td> <td>".$lista->getCaTipo()."</td> <td>".$lista->getCaSubject()."</td> <td>".$fchCreado."</td></tr>";
+            }
+            $texto .= "</table>";
+            
+            $email->setCaBodyhtml($texto);
+            
+            $email->save();
+            
+	}		
+  }
+}
+?>
