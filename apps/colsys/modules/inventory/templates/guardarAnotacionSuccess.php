059b3b5c47bde794b4f3d6e9dc2b035853318420
Andrea L. Ramirez <alramirez@coltrans.com.co>
2012-10-11 17:10:54 +0000

    [inventory] Actualizacion logo ok

--- apps/colsys/modules/inventory/templates/guardarAnotacionSuccess.php
+++ apps/colsys/modules/inventory/templates/guardarAnotacionSuccess.php
@@ -6,7 +6,7 @@
  */
 ?>
 <div align="center">
-	<h3>Sus comentarios se han guardado correctamente. Estaremos respondiendo a la menor brevedad</h3>
+	<?=image_tag("32x32/agt_action_success.gif")?><h3>Sus comentarios se han guardado correctamente. Estaremos respondiendo a la menor brevedad</h3>
 	<br />
 	<a href="<?=url_for("homepage/index")?>">Haga click aca para volver</a>
 </div>
--- apps/colsys/modules/inventory/templates/recordatorioSuccess.php
+++ apps/colsys/modules/inventory/templates/recordatorioSuccess.php
@@ -6,6 +6,6 @@
  */
 ?>
 <div align="center">
-	<h3>El recordatorio ha sido enviado correctamente.</h3>
+	<?=image_tag("32x32/agt_action_success.gif")?><h3>El recordatorio ha sido enviado correctamente.</h3>
 	<br />
 <a href="<?=url_for("inventory/informeMantenimientosRealizados?mes_man=".$mes_man.'&idsucursal='.$idsucursal.'&opcion=buscar')?>">Haga click aca para volver</a>
