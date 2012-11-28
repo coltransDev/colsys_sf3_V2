f15488bb1600341fa1b7a9b849f2ddbf4d74e476
Andrea L. Ramirez <alramirez@coltrans.com.co>
2012-11-28 17:21:04 +0000

    [adminUsers/directorio] Actualización del directorio para comunicarse através de IP

--- plugins/sfSharedApplicationsPlugin/modules/adminUsers/templates/_directorio.php
+++ plugins/sfSharedApplicationsPlugin/modules/adminUsers/templates/_directorio.php
@@ -1,14 +1,18 @@
 <div class="yellowbox">
 
     <?
-    if ($idsucursal == 'BOG' || $idsucursal == 'ABO') {
+    if ($idsucursal == 'BOG' || $idsucursal == 'ABO' || $idsucursal == 'OBO') {
         ?>
         <div align='left'>Para llamar desde <b>Bogotá</b> a <br>
             <ul>
-                <li><b>Medellín:</b> 804 44 + Ext.</li>
-                <li><b>Cali:</b> 804 22 + Ext.</li>
-                <li><b>B/quilla.:</b> 804 55 + Ext.</li>
-                <li><b>Buenaventura:</b> 804 33 + Ext.</li>
+                <li><b>Barranquilla: </b>804 + 55 + Ext.</li>
+                <li><b>Buenaventura: </b>804 + 33  + Ext.</li>
+                <li><b>Bucaramanga: </b>804 + 77 + Ext.</li>
+                <li><b>Cali: </b>804 + Ext. </li>
+                <li><b>Cartagena: </b>804 + 99  + Ext.</li>
+                <li><b>Medellín: </b>804 + 44 + Ext. + Ext.</li>
+                <li><b>Pereira: </b>804 + 66  + Ext.</li>
+                <li><b>Conferencia: </b>804 + 400 + Ext.</li>
             </ul>
         </div>
         <?
@@ -17,60 +21,111 @@
         ?>
         <div align='left'>Para llamar desde <b>Cali</b> a <br>
             <ul>
-                <li><b>Bogotá:</b> 84 11 + Ext.</li>
-                <li><b>Medellín:</b> 84 44 + Ext.</li>
-                <li><b>B/quilla.:</b> 84 55 + Ext.</li>
-                <li><b>Buenaventura:</b> 804 33 + Ext.</li>
+                <li><b>Barranquilla: </b>55 + Ext.</li>
+                <li><b>Bogotá: </b>11 + Ext. + Ext.</li>
+                <li><b>Buenaventura: </b>33 + Ext.</li>
+                <li><b>Bucaramanga: </b>77 + Ext.</li>
+                <li><b>Cartagena: </b>99 + Ext.</li>
+                <li><b>Medellín: </b>44 + Ext. + Ext.</li>
+                <li><b>Pereira: </b>66 + Ext.</li>
+                <li><b>Conferencia: </b>400 + Ext.</li>
             </ul>
-
         </div>
         <?
     }
-    if ($idsucursal == 'MDE' || $idsucursal == 'AMD') {
-        ?>
-        <div align='left'>Para llamar desde <b>Cali</b> a <br>
-            <ul>
-                <li><b>Bogotá:</b> 84 11 + Ext.</li>
-                <li><b>Medellín:</b> 84 44 + Ext.</li>
-                <li><b>B/quilla.:</b> 84 55 + Ext.</li>
-                <li><b>Buenaventura:</b> 804 33 + Ext.</li>
-            </ul>
-
-        </div>
-        <?
-    }
-    if ($idsucursal == 'MDE' || $idsucursal == 'AMD') {
+    if ($idsucursal == 'MDE' || $idsucursal == 'AMD' || $idsucursal == 'OMD') {
         ?>
         <div align='left'>Para llamar desde <b>Medellín</b> a <br>
             <ul>
-                <li><b>Bogotá:</b> 804 11 + Ext.</li>
-                <li><b>Cali:</b> 804 22 + Ext.</li>
-                <li><b>B/quilla.:</b> 804 55 + Ext.</li>
-                <li><b>Buenaventura:</b> 804 33 + Ext.</li>
+                <li><b>Barranquilla: </b>804 + 55 + Ext.</li>
+                <li><b>Bogotá: </b>804 + 11 + Ext. + Ext.</li>
+                <li><b>Buenaventura: </b>804 + 33 + Ext.</li>
+                <li><b>Bucaramanga: </b>804 + 77 + Ext.</li>
+                <li><b>Cali: </b>804 + Ext.</li>
+                <li><b>Cartagena: </b>804 + 99  + Ext.</li>
+                <li><b>Pereira: </b>804 + 66 + Ext.</li>
+                <li><b>Conferencia: </b>804 + 400 + Ext.</li>
             </ul>
         </div>
         <?
-    }
+    }    
     if ($idsucursal == 'BAQ' || $idsucursal == 'ABA') {
         ?>
         <div align='left'>Para llamar desde <b>Barranquilla</b> a <br>
             <ul>
-                <li><b>Bogotá:</b> 711 11 + Ext.</li>
-                <li><b>Medellín:</b> 711 44 + Ext.</li>
-                <li><b>Cali.:</b> 711 22 + Ext.</li>
+                <li><b>Bogotá: </b>7+ 11 + Ext. + Ext.</li>
+                <li><b>Buenaventura: </b>7+ 33 + Ext.</li>
+                <li><b>Bucaramanga: </b>7+ 77 + Ext.</li>
+                <li><b>Cali: </b>7+ Ext.</li>
+                <li><b>Cartagena: </b>7+ 99 + Ext.</li>
+                <li><b>Medellín: </b>7+ 44 + Ext. + Ext.</li>
+                <li><b>Pereira: </b>7+ 66 + Ext.</li>
+                <li><b>Conferencia: </b>7+ 400 + Ext.</li>
             </ul>
         </div>
         <?
     }
-    if ($idsucursal == 'BUN' || $idsucursal == 'ABU') {
+    if ($idsucursal == 'BUN' || $idsucursal == 'ABU' || $idsucursal == 'OBU') {
         ?>
         <div align='left'>Para llamar desde <b>Buenaventura</b> a <br>
             <ul>
-                <li><b>Bogotá:</b> 87 11 + Ext.</li>
-                <li><b>Cali:</b> 87 33 + Ext.</li>
-                <li><b>Medell&iacute;n:</b> 87 44 + Ext.</li>
+                <li><b>Bogotá: </b>87+ 11 + Ext. + Ext.</li>
+                <li><b>Barranquilla: </b>87+ 55 + Ext.</li>
+                <li><b>Bucaramanga: </b>87 +77 + Ext.</li>
+                <li><b>Cali: </b>87+ Ext.</li>
+                <li><b>Cartagena: </b>87+ 99 + Ext.</li>
+                <li><b>Medellín: </b>87+ 44 + Ext. + Ext.</li>
+                <li><b>Pereira: </b>87+ 66 + Ext.</li>
+                <li><b>Conferencia: </b>87+ 400 + Ext.</li>
             </ul>
-
+        </div>
+        <?
+    }
+    if ($idsucursal == 'CTG' || $idsucursal == 'ACT' || $idsucursal == 'OCT' ) {
+        ?>
+        <div align='left'>Para llamar desde <b>Cartagena</b> a <br>
+            <ul>
+                <li><b>Bogotá: </b>7+ 11 + Ext. + Ext.</li>
+                <li><b>Buenaventura: </b>7+ 33 + Ext.</li>
+                <li><b>Bucaramanga: </b>7+ 77 + Ext.</li>
+                <li><b>Cali: </b>7+ Ext.</li>
+                <li><b>Cartagena: </b>7+ 99 + Ext.</li>
+                <li><b>Medellín: </b>7+ 44 + Ext. + Ext.</li>
+                <li><b>Pereira: </b>7+ 66 + Ext.</li>
+                <li><b>Conferencia: </b>7+ 400 + Ext.</li>
+            </ul>
+        </div>
+        <?
+    }
+    if ($idsucursal == 'PEI') {
+        ?>
+        <div align='left'>Para llamar desde <b>Pereira</b> a <br>
+            <ul>
+                <li><b>Bogotá: </b>86+ 11 + Ext. + Ext.</li>
+                <li><b>Barranquilla: </b>86+ 55 + Ext.</li>
+                <li><b>Buenaventura: </b>86+ 33 + Ext.</li>
+                <li><b>Bucaramanga: </b>86 +77 + Ext.</li>
+                <li><b>Cali: </b>86+ Ext.</li>
+                <li><b>Cartagena: </b>86+ 99 + Ext.</li>
+                <li><b>Medellín: </b>86+ 44 + Ext. + Ext.</li>
+                <li><b>Conferencia: </b>86+ 400 + Ext.</li>
+            </ul>
+        </div>
+        <?
+    }
+    if ($idsucursal == 'ABG' || $idsucursal == 'BGA') {
+        ?>
+        <div align='left'>Para llamar desde <b>Bucaramanga</b> a <br>
+            <ul>
+                <li><b>Bogotá: </b>11 + Ext. + Ext.</li>
+                <li><b>Barranquilla: </b>55 + Ext.</li>
+                <li><b>Buenaventura: </b>33 + Ext.</li>
+                <li><b>Cali: </b>Ext. Directamente</li>
+                <li><b>Cartagena: </b>99 + Ext.</li>
+                <li><b>Medellín: </b>44 + Ext. + Ext.</li>
+                <li><b>Pereira: </b>66 + Ext.</li>
+                <li><b>Conferencia: </b>400 + Ext.</li>
+            </ul>
         </div>
         <?
     }
