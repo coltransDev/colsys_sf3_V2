0173f6a30319e1bcf0d65cb4c9342028fa86c32f
Carlos G. Lopez <clopez@coltrans.com.co>
2013-01-30 16:18:46 +0000

    Ticket # 9274 Env: ACa000716 - ENTREGA DE ANTECEDENTES

--- apps/colsys/modules/antecedentes/actions/components.class.php
+++ apps/colsys/modules/antecedentes/actions/components.class.php
@@ -9,99 +9,93 @@
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
 class antecedentesComponents extends sfComponents {
+   //private $filetypes = array("MBL", "HBL");
 
-    //private $filetypes = array("MBL", "HBL");
-    
-    /**
-     *
-     */
-    public function executePanelReportesAntecedentes() {
+   /**
+    *
+    */
+   public function executePanelReportesAntecedentes() {
+      
+   }
 
+   public function executePanelReportesOTMAntecedentes() {
+      
+   }
 
+   public function executePanelEntregaAntecedentes() {
+      
+   }
 
-    }
-    
-    public function executePanelReportesOTMAntecedentes() {
+   /**
+    *
+    */
+   public function executePanelMasterAntecedentes() {
+      
+   }
 
+   public function executePanelMasterOTMAntecedentes() {
+      
+   }
 
+   /**
+    *
+    */
+   public function executeGridDropZone() {
+      
+   }
 
-    }
+   /**
+    *
+    */
+   public function executeListaReportesMaster() {
+      $this->reportes = Doctrine::getTable("Reporte")
+              ->createQuery("r")
+              ->addWhere("r.ca_master = ? ", $this->master)
+              ->execute();
+   }
 
-    /**
-     *
-     */
-    public function executePanelMasterAntecedentes() {
+   /**
+    *
+    */
+   public function executeWidgetReporteAntecedentes() {
+      
+   }
 
-        
-    }
-    
-     public function executePanelMasterOTMAntecedentes() {
+   /**
+    *
+    */
+   public function executeWidgetHBLAntecedentes() {
+      
+   }
 
-        
-    }
+   /**
+    *
+    */
+   public function executeFileManager() {
+      $this->edit = (($this->getRequestParameter("format") == "" && $this->format == "") ? true : false);
 
-    /**
-     *
-     */
-    public function executeGridDropZone() {
+      $folder = "Referencias" . DIRECTORY_SEPARATOR . $this->ref->getCaReferencia();
+      $directory = sfConfig::get('app_digitalFile_root') . DIRECTORY_SEPARATOR . $folder . DIRECTORY_SEPARATOR;
 
-    }
+      if (!is_dir($directory)) {
+         @mkdir($directory, 0777, true);
+      }
+      chmod($directory, 0777);
 
-    /**
-     *
-     */
-    public function executeListaReportesMaster() {
+      $archivos = sfFinder::type('file')->maxDepth(0)->in($directory);
+      //echo print_r($archivos);
 
-        $this->reportes = Doctrine::getTable("Reporte")
-                          ->createQuery("r")
-                          ->addWhere("r.ca_master = ? ", $this->master )
-                          ->execute();
-        
-    }
+      $filenames = array();
 
-    /**
-     *
-     */
-    public function executeWidgetReporteAntecedentes() {
+      $fileTypes = $this->filetypes;
 
-    }
+      foreach ($archivos as $archivo) {
+         $file = explode("/", $archivo);
+         $filenames[]["file"] = $file[count($file) - 1];
+      }
 
-    /**
-     *
-     */
-    public function executeWidgetHBLAntecedentes() {
-
-    }
-
-    /**
-     *
-     */
-    public function executeFileManager() {        
-        $this->edit=(($this->getRequestParameter("format")=="" && $this->format=="")?true:false);
-        
-        $folder = "Referencias" . DIRECTORY_SEPARATOR . $this->ref->getCaReferencia();
-        $directory = sfConfig::get('app_digitalFile_root') . DIRECTORY_SEPARATOR . $folder . DIRECTORY_SEPARATOR;
-
-        if (!is_dir($directory)) {
-            @mkdir($directory, 0777, true);
-        }
-        chmod ( $directory , 0777 );
-
-        $archivos = sfFinder::type('file')->maxDepth(0)->in($directory);
-        //echo print_r($archivos);
-
-        $filenames = array();
-
-        $fileTypes = $this->filetypes;
-
-        foreach ($archivos as $archivo) {            
-            $file=explode("/", $archivo);
-            $filenames[]["file"] = $file[count($file)-1];
-        }
-
-        $this->folder = $folder;
-        $this->filenames = $filenames;
-        
-    }
+      $this->folder = $folder;
+      $this->filenames = $filenames;
+   }
 
 }
