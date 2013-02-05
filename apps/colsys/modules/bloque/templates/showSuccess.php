7fb5df98f95b304bd602c7303b3b8b8e1cc8c650
Gabriel Martinez <gmartinez@coltrans.com.co>
2013-02-05 21:40:30 +0000

    seagrego un nuevo metodo para que en la vista previo del bloque se muestren todas las pregubntas, incluso las inactivas

--- apps/colsys/modules/bloque/templates/showSuccess.php
+++ apps/colsys/modules/bloque/templates/showSuccess.php
@@ -1,35 +1,35 @@
-<h1>Detalles Bloque: <?php echo $bloque->getCaTitulo() ?></h1>
+<h1>Detalles Bloque: <? echo $bloque->getCaTitulo() ?></h1>
 <table>
     <tbody>
         <tr>
             <th>Id:</th>
-            <td><?php echo $bloque->getCaId() ?></td>
+            <td><? echo $bloque->getCaId() ?></td>
         </tr>
         <tr>
             <th>Formulario:</th>
-            <td><?php echo $bloque->getFormulario()->ca_titulo ?></td>
+            <td><? echo $bloque->getFormulario()->ca_titulo ?></td>
         </tr>
         <tr>
             <th>Titulo:</th>
-            <td><?php echo $bloque->getCaTitulo() ?></td>
+            <td><? echo $bloque->getCaTitulo() ?></td>
         </tr>
         <tr>
             <th>Introduccion:</th>
-            <td><?php echo html_entity_decode($bloque->getCaIntroduccion()) ?></td>
+            <td><? echo html_entity_decode($bloque->getCaIntroduccion()) ?></td>
         </tr>
         <tr>
             <th>Activo:</th>
-            <td><?php echo $bloque->getTextoBooleano($bloque->ca_activo) ?>
+            <td><? echo $bloque->getTextoBooleano($bloque->ca_activo) ?>
             </td>
         </tr>
         <tr>
             <th>No de preguntas:</th>
-            <td><?php echo sizeof($bloque->getTbPreguntas()); ?>
+            <td><? echo sizeof($bloque->getTbPreguntas()); ?>
             </td>
         </tr>
         <tr>
             <th>Tipo:</th>
-            <td><?php echo $bloque->getTipoBloque($bloque->ca_tipo); ?>
+            <td><? echo $bloque->getTipoBloque($bloque->ca_tipo); ?>
             </td>
         </tr>
 
@@ -48,22 +48,22 @@
         </tr>
     </thead>
     <tbody>
-        <?php foreach ($bloque->getPreguntasOrdenadas($bloque->ca_id) as $pregunta): ?>
+        <? foreach ($bloque->getTodasPreguntasOrdenadas($bloque->ca_id) as $pregunta): ?>
             <tr>
-                <td><a target="_BLANK" href="<?php echo url_for('pregunta/show?ca_id=' . $pregunta->getCaId()) ?>"><?php echo $pregunta->getCaId() ?></a></td>
-                <td><?php echo html_entity_decode($pregunta->getCaTexto()) ?></td>
-                <td><?php echo html_entity_decode($pregunta->getTextoBooleano($pregunta->getCaObligatoria())) ?></td>
-                <td><?php echo html_entity_decode($pregunta->getTextoBooleano($pregunta->getCaActivo())) ?></td>
-                <td><?php echo $pregunta->getCaOrden() ?></td>
+                <td><a target="_BLANK" href="<? echo url_for('pregunta/show?ca_id=' . $pregunta->getCaId()) ?>"><? echo $pregunta->getCaId() ?></a></td>
+                <td><? echo html_entity_decode($pregunta->getCaTexto()) ?></td>
+                <td><? echo html_entity_decode($pregunta->getTextoBooleano($pregunta->getCaObligatoria())) ?></td>
+                <td><? echo html_entity_decode($pregunta->getTextoBooleano($pregunta->getCaActivo())) ?></td>
+                <td><? echo $pregunta->getCaOrden() ?></td>
                 <td>
-                    <a class="" href="<?php echo url_for('pregunta/show?ca_id=' . $pregunta->getCaId()) ?>"><img title="Ver Detalle" alt="Ver Detalle" src="/images/formularios/detalle.png"></a>
-                    <a class="" href="<?php echo url_for('pregunta/edit?ca_id=' . $pregunta->getCaId()) ?>"><img title="Editar" alt="Editar" src="/images/formularios/edit.gif"></a>
-                    <a class="" target="_blank" href="<?php echo url_for('pregunta/vistaPrevia?ca_id=' . $pregunta->getCaId()) ?>"><img title="Previsualizar" title="Previsualizar" src="/images/formularios/verx16.png"></a>
+                    <a class="" href="<? echo url_for('pregunta/show?ca_id=' . $pregunta->getCaId()) ?>"><img title="Ver Detalle" alt="Ver Detalle" src="/images/formularios/detalle.png"></a>
+                    <a class="" href="<? echo url_for('pregunta/edit?ca_id=' . $pregunta->getCaId()) ?>"><img title="Editar" alt="Editar" src="/images/formularios/edit.gif"></a>
+                    <a class="" target="_blank" href="<? echo url_for('pregunta/vistaPrevia?ca_id=' . $pregunta->getCaId()) ?>"><img title="Previsualizar" title="Previsualizar" src="/images/formularios/verx16.png"></a>
                 </td>
             </tr>
-        <?php endforeach; ?>
+        <? endforeach; ?>
     </tbody>
 </table>
 <!--
 <hr />
-<a target="_BLANK" class="accion" href="<?php echo url_for('pregunta/nuevo?ca_id=' . $bloque->getCaId()) ?>"><img src="/images/formularios/add.gif"> Agregar Pregunta</a>-->
+<a target="_BLANK" class="accion" href="<? echo url_for('pregunta/nuevo?ca_id=' . $bloque->getCaId()) ?>"><img src="/images/formularios/add.gif"> Agregar Pregunta</a>-->
