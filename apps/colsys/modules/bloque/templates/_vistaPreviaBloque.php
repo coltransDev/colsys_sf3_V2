3dafdda0497a7869c1ae89d00631d70985e86654
Gabriel Martinez <gmartinez@coltrans.com.co>
2013-02-05 21:49:22 +0000

    pequeño ajuste

--- apps/colsys/modules/bloque/templates/_vistaPreviaBloque.php
+++ apps/colsys/modules/bloque/templates/_vistaPreviaBloque.php
@@ -1,15 +1,15 @@
-<div class="bloque" id="b_<?php echo html_entity_decode($bloque->ca_id) ?>">
-    <p class="intro-bloque"><?php echo html_entity_decode($bloque->ca_introduccion) ?></p>
-    <?php
+<div class="bloque" id="b_<? echo html_entity_decode($bloque->ca_id) ?>">
+    <p class="intro-bloque"><? echo html_entity_decode($bloque->ca_introduccion) ?></p>
+    <?
     foreach ($bloque->getPreguntasOrdenadas($bloque->ca_id) as $pregunta):
         include_partial('pregunta/vistaPreviaPregunta', array('pregunta' => $pregunta, 'servicios' => $servicios));
     endforeach;
     ?>
 </div>
-<?php if ($bloque->ca_tipo==1){ ?>
+<? if ($bloque->ca_tipo==1){ ?>
 
 <div id="indicator" style="display: none"></div>
-<?php } ?>
+<? } ?>
 
 
 
