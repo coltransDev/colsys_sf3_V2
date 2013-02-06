40dd55dbfd3f6551cab9c8c25c6d3aede0bd264c
Gabriel Martinez <gmartinez@coltrans.com.co>
2013-02-06 14:57:39 +0000

    ajuste para validar el bloque de servicios

---
+++ apps/colsys/modules/pregunta/templates/_pMultiple.php
@@ -0,0 +1,42 @@
+<? include_partial('pregunta/error', array('pregunta' => $pregunta)); ?>
+<ul class="seleccion_multiple">
+    <? $cont = 1; ?>
+    <? foreach ($pregunta->getOpcionesOrdenadas($pregunta->ca_id) as $opcion): ?>
+        <li class="box-item">
+            <label class="box-label">
+                <input type="checkbox" id="grupo_<? echo $pregunta->ca_id ?>_<? echo $cont ?>" class="checkbox" value="<? echo html_entity_decode($opcion->ca_texto) ?>" name="<? echo $cont ?>">
+                <? echo html_entity_decode($opcion->ca_texto) ?>
+            </label>
+        </li>
+        <? $cont++; ?>
+    <? endforeach; ?>
+</ul>
+
+<script>
+    function verificar_pmultiple_servicios(){
+        var inputs = document.getElementById("f_<? echo $pregunta->getBloque()->getFormulario()->getCaId() ?>").elements;
+        var i=0, j=0;
+
+        if($pregunta->getBloque()){
+            for(i;i<inputs.length;i++){
+                if(inputs[i].type == 'checkbox')
+                {
+                    if (inputs[i].checked == false)
+                    {
+                        j++;
+                    }
+                }
+            }
+            alert("numero de checkboxes: "+j);
+            if(j==<? echo sizeof($pregunta->getTbOpciones()) ?>){
+                alert("Debe seleccionar almenos un servicio");
+                return false;
+            }
+            else {
+                alert("Ok");
+                return true;
+            }
+        }
+        return true;
+    }
+</script>
