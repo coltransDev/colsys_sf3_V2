3d5d000e1091b3f2f2026ff6a66b8eadc2245ba5
Gabriel Martinez <gmartinez@coltrans.com.co>
2013-02-08 14:54:16 +0000

    soporte para el logo de colmas

--- apps/colsys/modules/formulario/templates/exitoSuccess.php
+++ apps/colsys/modules/formulario/templates/exitoSuccess.php
@@ -1,5 +1,9 @@
 <div class="formulario-cabecera">
-    <img class="logo-topmenu" src="/images/logos/coltrans.png" alt="Coltrans SA" />
+    <? if ($formulario->ca_color == 1) { ?>
+        <img class="logo-topmenu" src="/images/logos/colmas.png" alt="Colmas SA" />    
+    <? } else { ?>
+        <img class="logo-topmenu" src="/images/logos/coltrans.png" alt="Coltrans SA" />
+    <? } ?>
 </div>
 <div class="resultado">
     <h1>Muchas gracias por su tiempo!!!</h1>
@@ -10,8 +14,14 @@
     <br>
     <br>
     <br>
-    <p class="align-center">Puede continuar visitando nuestro sitio web  <a href="http://www.coltrans.com.co/" target="_BLANK">COLTRANS S.A.S<a></p>
-                </div>
+        <? if ($formulario->ca_color == 1) { ?>
+            <p class="align-center">Puede continuar visitando nuestro sitio web  <a href="http://www.colmas.com.co/" target="_BLANK">COLMAS LTDA.<a></p>    
+        <? } else { ?>
+            <p class="align-center">Puede continuar visitando nuestro sitio web  <a href="http://www.coltrans.com.co/" target="_BLANK">COLTRANS S.A.S<a></p>
+        <? } ?>    
+    
+    
+</div>
 
 
 
