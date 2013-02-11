f08d7f34aa861253148a0b2c9c5f8bd11a9133d5
Mauricio A Quinche <maquinche@coltrans.com.co>
2013-02-11 20:19:49 +0000

    [email] Actualizacion

--- plugins/sfSharedApplicationsPlugin/modules/email/actions/actions.class.php
+++ plugins/sfSharedApplicationsPlugin/modules/email/actions/actions.class.php
@@ -9,9 +9,7 @@
  * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
  */
 class emailActions extends sfActions
-{ 
-
-
+{
     /*
 	* Permite ver un email
     * @param sfRequest $request A request object
@@ -28,30 +26,49 @@ class emailActions extends sfActions
 	* Permite ver un email
 	*/
 	public function executeVerAttachment( $request ){
+        
+        $user = $this->getUser();
+        
+ 
         $id= base64_decode($request->getParameter("id"));
-		$this->archivo = sfConfig::get('app_digitalFile_root').DIRECTORY_SEPARATOR.$id;
-       
-        if(!file_exists($this->archivo) && !file_exists($this->archivo.".gz")){
+        $this->archivo = sfConfig::get('app_digitalFile_root').DIRECTORY_SEPARATOR.$id;
+
+        $pos1=stripos($this->archivo, ".pdf");
+
+        $file2="";
+        if ($pos1 !== false) {
+            $file2=str_replace(".pdf", "-P.pdf", $this->archivo);
+        }
+        if(file_exists($file2) || file_exists($file2.".gz"))
+        {
+            $this->archivo=$file2;
+        }
+        if(!file_exists($this->archivo) && !file_exists($this->archivo.".gz") && !file_exists($file2)){
             $this->archivo = str_replace(" ", "_", $this->archivo );
             if(!file_exists($this->archivo) && !file_exists($this->archivo.".gz")){        
-            
+
                 if(strpos($id, "reportes/")!==false){
                     $array = explode("/", $id);
                     $folderRep = substr($array[1], -4, 4 ); 
-                    $id = $array[0]."/".$folderRep."/".$array[1]."/".$array[2];
-                    $this->archivo = sfConfig::get('app_digitalFile_root').DIRECTORY_SEPARATOR.$id;
-                    if(!file_exists($this->archivo) && !file_exists($this->archivo.".gz") ){                    
-                        $this->forward404("No se encuentra el archivo especificado");
+                    $id = $array[0]."/".$folderRep."/".$array[1]."/".$array[2];                        
+                    $this->archivo = sfConfig::get('app_digitalFile_root').DIRECTORY_SEPARATOR.$id;                        
+
+
+                    if(!file_exists($this->archivo) && !file_exists($this->archivo.".gz")  && !file_exists($file2)  ){
+                        $this->forward404("No se encuentra el archivo especificado . ".$id);
                     }                
                 }else{
-                    $this->forward404("No se encuentra el archivo especificado");
+                    $this->forward404("No se encuentra el archivo especificado : ".$id);
                 }
             }
         }
-        
         if( file_exists($this->archivo.".gz")){
             $this->archivo.=".gz";
         }
+        else if( file_exists($file2)){
+            $this->archivo=$file2;
+        }
+        
         
 
 
