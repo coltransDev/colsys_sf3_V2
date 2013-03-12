d472f68a293f38d0b7f29f9fe350ae76071392f1
Gabriel Martinez <gmartinez@coltrans.com.co>
2013-03-12 16:35:26 +0000

    guardando en el repositorio

---
+++ apps/colsys/templates/tablet/formulario.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <?php include_http_metas() ?>
+        <?php include_metas() ?>
+        <title>
+            <?php if (!include_slot('title')): ?>
+                Coltrans S.A.S. - Agente de Carga Internacional
+            <?php endif; ?>
+        </title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <?php include_stylesheets() ?>
+        <link rel="stylesheet" type="text/css" media="screen"
+              href="/css/formulario.css" />
+              <?php include_javascripts() ?>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+             <meta content="IE=edge" http-equiv="X-UA-Compatible"></meta>
+    </head>     
+    <body id="body-tablet">
+        <div id="wrapper">
+            <?php
+            ?>
+            <div class="clear-both">
+            </div>
+            <div class="contenido">
+            <div class="contenedor-formulario-tablet">
+                <?php echo $sf_content ?>
+            </div>
+        </div>
+        <div class="clear-both"></div>
+
+        </div>
+    </body>
+</html>
+
+
+
+
+
