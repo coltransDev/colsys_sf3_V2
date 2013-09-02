8ed8b3e8362d9b91fb4a8a7899933b566d9d9f40
Andrea L. Ramirez <alramirez@coltrans.com.co>
2013-09-02 16:24:21 +0000

    [inoReportes] index.Success Menú para TPLogistics

--- apps/colsys/modules/inoReportes/templates/indexSuccess.php
+++ apps/colsys/modules/inoReportes/templates/indexSuccess.php
@@ -6,9 +6,24 @@
             <td class="titulo" width="110">Nombre</td>
             <td class="titulo">Detalle</td>
         </tr>
+        <?if($empresa=='TPLogistics'){?>
+        <tr>  
+            <td class="mostrar"><?=link_to("Cuadro INO Inicial", "inoReportes/cuadroIno") ?></td>
+            <td class="mostrar">Informe para Gerencia Cuadro INO hasta Febrero 2013</td>
+        </tr>
+        <tr>  
+            <td class="mostrar"><?=link_to("Cuadro INO Complemento", "inoReportes/cuadroInoComplemento") ?></td>
+            <td class="mostrar">Informe para Gerencia Cuadro INO a partir Marzo 2013</td>
+        </tr>
+        <?}else{?>
         <tr>  
             <td class="mostrar"><?=link_to("Cuadro INO", "inoReportes/cuadroIno") ?></td>
             <td class="mostrar">Informe para Gerencia Cuadro INO</td>
+        </tr>
+        <?}?>
+        <tr>  
+            <td class="mostrar"><?=link_to("Reporte de Comisiones", "inoReportes/reporteComisiones") ?></td>
+            <td class="mostrar">Reporte de Comisiones por vendedor</td>
         </tr>
         <tr>  
             <td class="mostrar"><?=link_to("Reporte de Comisiones", "inoReportes/reporteComisiones") ?></td>
@@ -18,6 +33,7 @@
             <td class="mostrar"><?=link_to("Listado de comprobantes", "inoReportes/listadoComprobantes") ?></td>
             <td class="mostrar">Lista los comprobantes de una referencia para efectos de auditoria</td>
         </tr>
+        <!--
         <!--
         <!--
         <tr>  
