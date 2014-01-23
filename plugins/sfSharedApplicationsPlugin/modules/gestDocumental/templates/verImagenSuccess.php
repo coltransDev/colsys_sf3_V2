d92e062ca7466152b7fb2cd7465e92797af65c18
Mauricio A Quinche <maquinche@coltrans.com.co>
2014-01-23 15:14:01 +0000

    [colsys_sf3] Actualizacion 17 Dig

--- plugins/sfSharedApplicationsPlugin/modules/gestDocumental/templates/verImagenSuccess.php
+++ plugins/sfSharedApplicationsPlugin/modules/gestDocumental/templates/verImagenSuccess.php
@@ -4,7 +4,7 @@
             $h = imagesy($image);
             if($w>$tam_max || $h>$tam_max)
             {
-                $control=($h>=$w);                    
+                $control=($h>=$w);
                 if($control)
                 {
                     $porcen=$tam_max/$h;
@@ -12,11 +12,8 @@
                 else {
                     $porcen=$tam_max/$w;
                 }
-                //echo $porcen;
                 $new_w= $w * $porcen;
                 $new_h= $h * $porcen;
-                //echo $new_w."-".$new_h;
-
                 $im2 = ImageCreateTrueColor($new_w, $new_h);
                 imagecopyResampled ($im2, $image, 0, 0, 0, 0, $new_w, $new_h, $w, $h);
                 $w = imagesx($im2);
