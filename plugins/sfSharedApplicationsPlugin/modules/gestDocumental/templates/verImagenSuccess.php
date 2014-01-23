51ff6f2fca0b77870b1ba4f25df89de5e8edfa1e
Andrea L. Ramirez <alramirez@coltrans.com.co>
2014-01-23 15:06:51 +0000

    Convwersion 17 digitos

--- plugins/sfSharedApplicationsPlugin/modules/gestDocumental/templates/verImagenSuccess.php
+++ plugins/sfSharedApplicationsPlugin/modules/gestDocumental/templates/verImagenSuccess.php
@@ -1,10 +1,7 @@
 <?
-//error_reporting(E_ALL);
     $image = imagecreatefromjpeg($archivo);
-
             $w = imagesx($image);
             $h = imagesy($image);
-            //$tam_max=200;
             if($w>$tam_max || $h>$tam_max)
             {
                 $control=($h>=$w);                    
@@ -24,18 +21,12 @@
                 imagecopyResampled ($im2, $image, 0, 0, 0, 0, $new_w, $new_h, $w, $h);
                 $w = imagesx($im2);
                 $h = imagesy($im2);
-                //echo "<br>".$w."-".$h;
-                //
-                //imagejpeg($im2,$directory.$fileNameMin.".jpg",80);
                 imagejpeg($img2,NULL,80);
-                //$this->im=$im2;
             }
             else
             {
                 imagejpeg($image,NULL,80);
-                //$this->im=$image;
             }
-//imagejpeg($im,NULL,80);
 header ('Content-type: image/jpeg');
 @imagejpeg($im, NULL, 80);
 exit();
