387cbbdbd2d7d53d0b44441e918c8191dca6a7ee
Carlos G. Lopez <clopez@coltrans.com.co>
2014-03-19 14:56:59 +0000

    Se incluye función para validación de formato de hora en HH:MM:SS

--- web/colsys/js/comunes.js
+++ web/colsys/js/comunes.js
@@ -233,6 +233,27 @@ function linkField( idLinkedFld, idDependantFld, includeBlank , defaultVal ){
     
 }
 
+/*
+ * Validación de formato de hora
+ **/
+
+function CheckTime( str ){
+    hora=str.value
+    if (hora=='') {return}
+    if (hora.length>8) {alert("Introdujo una cadena mayor a 8 caracteres");str.value="";str.focus();return}
+    if (hora.length!=8) {alert("Introducir HH:MM:SS");str.value="";str.focus();return}
+    a=hora.charAt(0) //<=2
+    b=hora.charAt(1) //<4
+    c=hora.charAt(2) //:
+    d=hora.charAt(3) //<=5
+    e=hora.charAt(5) //:
+    f=hora.charAt(6) //<=5
+    if ((a==2 && b>3) || (a>2)) {alert("El valor que introdujo en la Hora no corresponde, introduzca un digito entre 00 y 23 horas");str.value="";str.focus();return}
+    if (d>5) {alert("El valor que introdujo en los minutos no corresponde, introduzca un digito entre 00 y 59");str.value="";str.focus();return}
+    if (f>5) {alert("El valor que introdujo en los segundos no corresponde");str.value="";str.focus();return}
+    if (c!=':' || e!=':') {alert("Introduzca el caracter ':' para separar la hora, los minutos y los segundos");str.value="";str.focus();return}
+}
+
 
 
 
