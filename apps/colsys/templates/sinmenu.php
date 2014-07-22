8a241031941e270f6e15e4e4547c4e52f9fcf2a2
Mauricio A Quinche <maquinche@coltrans.com.co>
2014-07-22 21:33:30 +0000

    [templates] nuevo

---
+++ apps/colsys/templates/sinmenu.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+ <?
+    
+    $sfModule=sfContext::getInstance()->getModuleName ();
+    $sfAction=sfContext::getInstance()->getActionName ();
+    ?>
+<?php include_http_metas() ?>
+<?php include_metas() ?>
+<?php include_title() ?>
+
+<link rel="shortcut icon" href="/favicon.ico" />
+
+    
+    <?php 
+    if( (strpos("$sfAction", "Ext4")===false && strpos("$sfAction", "ext4")===false)&& (strpos("$sfAction", "Ext5")===false && strpos("$sfAction", "ext5")===false) && $sfModule."/".$sfAction!="gestDocumental/index")
+    {   
+        include_stylesheets();
+        include_javascripts();
+    }/*else if (strpos("$sfAction", "Ext5")===true || strpos("$sfAction", "ext5")===true){
+
+?>
+<link rel="stylesheet" type="text/css" media="screen" href="/css/coltrans.css" />
+<link rel="stylesheet" type="text/css" media="print" href="/css/print.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="/css/jquery/jquery.tooltip.css" />
+
+<link rel="stylesheet" type="text/css" media="screen" href="/css/menu/menu.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="/css/blog.css" />
+
+
+<link type="text/css" rel="stylesheet" href="/js/ext5/packages/ext-theme-neptune/build/resources/ext-theme-neptune-all-debug.css">
+
+<script type="text/javascript" src="/js/ext5/ext-all.js"></script>
+
+<script type="text/javascript" src="/js/comunes.js"></script>
+<script type="text/javascript" src="/js/jquery/jquery.js"></script>
+<script type="text/javascript" src="/js/jquery/jquery.tooltip.js"></script>
+<script type="text/javascript" src="/js/jquery/jquery.dimensions.js"></script>
+<script type="text/javascript" src="/js/menu/menu.js"></script>
+
+<?
+    }*/else
+    {
+?>
+
+
+<link rel="stylesheet" type="text/css" media="screen" href="/css/coltrans.css" />
+<link rel="stylesheet" type="text/css" media="print" href="/css/print.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="/css/jquery/jquery.tooltip.css" />
+
+<link rel="stylesheet" type="text/css" media="screen" href="/css/menu/menu.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="/css/blog.css" />
+
+
+<link type="text/css" rel="stylesheet" href="/js/ext4/resources/css/ext-all-neptune.css">
+
+<script type="text/javascript" src="/js/ext4/ext-all.js"></script>
+<script type="text/javascript" src="/js/ext4/ux/multiupload/swfobject.js"></script>
+
+<script type="text/javascript" src="/js/comunes.js"></script>
+<script type="text/javascript" src="/js/jquery/jquery.js"></script>
+<script type="text/javascript" src="/js/jquery/jquery.tooltip.js"></script>
+<script type="text/javascript" src="/js/jquery/jquery.dimensions.js"></script>
+<script type="text/javascript" src="/js/menu/menu.js"></script>
+<?
+    }
+?>
+
+
+    <script type="text/javascript" src="/js/loginWindow.js"></script>
+</head>
+<body >
+
+	
+    
+
+<?php echo $sf_content ?>
+
+<div class="footer">	
+	<div class="copyright">
+		<?=sfConfig::get("app_branding_name")?>. Todos los derechos reservados
+        <br />        
+        colsys@<?=$_SERVER["SERVER_ADDR"]?>
+	</div>
+</div>	
+
+
+
+
+<script type="text/javascript">
+
+    $('.qtip').tooltip({track: true});
+    $('.help').tooltip({track: true, fade: 250, opacity: 1, top: -15, extraClass: "pretty fancy" });
+</script>
+
+
+</body>
+</html>
