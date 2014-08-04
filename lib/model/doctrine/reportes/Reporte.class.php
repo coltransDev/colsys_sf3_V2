ecdc6c844e1cc16ec4b79b0579e6d84d31158fc3
Mauricio A Quinche <maquinche@coltrans.com.co>
2014-08-04 15:11:51 +0000

    [reportes] Se modifico el metodo esSeguro, para que traiga de parametros el listado de conceptos parametrizados para el envio de status.

--- lib/model/doctrine/reportes/Reporte.class.php
+++ lib/model/doctrine/reportes/Reporte.class.php
@@ -574,19 +574,19 @@ class Reporte extends BaseReporte {
         else
         {
             $conceptos=array();
-            $c= ParametroTable::retrieveByCaso("CU048");
+            $c= ParametroTable::retrieveByCaso("CU236");
             foreach($c as $s)
             {
                 $conceptos[]=$s->getCaValor();                        
             }
 
             $nreg = Doctrine::getTable("RepGasto")
-                        ->createQuery("r")
-                        ->select("count(*) as nreg")
-                        ->addWhere("r.ca_idreporte = ? ", $this->getCaIdreporte())
-                        ->andWhereIn("r.ca_idrecargo", $conceptos)
-                        ->setHydrationMode(Doctrine::HYDRATE_SINGLE_SCALAR)
-                        ->execute();
+                ->createQuery("r")
+                ->select("count(*) as nreg")
+                ->addWhere("r.ca_idreporte = ? ", $this->getCaIdreporte())
+                ->andWhereIn("r.ca_idrecargo", $conceptos)
+                ->setHydrationMode(Doctrine::HYDRATE_SINGLE_SCALAR)
+                ->execute();
 
             if($nreg>0)
             {
