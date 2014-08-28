1b39bd8de69005287bca0e2a98d184be17a19c24
Andrea L. Ramirez <alramirez@coltrans.com.co>
2014-08-28 17:20:40 +0000

    [menu] Agrega opcion TRM

--- plugins/sfSharedApplicationsPlugin/modules/menu/actions/components.class.php
+++ plugins/sfSharedApplicationsPlugin/modules/menu/actions/components.class.php
@@ -1,58 +1,52 @@
-<?php 
+<?php
+
 /*
-* 
-*/
-class menuComponents extends sfComponents
-{
-	/*
-	* 
-	*/	
-  	public function executeSubmenubar(){				
-		$module = $this->getContext()->getModuleName ();
-		$action = $this->getContext()->getActionName ();
-				
-		$button=array();			
+ * 
+ */
+
+class menuComponents extends sfComponents {
+    /*
+     * 
+     */
+
+    public function executeSubmenubar() {
+        $module = $this->getContext()->getModuleName();
+        $action = $this->getContext()->getActionName();
+
+        $button = array();
         //echo sfConfig::get("sf_plugins_dir");
 
         $user = $this->getUser();
-		$filename=sfConfig::get("sf_app_module_dir").DIRECTORY_SEPARATOR.$module.DIRECTORY_SEPARATOR."templates".DIRECTORY_SEPARATOR."_submenuBar.php";
-		if( file_exists($filename) ){
-			include($filename);
-		}else{
-          $pluginDirs = array(sfConfig::get('sf_plugins_dir').DIRECTORY_SEPARATOR."sfSharedApplicationsPlugin".DIRECTORY_SEPARATOR."modules".DIRECTORY_SEPARATOR.$module.DIRECTORY_SEPARATOR."templates".DIRECTORY_SEPARATOR);
-        
-          foreach ($pluginDirs as $dir)
-          {
-             $filename = $dir."_submenuBar.php";        
-             if( file_exists($filename) ){
-                include($filename);
+        $filename = sfConfig::get("sf_app_module_dir") . DIRECTORY_SEPARATOR . $module . DIRECTORY_SEPARATOR . "templates" . DIRECTORY_SEPARATOR . "_submenuBar.php";
+        if (file_exists($filename)) {
+            include($filename);
+        } else {
+            $pluginDirs = array(sfConfig::get('sf_plugins_dir') . DIRECTORY_SEPARATOR . "sfSharedApplicationsPlugin" . DIRECTORY_SEPARATOR . "modules" . DIRECTORY_SEPARATOR . $module . DIRECTORY_SEPARATOR . "templates" . DIRECTORY_SEPARATOR);
+
+            foreach ($pluginDirs as $dir) {
+                $filename = $dir . "_submenuBar.php";
+                if (file_exists($filename)) {
+                    include($filename);
+                }
             }
-          }
         }
 
-		$this->buttons = $button;
-        $this->buttonHelp = isset($buttonHelp)?$buttonHelp:null;
-					
-		
-	} 
-	
-	
-	
-	/*
-	* 
-	*/	
-	public function executeMenubar(){				
+        $this->buttons = $button;
+        $this->buttonHelp = isset($buttonHelp) ? $buttonHelp : null;
+    }
+    /*
+     * 
+     */
 
-		
-		$this->grupos = $this->getUser()->getMenu();
-		$this->userid = $this->getUser()->getUserId();
+    public function executeMenubar() {
 
-        
-
-
-	} 
-	
-	
-	
+        $this->grupos = $this->getUser()->getMenu();
+        $this->userid = "Bienvenido <b>" . $this->getUser()->getNombre() . "</b>";
+        $trm = Doctrine::getTable("Trms")->find(date("Y-m-d"));
+        if ($trm)
+            $this->trmHoy = "$" . round($trm->getCaPesos(), 2);
+        else
+            $this->trmHoy = "Sin Registrar";
+    }
 }
 ?>
