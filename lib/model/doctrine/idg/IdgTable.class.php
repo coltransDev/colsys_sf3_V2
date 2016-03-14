e0916b7c8a3de622253025a19dff783dad5e78d1
Andrea L. Ramirez <alramirez@coltrans.com.co>
2016-03-14 17:40:24 +0000

    [IdgTable]  Ticket 30396 Incluir usuarios de otras empresas

--- lib/model/doctrine/idg/IdgTable.class.php
+++ lib/model/doctrine/idg/IdgTable.class.php
@@ -2,19 +2,22 @@
 
 class IdgTable extends Doctrine_Table {
 
-   static public function getUnIndicador($idg, $fecha, $idsucursal) {
-      $suc_array = array($idsucursal, "999");
-      $idg_max = Doctrine::getTable("IdgConfig")
-              ->createQuery("c")
-              ->select("c.*")
-              ->where("c.ca_idg = ?", $idg)
-              ->whereIn("c.ca_idsucursal", $suc_array)
-              ->andWhere("'$fecha' between c.ca_fchini and c.ca_fchfin")
-              ->addOrderBy("c.ca_idsucursal")
-              ->limit(1)
-              //->getSqlQuery();
-              ->fetchOne();
-      return($idg_max);
-   }
+    static public function getUnIndicador($idg, $fecha, $idsucursal) {        
+        $sucursal = Doctrine::getTable('Sucursal')->find($idsucursal);
+        $suc_array = array($sucursal->getCaNombre(), "Todas Las sucursales");
 
+        $idg_max = Doctrine::getTable("IdgConfig")
+                ->createQuery("c")
+                ->select("c.*")
+                ->innerJoin("c.Sucursal s")
+                ->where("c.ca_idg = ?", $idg)                
+                ->whereIn("s.ca_nombre", $suc_array)
+                ->addWhere("c.ca_fcheliminado IS NULL")
+                ->andWhere("'$fecha' between c.ca_fchini and c.ca_fchfin")
+                ->addOrderBy("c.ca_idsucursal")
+                ->addOrderBy("c.ca_fchini DESC")
+                ->limit(1)                
+                ->fetchOne();
+        return($idg_max);
+    }
 }
