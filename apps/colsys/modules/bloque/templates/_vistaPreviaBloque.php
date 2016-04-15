cb066c7a5b80f6560be6e47881e695f3f8b6506a
Andrea L. Ramirez <alramirez@coltrans.com.co>
2016-04-15 19:05:14 +0000

    [bloque] Actualización de producción 2016/04/15

--- apps/colsys/modules/bloque/templates/_vistaPreviaBloque.php
+++ apps/colsys/modules/bloque/templates/_vistaPreviaBloque.php
@@ -1,11 +1,17 @@
-<div class="bloque" id="b_<?=html_entity_decode($bloque->ca_id) ?>">
-    <p class="intro-bloque"><?=html_entity_decode($bloque->ca_introduccion) ?></p>
+<? if ($bloque->getProperty("titulo")) { 
+    ?>
+    <span style="font-size: 12px; font-weight: bold;"><?= html_entity_decode($bloque->ca_titulo) ?></span>
+    <? 
+    } 
+?>
+<div class="bloque" id="b_<?= html_entity_decode($bloque->ca_id) ?>">
+    <p class="intro-bloque"><?= html_entity_decode($bloque->ca_introduccion) ?></p>
     <?
-    foreach ($bloque->getPreguntasOrdenadas($bloque->ca_id) as $pregunta){
-        include_partial('pregunta/vistaPreviaPregunta', array('pregunta' => $pregunta, 'servicios' => $servicios));
+    foreach ($bloque->getPreguntasOrdenadas($bloque->ca_id) as $pregunta) {
+        include_partial('pregunta/vistaPreviaPregunta', array('pregunta' => $pregunta, 'servicios' => $servicios, 'formulario' => $formulario));
     }
     ?>
 </div>
-<? if ($bloque->ca_tipo==1){ ?>
+<? if ($bloque->ca_tipo == 1) { ?>
     <div id="indicator" style="display: none"></div>
 <? } ?>
