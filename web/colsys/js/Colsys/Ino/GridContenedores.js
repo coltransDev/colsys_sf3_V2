e2193a3c9049d130386b1107ab4367e480c12e0c
Carlos G. Lopez <clopez@coltrans.com.co>
2017-09-08 17:03:35 +0000

    [inoF2] Radicación Muisca y Control Comodatos

--- web/colsys/js/Colsys/Ino/GridContenedores.js
+++ web/colsys/js/Colsys/Ino/GridContenedores.js
@@ -1,3 +1,5 @@
+var win_comodato = null;
+
 comboBoxRenderer = function (combo) {
     return function (value) {
 
@@ -6,6 +8,7 @@ comboBoxRenderer = function (combo) {
         return (rec === null ? value : rec.get(combo.displayField));
     };
 };
+
 Ext.define('Colsys.Ino.GridContenedores', {
     extend: 'Ext.grid.Panel',
     alias: 'widget.Colsys.Ino.GridContenedores',
@@ -15,6 +18,10 @@ Ext.define('Colsys.Ino.GridContenedores', {
     width: 640,
     height: 500,
     listeners: {
+        beforerender: function (ct, position) {
+            this.setHeight(this.up('tabpanel').up('tabpanel').getHeight() - 150);
+            this.setWidth(this.up('tabpanel').up('tabpanel').getWidth() - 50);
+        },
         afterrender: function (ct, position) {
             this.getStore().reload({
                 params: {
@@ -23,125 +30,140 @@ Ext.define('Colsys.Ino.GridContenedores', {
             });
         },
         render: function (ct, position) {
-            columns= new Array();
+            columns = new Array();
             //console.log(this.idtransporte);
-            if(this.idtransporte=="Terrestre" && this.idimpoexpo=="INTERNO")
-            {
-                columns.push(
-                        {
-                            header: "Vehiculo",
-                            dataIndex: 'idvehiculo' + this.idmaster,
-                            //width: 150,
-                            editor: Ext.create('Colsys.Widgets.WgParametros', {                                
-                                //style: 'display:inline-block;text-align:center;font-weight:bold;',
-                                caso_uso: 'CU020',
-                                id: 'tipovehiculo1' + this.idmaster,
-                                idmaster: this.idmaster,
-                                idtransporte: this.idtransporte,
-                                idimpoexpo: this.idimpoexpo
-                            }),
-                            renderer: comboBoxRenderer(Ext.getCmp('tipovehiculo1' + this.idmaster))
-                        }
-                        );
-                        
+            if (this.idtransporte == "Terrestre" && this.idimpoexpo == "INTERNO") {
+                columns.push({
+                    header: "Vehiculo",
+                    dataIndex: 'idvehiculo' + this.idmaster,
+                    //width: 150,
+                    editor: Ext.create('Colsys.Widgets.WgParametros', {
+                        //style: 'display:inline-block;text-align:center;font-weight:bold;',
+                        caso_uso: 'CU020',
+                        id: 'tipovehiculo1' + this.idmaster,
+                        idmaster: this.idmaster,
+                        idtransporte: this.idtransporte,
+                        idimpoexpo: this.idimpoexpo
+                    }),
+                    renderer: comboBoxRenderer(Ext.getCmp('tipovehiculo1' + this.idmaster))
+                });
             }
-            columns.push(
-                    
-                        {
-                            header: "equipo",
-                            dataIndex: 'idequipo' + this.idmaster,
-                            hidden: true
-                        },
-                        {
-                            header: "Concepto",
-                            dataIndex: 'idconcepto' + this.idmaster,
-                            width: 150,
-                            editor: Ext.create('Colsys.Widgets.wgConceptosContenedores', {
-                                id: 'conceptocontenedores' + this.idmaster,
-                                idmaster: this.idmaster,
-                                idtransporte: this.idtransporte,
-                                idimpoexpo: this.idimpoexpo
-                            }),
-                            renderer: comboBoxRenderer(Ext.getCmp('conceptocontenedores' + this.idmaster))
-                        },
-                        {
-                            header: "Cantidad",
-                            dataIndex: 'cantidad' + this.idmaster,
-                            width: 90,
-                            editor: {
-                                xtype: 'numberfield'
-                            }
-                        },
-                        {
-                            header: "Serial",
-                            dataIndex: 'serial' + this.idmaster,
-                            width: 158,
-                            editor: {
-                                xtype: 'textfield',
-                                maxLength: 12,
-                                maxLengthText: 'Tama\u00F1o m\u00E1ximo 12'
-                            }
-                        },
-                        {
-                            header: "Precinto",
-                            dataIndex: 'precinto' + this.idmaster,
-                            width: 158,
-                            editor: {
-                                xtype: 'textfield',
-                                maxLength: 30,
-                                maxLengthText: 'Tama\u00F1o m\u00E1ximo 30'
-                            }
-                        }, {
-                            header: "Observaciones",
-                            dataIndex: 'observaciones'+ this.idmaster,
-                            width: 550,
-                            editor: {
-                                xtype: 'textfield'
-                            }
-                        }, {
-                            menuDisabled: true,
-                            sortable: false,
-                            xtype: 'actioncolumn',
-                            width: 20,
-                            items: [{
-                                    iconCls: 'delete',
-                                    tooltip: 'Anular la Encuesta',
-                                    handler: function (grid, rowIndex, colIndex) {
-                                        me = this;
-                                        var store = me.up('grid').getStore();
-
-                                        var rec = grid.getStore().getAt(rowIndex);
-                                        idequipo = rec.data.idequipo;
-                                        Ext.MessageBox.confirm('Confirmaci&oacute;n de Eliminaci&oacute;n', 'Est&aacute; seguro que desea anular el registro?', function (choice) {
-                                            if (choice == 'yes') {
-                                                Ext.Ajax.request({
-                                                    waitMsg: 'Eliminando...',
-                                                    url: '/inoF2/eliminarContenedor',
-                                                    params: {
-                                                        idequipo: idequipo,
-                                                    },
-                                                    failure: function (response, options) {
-                                                        Ext.MessageBox.alert("Mensaje", 'Se presento un error Eliminando el registro.<br>' + response.errorInfo);
-                                                        success = false;
-                                                    },
-                                                    success: function (response, options) {
-                                                        var res = Ext.JSON.decode(response.responseText);
-                                                        if (res.success) {
-                                                            store.reload();
-                                                        } else {
-                                                            Ext.MessageBox.alert("Mensaje", 'Se presento un error guardando los registros.<br>' + res.responseInfo);
-                                                        }
-                                                    }
-                                                });
-                                            }
-                                        });
-
+            columns.push({
+                header: "equipo",
+                dataIndex: 'idequipo' + this.idmaster,
+                hidden: true
+            }, {
+                header: "Concepto",
+                dataIndex: 'idconcepto' + this.idmaster,
+                width: 150,
+                editor: Ext.create('Colsys.Widgets.wgConceptosContenedores', {
+                    id: 'conceptocontenedores' + this.idmaster,
+                    idmaster: this.idmaster,
+                    idtransporte: this.idtransporte,
+                    idimpoexpo: this.idimpoexpo
+                }),
+                renderer: comboBoxRenderer(Ext.getCmp('conceptocontenedores' + this.idmaster))
+            }, {
+                header: "Cantidad",
+                dataIndex: 'cantidad' + this.idmaster,
+                width: 90,
+                editor: {
+                    xtype: 'numberfield'
+                }
+            }, {
+                header: "Serial",
+                dataIndex: 'serial' + this.idmaster,
+                width: 158,
+                editor: {
+                    xtype: 'textfield',
+                    maxLength: 12,
+                    maxLengthText: 'Tama\u00F1o m\u00E1ximo 12'
+                }
+            }, {
+                header: "Precinto",
+                dataIndex: 'precinto' + this.idmaster,
+                width: 158,
+                editor: {
+                    xtype: 'textfield',
+                    maxLength: 30,
+                    maxLengthText: 'Tama\u00F1o m\u00E1ximo 30'
+                }
+            }, {
+                header: "Observaciones",
+                dataIndex: 'observaciones' + this.idmaster,
+                width: 550,
+                editor: {
+                    xtype: 'textfield'
+                }
+            }, {
+                menuDisabled: true,
+                sortable: false,
+                xtype: 'actioncolumn',
+                width: 40,
+                items: [{
+                        iconCls: 'report_edit',
+                        tooltip: 'Contrato de Comodato',
+                        handler: function (grid, rowIndex, colIndex) {
+                            var rec = grid.getStore().getAt(rowIndex);
+                            if (win_comodato == null) {
+                                win_comodato = new Ext.Window({
+                                    id: 'winControlComodato',
+                                    title: 'Control Comodatos',
+                                    width: 800,
+                                    height: 310,
+                                    closeAction: 'destroy',
+                                    listeners: {
+                                        destroy: function (obj, eOpts)
+                                        {
+                                            win_comodato = null;
+                                        }
+                                    },
+                                    items: {
+                                        xtype: 'Colsys.Ino.FormControlComodato',
+                                        id: 'formControlComodato',
+                                        idequipo: rec.get("idequipo")
                                     }
-                                }]
+                                });
+                            }
+                            win_comodato.show();
                         }
+                    }, {
+                        iconCls: 'delete',
+                        tooltip: 'Anular la Encuesta',
+                        handler: function (grid, rowIndex, colIndex) {
+                            me = this;
+                            var store = me.up('grid').getStore();
 
-                   
-                    );
+                            var rec = grid.getStore().getAt(rowIndex);
+                            idequipo = rec.data.idequipo;
+                            Ext.MessageBox.confirm('Confirmaci&oacute;n de Eliminaci&oacute;n', 'Est&aacute; seguro que desea anular el registro?', function (choice) {
+                                if (choice == 'yes') {
+                                    Ext.Ajax.request({
+                                        waitMsg: 'Eliminando...',
+                                        url: '/inoF2/eliminarContenedor',
+                                        params: {
+                                            idequipo: idequipo,
+                                        },
+                                        failure: function (response, options) {
+                                            Ext.MessageBox.alert("Mensaje", 'Se presento un error Eliminando el registro.<br>' + response.errorInfo);
+                                            success = false;
+                                        },
+                                        success: function (response, options) {
+                                            var res = Ext.JSON.decode(response.responseText);
+                                            if (res.success) {
+                                                store.reload();
+                                            } else {
+                                                Ext.MessageBox.alert("Mensaje", 'Se presento un error guardando los registros.<br>' + res.responseInfo);
+                                            }
+                                        }
+                                    });
+                                }
+                            });
+
+                        }
+                    }]
+            });
+
             this.reconfigure(
                     //     this.superclass.onRender.call(this, ct, position),
                     store = Ext.create('Ext.data.Store', {
@@ -168,9 +190,8 @@ Ext.define('Colsys.Ino.GridContenedores', {
 
 
             tb = new Ext.toolbar.Toolbar();
-            
-            if (this.permisos.Crear == true)
-            {
+
+            if (this.permisos.Crear == true) {
                 tb.add({
                     xtype: 'button',
                     text: 'Nuevo Contenedor',
@@ -184,10 +205,9 @@ Ext.define('Colsys.Ino.GridContenedores', {
                     }
                 });
             }
-            if (this.permisos.Editar == true)
-            {
-                tb.add(
-                {
+
+            if (this.permisos.Editar == true) {
+                tb.add({
                     xtype: 'button',
                     text: 'Guardar',
                     height: 30,
@@ -203,8 +223,7 @@ Ext.define('Colsys.Ino.GridContenedores', {
 
                         var r = Ext.create(store.getModel());
                         fields = new Array();
-                        for (i = 0; i < r.fields.length; i++)
-                        {
+                        for (i = 0; i < r.fields.length; i++) {
                             fields.push(r.fields[i].name.replace(idmaster, ""));
                         }
 
@@ -250,11 +269,10 @@ Ext.define('Colsys.Ino.GridContenedores', {
                             }
                         });
                     }
-                }); 
+                });
             }
-            
-            tb.add(
-            {
+
+            tb.add({
                 xtype: 'button',
                 text: 'Refrescar',
                 height: 30,
@@ -265,7 +283,6 @@ Ext.define('Colsys.Ino.GridContenedores', {
                 }
             });
 
-            
             this.addDocked(tb);
 
             Ext.getCmp('conceptocontenedores' + this.idmaster).getStore().reload({
