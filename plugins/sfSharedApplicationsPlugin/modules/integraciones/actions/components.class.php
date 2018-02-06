56b738e89078d4840e64aa7de834c308c3544aec
Carlos G. Lopez <clopez@coltrans.com.co>
2018-02-06 21:20:07 +0000

    Actualización con versión de Replica

---
+++ plugins/sfSharedApplicationsPlugin/modules/integraciones/actions/components.class.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * gestDocumental components.
+ *
+ * @package    colsys
+ * @subpackage config
+ * @author     Mauricio Quinche
+ * @version    SVN: $Id: actions.class.php 2692 2006-11-15 21:03:55Z fabien $
+ */
+class integracionesComponents extends sfComponents
+{
+    
+    
+
+    
+}
+?>
