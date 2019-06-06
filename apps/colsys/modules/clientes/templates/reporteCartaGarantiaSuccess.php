8b7c71a2cf3a09272a8dd65cb5c1eef0282b6d60
Andrea L. Ramirez <alramirez@coltrans.com.co>
2019-06-06 14:36:22 +0000

    [clientes/actions.class.php] Ticket 69622 Carta de Garantía

--- apps/colsys/modules/clientes/templates/reporteCartaGarantiaSuccess.php
+++ apps/colsys/modules/clientes/templates/reporteCartaGarantiaSuccess.php
@@ -51,5 +51,58 @@
 
 </tbody>
 </table>
+<br />
+<br />
+
+<h3>Clientes con Carta de Garantía VENCIDA</h3>
+
+<table class="tableList" border="1">
+<thead>
+<tr>
+  <th>Id Cliente</th>
+  <th>D&iacute;gito</th>
+  <th>Cliente</th>
+  <th>Direcci&oacute;n</th>
+  <th>Tel&eacute;fono</th>
+  <th>Fax</th>
+  <th>Ciudad</th>
+  <th>Fch.Firmado</th>
+  <th>Fch.Vencimiento</th>
+  <th>Vendedor</th>
+  <th>Sucursal</th>
+</tr>
+</thead>
+<tbody>
+<?php foreach ($clientesVenCartaGarantia as $cliente): ?>
+<tr >
+      <td><?php echo $cliente["ca_idalterno"] ?></td>
+      <td><?php echo $cliente["ca_digito"] ?></td>
+      <td><?php echo $cliente["ca_compania"] ?></td>
+      <td><?php echo $cliente["ca_direccion"]." ".$cliente["ca_oficina"]." ".$cliente["ca_torre"]." ".$cliente["ca_bloque"]." ".$cliente["ca_interior"]." ".$cliente["ca_complemento"] ?></td>
+      <td><?php echo $cliente["ca_telefonos"] ?></td>
+      <td><?php echo $cliente["ca_fax"] ?></td>
+      <td><?php echo $cliente["ca_ciudad"] ?></td>
+      <td><?php echo $cliente["ca_fchfirmado"] ?></td>
+      <td><?php echo $cliente["ca_fchvencimiento"] ?></td>
+      <td><?php echo $cliente["ca_vendedor"] ?></td>
+      <td><?php echo $cliente["ca_sucursal"] ?></td>
+</tr>
+<?php
+      endforeach;
+
+	if( count($clientesVenCartaGarantia)==0 ){
+?>
+    <tr>
+	    <td colspan="11"><div align="center">Reporte sin Registros</div></td>
+    </tr>
+
+<?
+	}
+?>
+
+</tbody>
+</table>
+<br />
+<br />
 
 </div>
