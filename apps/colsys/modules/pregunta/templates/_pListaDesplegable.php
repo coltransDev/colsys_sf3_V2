42d064b7e7185d81f6a9c1e7c6dbc42fa768921a
Andrea L. Ramirez <alramirez@coltrans.com.co>
2019-08-14 17:40:08 +0000

    [pregunta/_plistaDesplegable] Actualización para formulario 19

--- apps/colsys/modules/pregunta/templates/_pListaDesplegable.php
+++ apps/colsys/modules/pregunta/templates/_pListaDesplegable.php
@@ -1,5 +1,5 @@
 <? include_partial('pregunta/error', array('pregunta' => $pregunta)); ?>
-<select class="lista-desplegable" name="en0<? echo $pregunta->ca_id ?>sencillo" id="en_<? echo $pregunta->ca_id ?>" obligatorio="<?=$pregunta->ca_obligatoria?>" idpregunta="<?=$pregunta->ca_id?>">
+<select onchange="mostrar(<?=$pregunta->ca_id?>)" class="lista-desplegable" name="en0<? echo $pregunta->ca_id ?>sencillo" id="en_<? echo $pregunta->ca_id ?>" obligatorio="<?=$pregunta->ca_obligatoria?>" idpregunta="<?=$pregunta->ca_id?>">
      <option class="select-option" value="">seleccione una opci&oacute;n</option>
     <? foreach ($pregunta->getOpcionesOrdenadas($pregunta->ca_id) as $opcion): ?>
         <option class="select-option" value="<? echo html_entity_decode($opcion->ca_texto) ?>"><? echo html_entity_decode($opcion->ca_texto) ?></option>
@@ -15,3 +15,60 @@
 <textarea class="parrafo area-adicional" id="en_<?php echo $pregunta->ca_id ?>"   name="en0<?php echo $pregunta->ca_id ?>sencilloComentario"></textarea>
 <?}?>
 
+<script>
+    
+function mostrar(idpregunta){
+    //se realiza para el formulario 17 la pregunta si ha tenido o no quejas y reclamos
+    if(idpregunta == 143){
+        var opcion = $("#en_"+idpregunta).val();
+        var obj = $("input[id^='grupo_"+144+"']");
+        var pregunta = $("div[id^="+144+"]");
+        var campoPregunta = $("#p_"+144);
+        
+        if(opcion === "NO"){            
+            pregunta.hide();            
+            campoPregunta.hide();
+            
+            $.each( obj, function( key, el ) {                
+                var j = $('input[name='+el.name+']');
+                j.attr("obligatorio", 0); 
+            });
+            
+        }else if(opcion === "SI"){
+            pregunta.show();            
+            campoPregunta.show();
+            
+            $.each( obj, function( key, el ) {                
+                var j = $('input[name='+el.name+']');                
+                j.attr("obligatorio", 1);                
+            });
+        }
+    }
+    //se realiza para el formulario 17 la pregunta si ha tenido o no quejas y reclamos
+    if(idpregunta == 154){
+        var opcion = $("#en_"+idpregunta).val();
+        var obj = $("input[id^='grupo_"+155+"']");
+        var pregunta = $("div[id^="+155+"]");
+        var campoPregunta = $("#p_"+155);
+        
+        if(opcion === "NO"){            
+            pregunta.hide();            
+            campoPregunta.hide();
+            
+            $.each( obj, function( key, el ) {                
+                var j = $('input[name='+el.name+']');
+                j.attr("obligatorio", 0); 
+            });
+            
+        }else if(opcion === "SI"){
+            pregunta.show();            
+            campoPregunta.show();
+            
+            $.each( obj, function( key, el ) {                
+                var j = $('input[name='+el.name+']');                
+                j.attr("obligatorio", 1);                
+            });
+        }
+    }
+}
+</script>
