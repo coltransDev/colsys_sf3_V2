e5de3f6d3be5dbfcfdad53d04dd99e155e50ce3d
Andrea L. Ramirez <alramirez@coltrans.com.co>
2019-11-27 13:41:04 +0000

    Ticket # 78517: Ajuste en la intranet para desavtivar usuario automáticamente

--- plugins/sfSharedApplicationsPlugin/modules/adminUsers/templates/emailDesvinculacionSuccess.php
+++ plugins/sfSharedApplicationsPlugin/modules/adminUsers/templates/emailDesvinculacionSuccess.php
@@ -2,6 +2,7 @@
     <div class="box1" align="center">
         <table class="tableList" border="0">
             <tr>Se ha enviado correctamente la notificación a nivel nacional</tr><br />
+            <tr><b>Por seguridad, se ha desactivado la cuenta de Colsys.</b></tr><br />
             <tr>Por favor recuerde al Jefe inmediato crear Ticket a sistemas para eliminar las cuentas asociadas.</tr>
             
         </table>
