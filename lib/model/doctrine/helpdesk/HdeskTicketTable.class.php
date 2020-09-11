cfa1084a748f6859be11491394329b5baa0956ce
Andrea L. Ramirez <alramirez@coltrans.com.co>
2020-09-11 22:01:53 +0000

    Ticket # 92414 Inconvenientes con plataforma colsys

--- lib/model/doctrine/helpdesk/HdeskTicketTable.class.php
+++ lib/model/doctrine/helpdesk/HdeskTicketTable.class.php
@@ -23,10 +23,10 @@ class HdeskTicketTable extends Doctrine_Table {
                  ->fetchOne();
             $iddepartamento=$departamento->getCaIddepartamento();            
         }
-            
+        
+
         
         
-            
         $q = Doctrine_Query::create()->from("HdeskTicket h");
         $q->where("h.ca_idticket = ?", $idticket);
         /*$ticket = $q->fetchOne();
@@ -45,15 +45,35 @@ class HdeskTicketTable extends Doctrine_Table {
                 $q->leftJoin("h.HdeskTicketUser hu  ");
                 $q->addWhere("(h.ca_login = ? OR ug.ca_login = ? OR hu.ca_login = ? )", array($user->getCaLogin(), $user->getCaLogin(), $user->getCaLogin()));
                 break;
-            case 2:
+            case 2:                
+                $depAdic = $user->getProperty("helpDesk");
+                if($depAdic){
+                
+                    $d = Doctrine::getTable("Departamento")
+                          ->createQuery("d");
+
+                    if(strpos($depAdic,"|"))
+                        $d->orWhereIn("d.ca_nombre", explode("|",$depAdic));
+                    else
+                        $d->orWhere("d.ca_nombre = ?", $depAdic);            
+
+                    $departamentos = $d->execute();
+
+                    foreach($departamentos as $d){                    
+                        $deps[] = $d->getCaIddepartamento();                                        
+                    }
+                }
+                $deps[] = $iddepartamento;
+                
                 $q->leftJoin("h.HdeskGroup g ");
-                $q->leftJoin("h.HdeskTicketUser hu  ");
-                $q->addWhere("(h.ca_login = ? OR g.ca_iddepartament = ? OR hu.ca_login = ?)", array($user->getCaLogin(), $iddepartamento, $user->getCaLogin()));
+                $q->leftJoin("h.HdeskTicketUser hu  ");    
+                $q->addWhere("(h.ca_login = '".$user->getCaLogin()."' OR g.ca_iddepartament IN (".implode(",", $deps).") OR hu.ca_login = '".$user->getCaLogin()."')");                
                 break;
         } 
-        /*echo $idticket."<br>".$user->getCaLogin()."<br>".$iddepartamento."<br>".$user->getCaLogin()."<br>";
-        echo $q->getSqlQuery();
-        exit;*/
+//        echo $idticket."<br>".$user->getCaLogin()."<br>".$iddepartamento."<br>".$user->getCaLogin()."<br>";
+//        print_r($deps);
+//        echo $q->getSqlQuery();
+//        exit;
         return $q->fetchOne();
     }
     
