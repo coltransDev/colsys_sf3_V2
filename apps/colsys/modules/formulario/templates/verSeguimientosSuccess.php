15b5d558f4871e2d7e8a22287496b559b8481f3d
Andrea L. Ramirez <alramirez@coltrans.com.co>
2021-02-26 20:32:21 +0000

    Actualización se realiza mensaje cuando no hay seguimientos para una encuesta.

--- apps/colsys/modules/formulario/templates/verSeguimientosSuccess.php
+++ apps/colsys/modules/formulario/templates/verSeguimientosSuccess.php
@@ -31,22 +31,29 @@
 <?
 $seguimientos = $sf_data->getRaw("seguimientos");
 $i=0;
-foreach( $seguimientos as $seguimiento ){
-    ?>
-    <div  class="entry-<?=$i%2==0?"even":"odd"?>">
-        <div  class="entry-date"><?=Utils::fechaMes($seguimiento->getCaFchevento())?></div>
-        <b><span style="color:#3792D4;"><?=($seguimiento->getUsuario()?$seguimiento->getUsuario()->getCaNombre():$seguimiento->getCaUsucreado())?></span></b>
-        <br />
-        <?
-        if( $seguimiento->getCaFchcompromiso() ){
+if(count($seguimientos)>0){
+    foreach( $seguimientos as $seguimiento ){
         ?>
-        <span style="font-size:11px;"><b>Seguimiento programado:</b> <?=Utils::fechaMes(Utils::parseDate($seguimiento->getCaFchcompromiso(), "Y-m-d"))?></span>
-        <br /><br/>
+        <div  class="entry-<?=$i%2==0?"even":"odd"?>">
+            <div  class="entry-date"><?=Utils::fechaMes($seguimiento->getCaFchevento())?></div>
+            <b><span style="color:#3792D4;"><?=($seguimiento->getUsuario()?$seguimiento->getUsuario()->getCaNombre():$seguimiento->getCaUsucreado())?></span></b>
+            <br />
+            <?
+            if( $seguimiento->getCaFchcompromiso() ){
+            ?>
+            <span style="font-size:11px;"><b>Seguimiento programado:</b> <?=Utils::fechaMes(Utils::parseDate($seguimiento->getCaFchcompromiso(), "Y-m-d"))?></span>
+            <br /><br/>
+            <?
+            }?>
+            <span style="font-style:italic;">Detalles: </span><?=str_replace("\n","<br />",$seguimiento->getCaDetalle())?><br/>
+            <span style="font-style:italic;">Compromisos: </span><?=str_replace("\n","<br />",$seguimiento->getCaCompromisos())?><br/>
+        </div><br /><br />
         <?
-        }?>
-        <span style="font-style:italic;">Detalles: </span><?=str_replace("\n","<br />",$seguimiento->getCaDetalle())?><br/>
-        <span style="font-style:italic;">Compromisos: </span><?=str_replace("\n","<br />",$seguimiento->getCaCompromisos())?><br/>
-    </div><br /><br />
+        $i++;
+    }
+}else{
+    ?>
+    <div  class="entry-even">No hay seguimientos para &eacute;sta encuesta.</div>
     <?
-    $i++;
 }
+?>
