c941bc31beeabe0c95ef337da1ddb7a0d8fd1dd8
Andrea L. Ramirez <alramirez@coltrans.com.co>
2021-04-09 17:13:50 +0000

    2021_04_09 12:10 Actualización de producción

--- web/colsys/js/Colsys/GestDocumental/FormSubirArchivos.js
+++ web/colsys/js/Colsys/GestDocumental/FormSubirArchivos.js
@@ -27,10 +27,11 @@ Ext.define('Colsys.GestDocumental.FormSubirArchivos', {
         anchor: '100%',
         allowBlank: false,
         msgTarget: 'side',
-        labelWidth: 70
+        labelWidth: 75
     },
     listeners: {
         beforerender: function () {
+            me=this;
             this.add({
                 xtype: 'textfield',
                 fieldLabel: 'Name',
@@ -48,7 +49,8 @@ Ext.define('Colsys.GestDocumental.FormSubirArchivos', {
                     style: 'position:relative',
                     iconCls: 'upload-icon'
                 }
-            }, {
+            },
+             {
                 xtype: 'Colsys.Widgets.WgDocumentos',
                 id: 'documento',
                 name: 'documento',
@@ -56,16 +58,94 @@ Ext.define('Colsys.GestDocumental.FormSubirArchivos', {
                 queryMode: 'local',
                 displayField: 'name',
                 valueField: 'id',
-                idsserie: this.idsserie,
-                idimpoexpo: this.idimpoexpo,
-                idtransporte: this.idtransporte
-            }, {
-                xtype: 'textfield',
-                fieldLabel: 'Subcarpeta',
-                id: 'ref2',
-                name: 'ref2',
-                allowBlank: true
+                idsserie: me.idsserie,
+                idimpoexpo: me.idimpoexpo,
+                idtransporte: me.idtransporte
             });
+            
+            //console.log(me.idtransporte);
+            //console.log(me.idimpoexpo);
+            //console.log(me.idmodalidad);
+            if(me.idimpoexpo == "OTM-DTA")
+            {
+                if(me.idmodalidad=="LCL")
+                {
+                    this.add(
+                        Ext.create('Ext.form.ComboBox', {
+                        fieldLabel: 'Subcarpeta',
+                        id:'ref2',
+                        name:'ref2',
+                        store: Ext.create('Ext.data.Store', {
+                            fields: ['id', 'name'],
+                            data : [
+                                {"id":"DOCUMENTOS SOPORTE", "name":"DOCUMENTOS SOPORTE"},
+                                {"id":"DOCUMENTOS MUISCA", "name":"DOCUMENTOS MUISCA"},
+                                {"id":"LIBERACION", "name":"LIBERACION"},
+                                {"id":"CARTA PORTE", "name":"CARTA PORTE"},
+                                {"id":"AUTORIZACION", "name":"AUTORIZACION"},
+                                {"id":"COSTOS", "name":"COSTOS"},
+                                {"id":"CIERRE", "name":"CIERRE"}
+                                
+                            ]
+                        }),
+                        width:150,
+                        //labelWidth:50,
+                        queryMode: 'local',
+                        displayField: 'name',
+                        valueField: 'id'
+                        })
+                    );
+                }
+                else
+                {
+                    this.add({
+                        xtype: 'textfield',
+                        fieldLabel: 'Subcarpeta 2',
+                        id: 'ref2',
+                        name: 'ref2',
+                        allowBlank: false,
+                        value:me.ref2
+                    });
+                    
+                    this.add(
+                        Ext.create('Ext.form.ComboBox', {
+                        fieldLabel: 'Subcarpeta',
+                        id:'ref3',
+                        name:'ref3',
+                        store: Ext.create('Ext.data.Store', {
+                            fields: ['id', 'name'],
+                            data : [
+                                {"id":"DOCUMENTOS SOPORTE", "name":"DOCUMENTOS SOPORTE"},
+                                {"id":"DOCUMENTOS MUISCA", "name":"DOCUMENTOS MUISCA"},
+                                {"id":"LIBERACION", "name":"LIBERACION"},
+                                {"id":"CARTA PORTE", "name":"CARTA PORTE"},
+                                {"id":"AUTORIZACION", "name":"AUTORIZACION"},
+                                {"id":"COSTOS", "name":"COSTOS"},
+                                {"id":"CIERRE", "name":"CIERRE"}
+                                
+                            ]
+                        }),
+                        width:150,
+                        //labelWidth:50,
+                        queryMode: 'local',
+                        displayField: 'name',
+                        valueField: 'id'
+                        })
+                    );
+                    
+                }
+            }
+            else
+            {
+                this.add({
+                    xtype: 'textfield',
+                    fieldLabel: 'Subcarpeta',
+                    id: 'ref2',
+                    name: 'ref2',
+                    allowBlank: true,
+                    value:me.ref2
+                });
+            }
         }
     },
     buttons: [{
@@ -75,13 +155,15 @@ Ext.define('Colsys.GestDocumental.FormSubirArchivos', {
                 var idmaster = this.up('form').idmaster;
                 var dcto = Ext.getCmp("documento").rawValue;
                 var form = this.up('form').getForm();
-
+                console.log(idreferencia);
+                
                 if (form.isValid()) {
                     form.submit({
                         waitMsg: 'Guardando',
                         url: '/gestDocumental/subirArchivoTRD',
                         params: {
-                            ref1: idreferencia
+                            ref1: idreferencia,
+                            tam_max: 800
                         },
                         success: function (fp, o) {
                             if (o.result.success) {
