06340251bc11b02f7be0df86abd861d3ae8c3e6c
Andrea L. Ramirez <alramirez@coltrans.com.co>
2021-04-27 16:06:09 +0000

    Ticket # 92890 Revisar la autenticación de usuarios por BD en Colsys

--- apps/colsys/lib/myLoginValidator.class.php
+++ apps/colsys/lib/myLoginValidator.class.php
@@ -1,53 +1,55 @@
 <?php
- 
-class myLoginValidator extends sfValidatorBase
-{    
-	public function configure($options = array(), $messages = array()){
-		$this->addOption('username_field', 'username');
-		$this->addOption('password_field', 'passwd');    		
-		
-		$this->setMessage('invalid', 'El usuario o la clave es invalida');
-		
-	}
-  
- 
-	protected function doClean($values)
-	{			
-            $username = isset($values[$this->getOption('username_field')]) ? $values[$this->getOption('username_field')] : '';
-            $passwd = isset($values[$this->getOption('password_field')]) ? $values[$this->getOption('password_field')] : '';			
-            if( $username && $passwd ){
-                
-                $usuario = Doctrine::getTable("Usuario")
+
+class myLoginValidator extends sfValidatorBase {
+
+    public function configure($options = array(), $messages = array()) {
+        $this->addOption('username_field', 'username');
+        $this->addOption('password_field', 'passwd');
+
+        $this->setMessage('invalid', 'El usuario o la clave es invalida');
+    }
+
+    protected function doClean($values) {
+        $username = isset($values[$this->getOption('username_field')]) ? $values[$this->getOption('username_field')] : '';
+        $passwd = isset($values[$this->getOption('password_field')]) ? $values[$this->getOption('password_field')] : '';
+        if ($username && $passwd) {
+
+            $usuario = Doctrine::getTable("Usuario")
                     ->createQuery("u")
                     ->select("ca_login,ca_authmethod")
                     ->where("u.ca_login = ?", $username)
                     ->fetchOne();
-                $error = "";
-                $errorno = "";
-                if( $usuario && $usuario->checkPasswd( $passwd, $error , $errorno) ){
-                    if( $usuario->getCaAuthmethod()=="ldap" ){
-                        sfContext::getInstance()->getUser()->signInLDAP( $username );
-                        return $values;					
-                    }
+            $error = "";
+            $errorno = "";
+            if ($usuario && $usuario->checkPasswd($passwd, $error, $errorno)) {
+                if ($usuario->getCaAuthmethod() == "ldap") {
+                    sfContext::getInstance()->getUser()->signInLDAP($username);
+                    return $values;
+                }
 
-                    if( $usuario->getCaAuthmethod()=="sha1" ){										                   
-                        sfContext::getInstance()->getUser()->signInAlternative( $username );
-                        return $values;                    
-                    }
+                if ($usuario->getCaAuthmethod() == "sha1") {
+                    sfContext::getInstance()->getUser()->signInAlternative($username);
+                    return $values;
                 }
             }
-        
-            switch( $errorno ){
-                /*case 49:
-                    $this->setMessage('invalid', 'Las entradas de gracias para este usuario se acabaron, debe cambiar su clave de NOVELL');
-                    break;*/
-                case 53:
-                    $this->setMessage('invalid', 'La cuenta de NOVELL se encuentra bloqueada');
-                    break;
-                default :
-                    $this->setMessage('invalid', 'El usuario o la clave es invalida'.(isset($errorno)?', Cod: '.$errorno:''));
-                    break;
-            }throw new sfValidatorErrorSchema($this, array($this->getOption('username_field') => new sfValidatorError($this, 'invalid')));	
-	}
+        }
+
+        switch ($errorno) {
+            /* case 49:
+              $this->setMessage('invalid', 'Las entradas de gracias para este usuario se acabaron, debe cambiar su clave de NOVELL');
+              break; */
+            case 9999:
+                $this->setMessage('invalid', 'El usuario se encuentra bloqueado. Por favor comuníquese con el área de sistemas.');
+                break;
+            case 53:
+                $this->setMessage('invalid', 'La cuenta de NOVELL se encuentra bloqueada');
+                break;
+            default :
+                $this->setMessage('invalid', 'El usuario o la clave es invalida' . (isset($errorno) ? ', Cod: ' . $errorno : ''));
+                break;
+        }
+
+        throw new sfValidatorErrorSchema($this, array($this->getOption('username_field') => new sfValidatorError($this, 'invalid')));
+    }
 }
 ?>
