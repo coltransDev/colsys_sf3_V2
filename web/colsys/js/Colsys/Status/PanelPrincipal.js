10df7827b32c2bc342468648918fee425c24f86b
Andrea L. Ramirez <alramirez@coltrans.com.co>
2021-09-22 21:32:39 +0000

    [Colsys/status/PanelPrincipal] Modificación de visualización del panel

--- web/colsys/js/Colsys/Status/PanelPrincipal.js
+++ web/colsys/js/Colsys/Status/PanelPrincipal.js
@@ -1,11 +1,13 @@
 Ext.define('Colsys.Status.PanelPrincipal', {
     extend: 'Ext.panel.Panel',
     alias: 'widget.Colsys.Status.PanelPrincipal',
-    autoScroll: true,
+    //height: 800,
+    //scrollable: true,
+    //autoScroll: true,
     layout: {
         type: 'table',
         columns: 1,
-        tdAttrs: {style: 'padding: 10px; vertical-align: top;'},
+        tdAttrs: {style: 'padding: 5px 5px 0px 5px; vertical-align: top;'},
         tableAttrs: {
             style: {
                 width: '98%'
@@ -16,15 +18,200 @@ Ext.define('Colsys.Status.PanelPrincipal', {
          listeners: {
         render: function (me, eOpts) {
             var idmaster = this.idmaster;
-            this.add(
-                    Ext.create('Colsys.Status.PanelHeaderMaster', {
-                        id: 'panel-header-master-' + this.idmaster,
-                        title: 'Informaci\u00F3n General',
-                        idmaster: this.idmaster
-                    })
-                    );
-
+            var referencia = this.idreferencia;
+            
             tabs = new Array();
+            
+            tb = Ext.create('Ext.toolbar.Toolbar', {});
+            tb.add({
+                xtype: 'button',
+                text: 'Informaci\u00F3n General',                
+                iconCls: 'page_white_edit',
+                handler: function () {                                        
+                    Ext.create('Ext.window.Window', {
+                        title: 'Informaci\u00F3n General '+ referencia,
+                        width: 800,
+                        closeAction: 'destroy',                                    
+                        bodyPadding: 5,                        
+                        layout: 'fit',
+                        items: [  // Let's put an empty grid in just to illustrate fit layout
+                            Ext.create('Colsys.Status.PanelHeaderMaster', {
+                                id: 'panel-header-master-' + idmaster,                                
+                                idmaster: idmaster
+                            }) // A dummy empty data store
+                        ]
+                    }).show();
+                }
+            },{
+                xtype: 'button',
+                text: 'Notificaciones Puerto',
+                id: 'not-pto' + me.idmaster,
+                iconCls: 'email',
+                handler: function () {
+                    var myMsg = Ext.create('Ext.window.MessageBox', {
+                        closeAction: 'destroy',
+                        id: 'message',
+                        closable: false
+                    }).show({
+                        title: 'Mensaje',
+                        message: 'Consultando status de la referencia...!'
+                    });
+
+                    Ext.Ajax.request({
+                        waiting: 'Cargando información de Status',
+                        url: '/status/confirmaciones',
+                        params: {
+                            idmaster: me.idmaster
+                        },
+                        method: 'POST',
+                        waitTitle: 'Connecting',
+                        waitMsg: 'Cargando Información General...',
+                        //scope: me,
+                        success: function (response, options) {
+
+                            var res = Ext.decode(response.responseText);
+                            myMsg.close();
+                            if (res.root !== null) {
+                                console.log(res.root.confirmaciones);
+
+
+                                winNotPuerto = new Ext.Window({
+                                    title: 'Notificaciones Puerto',
+                                    id: 'win-not-' + me.idmaster,
+                                    width: 800,
+                                    //height: 250,
+                                    closeAction: 'destroy',
+                                    autoScroll: true,
+                                    bodyPadding: 5,
+                                    items: [
+                                        Ext.create('Ext.grid.Panel', {
+                                            store: Ext.create('Ext.data.Store', {
+                                                data: res.root.confirmaciones
+                                            }),
+                                            columns: [{
+                                                    text: 'Fch. Env\u00EDo',
+                                                    dataIndex: 'fchenvio',
+                                                    flex: 1
+                                                }, {
+                                                    text: 'Asunto',
+                                                    dataIndex: 'subject',
+                                                    flex: 3
+                                                }, {
+                                                    text: ' Ver Email',
+                                                    dataIndex: 'idemail',
+                                                    flex: 0.6,
+                                                    renderer: function (value) {
+                                                        return "<a href='/email/verEmail?id=" + value + "' target='_blank'><img src='/images/22x22/email.gif'/></a>";
+                                                    }
+                                                    //width: 120
+                                                }],
+                                            height: 400,
+                                            //width: 500,
+                                            renderTo: Ext.getBody()
+                                        })
+                                    ],
+                                    listeners: {
+                                        close: function (win, eOpts) {
+                                            winNotPuerto = null;
+                                        }
+                                    }
+                                }).show();
+                            } else {
+                                Ext.MessageBox.alert("Mensaje", 'Esta referencia no tiene confirmaciones asociadas');
+                            }
+
+
+                        }
+                    });
+
+                }
+            },{
+                xtype: 'button',
+                text: 'Tickets Asociados',
+                id: 'ticket-' + me.idmaster,
+                iconCls: 'application_form',
+                handler: function () {
+
+                    var myMsg = Ext.create('Ext.window.MessageBox', {
+                        closeAction: 'destroy',
+                        id: 'message',
+                        closable: false
+                    }).show({
+                        title: 'Mensaje',
+                        message: 'Consultando tickets asociados de la referencia...!'
+                    });
+
+                    Ext.Ajax.request({
+                        waiting: 'Cargando información de Status',
+                        url: '/status/tickets',
+                        params: {
+                            idmaster: me.idmaster
+                        },
+                        method: 'POST',
+                        waitTitle: 'Connecting',
+                        waitMsg: 'Cargando Información General...',
+                        //scope: me,
+                        success: function (response, options) {
+
+                            var res = Ext.decode(response.responseText);
+                            myMsg.close();
+                            if (res.root !== null) {
+                                console.log(res.root.tickets);
+
+                                winTicket = new Ext.Window({
+                                    title: 'Tickets Asociados',
+                                    id: 'win-tk-' + me.idmaster,
+                                    width: 700,
+                                    height: 250,
+                                    closeAction: 'destroy',
+                                    autoScroll: true,
+                                    bodyPadding: 5,
+                                    items: [
+                                        Ext.create('Ext.grid.Panel', {
+                                            store: Ext.create('Ext.data.Store', {
+                                                data: res.root.tickets
+                                            }),
+                                            columns: [{
+                                                    text: 'Ticket No.',
+                                                    dataIndex: 'idticket',
+                                                    flex: 1
+                                                }, {
+                                                    text: 'Asunto',
+                                                    dataIndex: 'title',
+                                                    flex: 3
+                                                }, {
+                                                    text: 'Ver Ticket',
+                                                    dataIndex: 'idemail',
+                                                    flex: 0.6,
+                                                    renderer: function (value) {
+                                                        return "<a href='/email/verEmail?id=" + value + "' target='_blank'><img src='/images/22x22/email.gif'/></a>";
+                                                    }                                                                    
+                                                }],
+                                            height: 400,
+                                            //width: 500,
+                                            renderTo: Ext.getBody()
+                                        })
+                                    ],
+                                    listeners: {
+                                        close: function (win, eOpts) {
+                                            winTicket = null;
+                                        }
+                                    }
+                                }).show();
+
+                            } else {
+                                Ext.MessageBox.alert("Mensaje", 'Esta referencia no tiene tickets asociados');
+                            }
+
+
+                        }
+                    });
+
+
+                    }
+            });
+            
+            this.addDocked(tb);
 
             $.each(this.permisos, function (modulo, data) {
                 $.each(data, function (tipo, valor) {
@@ -48,7 +235,7 @@ Ext.define('Colsys.Status.PanelPrincipal', {
                     {
                         xtype: 'tabpanel',
                         id: 'tab-panel-modulos' + this.idmaster,
-                        bodyPadding: 10,
+                        bodyPadding: 5,
                         items: tabs,
                         listeners: {
                             tabchange: function (tabPanel, newCard, oldCard, eOpts) {
@@ -104,40 +291,7 @@ Ext.define('Colsys.Status.PanelPrincipal', {
                                         if (Ext.getCmp('grid-planilla-' + me.idmaster)) {
                                             Ext.getCmp('grid-planilla-' + me.idmaster).hide();
                                         }
-//                                        Ext.Ajax.request({
-//                                            url: '/status/datosContenedores',
-//                                            params: {
-//                                                idmaster: me.idmaster
-//                                            },
-//                                            method: 'POST',
-//                                            waitTitle: 'Connecting',
-//                                            waitMsg: 'Cargando Datos contenedores...',
-//                                            scope: me,
-//                                            success: function (response, options) {
-//
-//                                                var res = Ext.decode(response.responseText);
-//                                                var equipos = res.root;
-//                                                var html = "";
-//                                                html += "\n\n";
-//                                                $.each(equipos, function (index, data) {
-//                                                    html += "# " + (index + 1) + "\n";
-//                                                    html += "Equipo: " + data["concepto"] + "\n";
-//                                                    html += "Id. Equipo: " + data["serial"] + "\n";
-//                                                    html += "Entrega Comodado: " + data["entregacomodato"] + "\n";
-//                                                    html += "Patio de entrega: " + data["idpatio"] + "\n";
-//                                                    html += "Observaciones: " + data["observaciones"] + "\n";
-//                                                    html += "/*********************/\n";
-//                                                    html += "\n";
-//                                                });
-//                                                var mensajeCont = Ext.getCmp('form-principal-' + modulo + idmaster).texto;
-//                                                Ext.getCmp('status_mensaje-' + modulo + me.idmaster).setValue(mensajeCont + html);
-//
-//                                            },
-//                                            failure: function () {
-//                                                console.log('failure');
-//                                            }
-//                                        });
-                                                var mensajeCont = Ext.getCmp('form-principal-' + modulo + idmaster).texto;
+                                        var mensajeCont = Ext.getCmp('form-principal-' + modulo + idmaster).texto;
                                         Ext.getCmp('status_mensaje-' + modulo + me.idmaster).setValue(mensajeCont);
                                         break;
                                     case "otm":
@@ -159,6 +313,9 @@ Ext.define('Colsys.Status.PanelPrincipal', {
             );
     
             this.crearPanelClientes(me, this.idmaster);
+        },
+        beforerender: function (ct, position) {            
+            this.setHeight(this.up('tabpanel').getHeight() - 50);            
         }
     },
     agregarPanelFotos: function (tipo) {
@@ -236,6 +393,7 @@ Ext.define('Colsys.Status.PanelPrincipal', {
                                 manageHeight : true,
                                 collapsed: true,
                                 collapsible: true,
+//                                scrollable: true,
                                 idhouse: data["idhouse"],
                                 hideCollapseTool: false,
                                 idcliente: data["idcliente"],
@@ -266,25 +424,36 @@ Ext.define('Colsys.Status.PanelPrincipal', {
 
                     me.add({
                         xtype: 'panel',
-                        title: 'Cuadro de Clientes de la Referencia',
+                        controller: 'panel-hijos',
+                        title: 'Cuadro de Clientes de la Referencia',                        
                         id: 'panel-hijos-' + me.idmaster,
                         name: 'panel-hijos-' + me.idmaster,
                         idmaster: me.idmaster,
+                        idreferencia: me.idreferencia,
+                        height: 300,
+                        scrollable: true,
+                        tbar: [
+                        { 
+                            xtype: 'button', 
+                            text: 'Expandir',        
+                            iconCls: 'switch',
+                            handler: 'onToggle'
+                        }],                        
                         layout: {
                             type: 'vbox',
                             pack: 'start',
                             align: 'stretch'
                         },
                         defaults: {
-                            margin: '0 0 5 0',
+//                            margin: '0 0 5 0',
                             //height: 820,
-                            bodyPadding: 10,
+                            //bodyPadding: 10,
                             frame: true,
                             border: true
                         },
-                        items: hijos
+                        items: hijos                        
                     });
-
+                    
                     me.down("tabpanel").idhouses = idhouses.join();
                     me.down("tabpanel").setActiveTab(1);
                     me.down("tabpanel").setActiveTab(0);
@@ -322,3 +491,41 @@ Ext.define('Colsys.Status.PanelPrincipal', {
         }
     }
 });
+
+Ext.define('Colsys.view.form.PanelHijosController', {
+        extend: 'Ext.app.ViewController',
+        alias: 'controller.panel-hijos', 
+        onToggle: function(t,eOpts){
+            t.disable();
+            var idmaster = t.up("panel").idmaster;
+            var myMask = new Ext.LoadMask({
+                msg    : 'Seleccionando todos los clientes, un momento por favor..',
+                target :  Ext.getCmp('panel-principal-' + idmaster)
+            });
+            myMask.show(); 
+
+            var idhouses = t.up("panel").up("panel").down("tabpanel").idhouses;
+            var tipo = t.text;
+            
+            switch(tipo){
+                case "Expandir":
+
+                    $.each(idhouses.split(","), function (key, idhouse) {                        
+                        t.up("panel").child('panel[id=panel-house-' + idhouse + ']').expand();
+                    }); 
+                    myMask.hide(); 
+                    t.setText("Contraer");
+                    t.enable();
+                    break;
+
+                case "Contraer":
+                    $.each(idhouses.split(","), function (key, idhouse) {                        
+                        t.up("panel").child('panel[id=panel-house-' + idhouse + ']').collapse();
+                    }); 
+                    myMask.hide();
+                    t.setText("Expandir");
+                    t.enable();
+                    break;
+            }
+        }
+    });
